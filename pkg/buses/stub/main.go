0c428fea842695c945e629dd3d1b4a557298d72b
Scott Andrews <sandrews@pivotal.io>
2018-06-11 18:32:11 -0400

    Drop martini dependency

--- pkg/buses/stub/main.go
+++ pkg/buses/stub/main.go
@@ -26,7 +26,6 @@ import (
 	"strings"
 	"time"
 
-	"github.com/go-martini/martini"
 	"github.com/golang/glog"
 	channelsv1alpha1 "github.com/knative/eventing/pkg/apis/channels/v1alpha1"
 	"github.com/knative/eventing/pkg/buses"
@@ -39,9 +38,70 @@ import (
 var (
 	masterURL  string
 	kubeconfig string
+)
 
-	bus            = os.Getenv("BUS_NAME")
-	forwardHeaders = []string{
+type StubBus struct {
+	name           string
+	monitor        *buses.Monitor
+	client         *http.Client
+	forwardHeaders []string
+}
+
+func (b *StubBus) splitChannelName(host string) (string, string) {
+	chunks := strings.Split(host, ".")
+	channel := chunks[0]
+	namespace := chunks[1]
+	return channel, namespace
+}
+
+func (b *StubBus) handleEvent(res http.ResponseWriter, req *http.Request) {
+	host := req.Host
+	glog.Infof("Recieved request for %s\n", host)
+	channel, namespace := b.splitChannelName(host)
+	subscriptions := b.monitor.Subscriptions(channel, namespace)
+	if subscriptions == nil {
+		res.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	body, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		res.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	res.WriteHeader(http.StatusAccepted)
+
+	if len(*subscriptions) == 0 {
+		glog.Warningf("No subscribers for channel %q\n", channel)
+	}
+	for _, subscription := range *subscriptions {
+		go b.dispatchEvent(channel, body, req.Header, subscription.Subscriber)
+	}
+}
+
+func (b *StubBus) dispatchEvent(channel string, body []byte, headers http.Header, subscriber string) {
+	glog.Infof("Sending to %q for %q\n", subscriber, channel)
+
+	url := fmt.Sprintf("http://%s/", subscriber)
+	request, err := http.NewRequest(http.MethodPost, url, bytes.NewReader(body))
+	if err != nil {
+		glog.Errorf("Unable to create subscriber request %v", err)
+	}
+	request.Header.Set("x-bus", b.name)
+	request.Header.Set("x-channel", channel)
+	for _, header := range b.forwardHeaders {
+		if value := headers.Get(header); value != "" {
+			request.Header.Set(header, value)
+		}
+	}
+	_, err = b.client.Do(request)
+	if err != nil {
+		glog.Errorf("Unable to complete subscriber request %v", err)
+	}
+}
+
+func NewStubBus(name string, monitor *buses.Monitor) *StubBus {
+	forwardHeaders := []string{
 		"content-type",
 		"x-request-id",
 		"x-b3-traceid",
@@ -51,13 +111,15 @@ var (
 		"x-b3-flags",
 		"x-ot-span-context",
 	}
-)
 
-func splitChannelName(host string) (string, string) {
-	chunks := strings.Split(host, ".")
-	channel := chunks[0]
-	namespace := chunks[1]
-	return channel, namespace
+	bus := StubBus{
+		name:           name,
+		monitor:        monitor,
+		client:         &http.Client{},
+		forwardHeaders: forwardHeaders,
+	}
+
+	return &bus
 }
 
 func main() {
@@ -76,8 +138,10 @@ func main() {
 		glog.Fatalf("Error building clientset: %s", err.Error())
 	}
 
+	name := os.Getenv("BUS_NAME")
+
 	informerFactory := informers.NewSharedInformerFactory(client, time.Second*30)
-	monitor := buses.NewMonitor(bus, informerFactory, buses.MonitorEventHandlerFuncs{
+	monitor := buses.NewMonitor(name, informerFactory, buses.MonitorEventHandlerFuncs{
 		ProvisionFunc: func(channel channelsv1alpha1.Channel) {
 			glog.Infof("Provision channel %q\n", channel.Name)
 		},
@@ -91,68 +155,14 @@ func main() {
 			glog.Infof("Unubscribe %q from %q channel\n", subscription.Spec.Subscriber, subscription.Spec.Channel)
 		},
 	})
+	bus := NewStubBus(name, monitor)
+
 	go informerFactory.Start(stopCh)
 
-	m := createServer(monitor)
-	m.Run()
+	http.HandleFunc("/", bus.handleEvent)
+	glog.Fatal(http.ListenAndServe(":8080", nil))
 
 	glog.Flush()
-}
-
-func createServer(monitor *buses.Monitor) *martini.ClassicMartini {
-	m := martini.Classic()
-
-	m.Post("/", func(req *http.Request, res http.ResponseWriter) {
-		host := req.Host
-		glog.Infof("Recieved request for %s\n", host)
-		channel, namespace := splitChannelName(host)
-		subscriptions := monitor.Subscriptions(channel, namespace)
-		if subscriptions == nil {
-			res.WriteHeader(http.StatusNotFound)
-			return
-		}
-
-		body, err := ioutil.ReadAll(req.Body)
-		if err != nil {
-			res.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-
-		res.WriteHeader(http.StatusAccepted)
-		go func() {
-			if len(*subscriptions) == 0 {
-				glog.Warningf("No subscribers for channel %q\n", channel)
-			}
-
-			// make upstream requests
-			client := &http.Client{}
-
-			for _, subscription := range *subscriptions {
-				go func(subscriber string) {
-					glog.Infof("Sending to %q for %q\n", subscriber, channel)
-
-					url := fmt.Sprintf("http://%s/", subscriber)
-					request, err := http.NewRequest(http.MethodPost, url, bytes.NewReader(body))
-					if err != nil {
-						glog.Errorf("Unable to create subscriber request %v", err)
-					}
-					request.Header.Set("x-bus", bus)
-					request.Header.Set("x-channel", channel)
-					for _, header := range forwardHeaders {
-						if value := req.Header.Get(header); value != "" {
-							request.Header.Set(header, value)
-						}
-					}
-					_, err = client.Do(request)
-					if err != nil {
-						glog.Errorf("Unable to complete subscriber request %v", err)
-					}
-				}(subscription.Subscriber)
-			}
-		}()
-	})
-
-	return m
 }
 
 func init() {
