54c896e11e0bfb4d87a04fcbbde98a9da829ee70
Grant Rodgers <grantrodgers@google.com>
2018-06-29 16:34:28 -0700

    Set up controller-runtime manager and example
    This is an example of a controller-runtime controller-manager command
    and a sample controller with table-driven controller tests.
    
    The controller-runtime project contains packages meant to make
    controllers easier to write with less boilerplate.
    
    Table testing is inspired by similar work in knative/serving, modified
    to work with controller-runtime reconcilers and clients.

---
+++ pkg/controller/sample/provider.go
@@ -0,0 +1,61 @@
+/*
+Copyright 2018 The Knative Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package sample
+
+import (
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	"sigs.k8s.io/controller-runtime/pkg/source"
+)
+
+type reconciler struct {
+	client client.Client
+}
+
+// Verify the struct implements reconcile.Reconciler
+var _ reconcile.Reconciler = &reconciler{}
+
+// ProvideController returns a foo controller.
+func ProvideController(mrg manager.Manager) (controller.Controller, error) {
+	// Setup a new controller to Reconcile ReplicaSets
+	c, err := controller.New("foo-controller", mrg, controller.Options{
+		Reconciler: &reconciler{
+			client: mrg.GetClient(),
+		},
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	// Watch ReplicaSets and enqueue ReplicaSet object key
+	if err := c.Watch(&source.Kind{Type: &appsv1.ReplicaSet{}}, &handler.EnqueueRequestForObject{}); err != nil {
+		return nil, err
+	}
+
+	// Watch Pods and enqueue owning ReplicaSet key
+	if err := c.Watch(&source.Kind{Type: &corev1.Pod{}},
+		&handler.EnqueueRequestForOwner{OwnerType: &appsv1.ReplicaSet{}, IsController: true}); err != nil {
+		return nil, err
+	}
+
+	return c, nil
+}
