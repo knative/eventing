75a719d392959a9cff7795dc461e94a9f90db664
Paul Morie <pmorie@redhat.com>
2018-07-02 19:21:25 -0400

    Add/clarify godoc in bind api types.go files (#146)

--- pkg/apis/feeds/v1alpha1/bind_types.go
+++ pkg/apis/feeds/v1alpha1/bind_types.go
@@ -151,7 +151,7 @@ type ParametersFromSource struct {
 
 // SecretKeyReference references a key of a Secret.
 type SecretKeyReference struct {
-	// The name of the secret in the pod's namespace to select from.
+	// The name of the secret in the resource's namespace to select from.
 	Name string `json:"name"`
 	// The key of the secret to select from.  Must be a valid secret key.
 	Key string `json:"key"`
