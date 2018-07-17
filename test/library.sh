e1ca44e1b3a57f26c027022f9a4605e0f901ab3e
Grant Rodgers <grantrodgers@google.com>
2018-07-17 15:25:42 -0700

    Use the feeds kind when getting a feed
    $KIND isn't defined so it breaks the command. Also, get the full yaml of
    the object instead of just the name.

--- test/library.sh
+++ test/library.sh
@@ -152,7 +152,7 @@ function wait_until_feed_ready() {
     sleep 2
   done
   echo -e "\n\nERROR: timeout waiting for feed $NAMESPACE/$NAME to be ready"
-  kubectl get -n $NAMESPACE $KIND $NAME  $1
+  kubectl get -n $NAMESPACE feeds $NAME $1 -oyaml
   return 1
 }
 
