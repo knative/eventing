e63455ad8cef9aa2db68262852055c2ba76f02cd
Grant Rodgers <grantrodgers@google.com>
2018-07-18 10:34:20 -0700

    remove stray argument to kubectl get
    This seems like a copy/paste error.

--- test/library.sh
+++ test/library.sh
@@ -152,8 +152,8 @@ function wait_until_feed_ready() {
     sleep 2
   done
   echo -e "\n\nERROR: timeout waiting for feed $NAMESPACE/$NAME to be ready"
-  kubectl get -n $NAMESPACE feeds $NAME $1 -oyaml
-  kubectl get -n $NAMESPACE jobs $NAME-start $1 -oyaml
+  kubectl get -n $NAMESPACE feeds $NAME -oyaml
+  kubectl get -n $NAMESPACE jobs $NAME-start -oyaml
   return 1
 }
 
