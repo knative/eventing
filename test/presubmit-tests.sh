1a507a71d944034864f4045d32b8251990753c63
Adriano Cunha <adrcunha@google.com>
2018-07-22 14:56:53 -0700

    Use dep-collector from prow-tests image
    We're consolidating the test infrastructure into a single place, so all repos get the same fixes, updates and new features.
    
    `dep-collector` was added to prow-tests image in knative/test-infra#22 and `update_licenses()` in knative/test-infra#24
    
    Bonus: also remove `ko` as a dependency, since it's also already installed in prow-tests image.

--- test/presubmit-tests.sh
+++ test/presubmit-tests.sh
@@ -44,7 +44,7 @@ function build_tests() {
   # Fetch the google/licenseclassifier for its license db
   go get github.com/google/licenseclassifier || return 1
   # Check that we don't have any forbidden licenses in our images.
-  go run ./vendor/github.com/mattmoor/dep-collector/*.go -check ./cmd/* || result=1
+  dep-collector -check ./cmd/* || result=1
   return ${result}
 }
 
