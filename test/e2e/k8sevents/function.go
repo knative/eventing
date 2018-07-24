996ae295aa150d8e892128422e6427157709c90f
Evan Anderson <evan.k.anderson@gmail.com>
2018-07-24 11:29:35 -0700

    Remove `k8sevent` sample from eventing now that it's in docs. (#275)
    * Remove `k8sevent` sample from eventing now that it's in docs.
    
    * Clean up `README.md`, too
    
    * Fix e2e test that was depending on our samples, excitingly.

--- test/e2e/k8sevents/function.go
+++ test/e2e/k8sevents/function.go
@@ -1,12 +1,9 @@
 /*
 Copyright 2018 The Knative Authors
-
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
-
     https://www.apache.org/licenses/LICENSE-2.0
-
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -33,5 +30,6 @@ func handler(ctx context.Context, e *corev1.Event) {
 }
 
 func main() {
+	log.Print("Ready and listening on port 8080")
 	log.Fatal(http.ListenAndServe(":8080", event.Handler(handler)))
 }
