06438e2f03c9856830d83691a957a1d3d2440f3f
Scott Nichols <nicholss@google.com>
2018-08-01 11:06:36 -0700

    Starting to add webhook.

--- cmd/webhook/main.go
+++ cmd/webhook/main.go
@@ -1,5 +1,6 @@
 /*
-Copyright 2018 The Knative Authors
+Copyright 2017 The Knative Authors
+
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
@@ -17,45 +18,86 @@ package main
 
 import (
 	"flag"
+	"log"
 
-	"github.com/golang/glog"
+	"go.uber.org/zap"
+
+	"github.com/knative/pkg/configmap"
+	"github.com/knative/pkg/logging"
+	"github.com/knative/pkg/logging/logkey"
+	"github.com/knative/pkg/signals"
+	"github.com/knative/pkg/webhook"
+
 	"github.com/knative/eventing/pkg/system"
-	"github.com/knative/eventing/pkg/signals"
-	"github.com/knative/eventing/pkg/webhook"
 
+	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 )
 
+const (
+	logLevelKey = "webhook"
+)
+
 func main() {
 	flag.Parse()
-	defer glog.Flush()
+	cm, err := configmap.Load("/etc/config-logging")
+	if err != nil {
+		log.Fatalf("Error loading logging configuration: %v", err)
+	}
+	config, err := logging.NewConfigFromMap(cm)
+	if err != nil {
+		log.Fatalf("Error parsing logging configuration: %v", err)
+	}
+	logger, atomicLevel := logging.NewLoggerFromConfig(config, logLevelKey)
+	defer logger.Sync()
+	logger = logger.With(zap.String(logkey.ControllerType, "webhook"))
 
-	glog.Info("Starting the Configuration Webhook")
+	logger.Info("Starting the Eventing Webhook")
 
 	// set up signals so we handle the first shutdown signal gracefully
 	stopCh := signals.SetupSignalHandler()
 
 	clusterConfig, err := rest.InClusterConfig()
 	if err != nil {
-		glog.Fatal("Failed to get in cluster config", err)
+		logger.Fatal("Failed to get in cluster config", zap.Error(err))
 	}
 
-	clientset, err := kubernetes.NewForConfig(clusterConfig)
+	kubeClient, err := kubernetes.NewForConfig(clusterConfig)
 	if err != nil {
-		glog.Fatal("Failed to get the client set", err)
+		logger.Fatal("Failed to get the client set", zap.Error(err))
+	}
+
+	// Watch the logging config map and dynamically update logging levels.
+	configMapWatcher := configmap.NewDefaultWatcher(kubeClient, system.Namespace)
+	configMapWatcher.Watch(logging.ConfigName, logging.UpdateLevelFromConfigMap(logger, atomicLevel, logLevelKey))
+	if err = configMapWatcher.Start(stopCh); err != nil {
+		logger.Fatalf("failed to start configuration manager: %v", err)
 	}
 
 	options := webhook.ControllerOptions{
-		ServiceName:      "eventing-webhook",
-		ServiceNamespace: system.Namespace,
-		Port:             443,
-		SecretName:       "eventing-webhook-certs",
-		WebhookName:      "webhook.eventing.knative.dev",
+		ServiceName:    "eventing-webhook",
+		DeploymentName: "eventing-webhook",
+		Namespace:      system.Namespace,
+		Port:           443,
+		SecretName:     "eventing-webhook-certs",
+		WebhookName:    "eventing-webhook.eventing.knative.dev",
 	}
-	controller, err := webhook.NewAdmissionController(clientset, options)
+	controller := webhook.AdmissionController{
+		Client:  kubeClient,
+		Options: options,
+		// TODO(mattmoor): Will we need to rework these to support versioning?
+		GroupVersion: v1alpha1.SchemeGroupVersion,
+		Handlers: map[string]runtime.Object{
+			"Revision":      &v1alpha1.Revision{},
+			"Configuration": &v1alpha1.Configuration{},
+			"Route":         &v1alpha1.Route{},
+			"Service":       &v1alpha1.Service{},
+		},
+		Logger: logger,
+	}
 	if err != nil {
-		glog.Fatal("Failed to create the admission controller", err)
+		logger.Fatal("Failed to create the admission controller", zap.Error(err))
 	}
 	controller.Run(stopCh)
 }
