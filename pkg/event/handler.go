49ad835eb8867ec64d05d16b37f78e85f08b2e72
Adam Harwayne <harwayne@google.com>
2018-08-03 10:53:19 -0700

    Re-add accidentally removed debug messages.

--- pkg/event/handler.go
+++ pkg/event/handler.go
@@ -232,6 +232,7 @@ func (h *handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		dataPtr, dataArg := allocate(h.dataType)
 		eventContext, err := FromRequest(dataPtr, r)
 		if err != nil {
+			log.Printf("Failed to handle request %s; error %s", spew.Sdump(r), err)
 			w.WriteHeader(http.StatusBadRequest)
 			w.Write([]byte(`Invalid request`))
 			return
@@ -316,6 +317,7 @@ func (m Mux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	var rawData io.Reader
 	eventContext, err := FromRequest(&rawData, r)
 	if err != nil {
+		log.Printf("Failed to handle request: %s %s", err, spew.Sdump(r))
 		w.WriteHeader(http.StatusBadRequest)
 		w.Write([]byte(`Invalid request`))
 		return
