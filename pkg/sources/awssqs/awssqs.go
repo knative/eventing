rootfs/eventing-1

support aws sqs as event source
Signed-off-by: Huamin Chen <hchen@redhat.com>

pkg/sources/awssqs/awssqs.go
@@ -1,0 +1,216 @@
+/*
+Copyright 2018 The Knative Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"encoding/base64"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/golang/glog"
+	"github.com/google/uuid"
+	"github.com/knative/eventing/pkg/sources"
+
+	apierrs "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sqs"
+)
+
+const (
+	deployment = "deployment"
+)
+
+type AWSSQSEventSource struct {
+	// kubeclientset is a standard kubernetes clientset
+	kubeclientset kubernetes.Interface
+	image         string
+	// namespace where the binding is created
+	bindNamespace string
+	// serviceAccount that the container runs as. Launches Receive Adapter with the
+	// same Service Account
+	bindServiceAccountName string
+}
+
+func NewAWSSQSEventSource(kubeclientset kubernetes.Interface, bindNamespace string, bindServiceAccountName string, image string) sources.EventSource {
+	glog.Infof("Image: %q", image)
+	return &AWSSQSEventSource{kubeclientset: kubeclientset, bindNamespace: bindNamespace, bindServiceAccountName: bindServiceAccountName, image: image}
+}
+
+func (a *AWSSQSEventSource) StopFeed(trigger sources.EventTrigger, feedContext sources.FeedContext) error {
+	glog.Infof("Stopping AWS SQS Events feed with context %+v", feedContext)
+
+	deploymentName := feedContext.Context[deployment].(string)
+
+	err := a.deleteAWSSQSDeployment(deploymentName)
+	if err != nil {
+		glog.Warningf("Failed to delete deployment: %s", err)
+		return err
+	}
+	return nil
+}
+
+func (a *AWSSQSEventSource) StartFeed(trigger sources.EventTrigger, route string) (*sources.FeedContext, error) {
+	glog.Infof("creating awssqs feed context")
+	// create aws sqs deployment
+	awsToken := ""
+	awsId := trigger.Parameters["AWS_ACCESS_KEY_ID"].(string)
+	awsKey := trigger.Parameters["AWS_SECRET_ACCESS_KEY"].(string)
+	if token, ok := trigger.Parameters["AWS_SESSION_TOKEN"].(string); ok {
+		awsToken = token
+	}
+	if len(awsId) == 0 && len(awsKey) == 0 {
+		return nil, fmt.Errorf("missing AWS_ACCESS_KEY_ID or AWS_SECRET_ACCESS_KEY")
+	}
+	resource := strings.Split(trigger.Resource, "/")
+	if len(resource) != 2 {
+		return nil, fmt.Errorf("invalid resource: must be region/queue-name")
+	}
+	region := resource[0]
+	queueName := resource[1]
+
+	// create aws sqs client
+	sess := session.New(&aws.Config{
+		Region:      aws.String(region),
+		Credentials: credentials.NewStaticCredentials(awsId, awsKey, awsToken),
+	})
+	sqsClient := sqs.New(sess)
+	// list queue
+	param := &sqs.ListQueuesInput{
+		QueueNamePrefix: &queueName,
+	}
+	resp, err := sqsClient.ListQueues(param)
+	if err != nil {
+		return nil, fmt.Errorf("failed to list queue %q: %v", queueName, err)
+	}
+	queueUrl := ""
+	for i := range resp.QueueUrls {
+		q := *resp.QueueUrls[i]
+		// if both prefix and suffix match, return it
+		if strings.HasSuffix(q, queueName) {
+			queueUrl = q
+			break
+		}
+	}
+	// if queue not found, create one
+	if len(queueUrl) == 0 {
+		param := &sqs.CreateQueueInput{
+			QueueName: &queueName,
+		}
+		glog.Infof("create queue %s", queueName)
+		resp, err := sqsClient.CreateQueue(param)
+		if err != nil {
+			return nil, fmt.Errorf("failed to create queue %v", err)
+		}
+		queueUrl = *resp.QueueUrl
+	}
+
+	// create a random deployment name
+	uuid, err := uuid.NewRandom()
+	if err != nil {
+		return nil, err
+	}
+	deploymentName := "awssqs-" + queueName + "-" + uuid.String()
+	err = a.createAWSSQSDeployment(deploymentName, a.image, awsId, awsKey, awsToken, region, queueUrl, route)
+	if err != nil {
+		glog.Warningf("Failed to create deployment: %s", err)
+		return nil, err
+	}
+	return &sources.FeedContext{
+		Context: map[string]interface{}{
+			deployment: deploymentName,
+		}}, nil
+}
+
+func (a *AWSSQSEventSource) createAWSSQSDeployment(deploymentName, image, awsId, awsKey, awsToken, region, queueUrl, route string) error {
+	dc := a.kubeclientset.AppsV1().Deployments(a.bindNamespace)
+
+	// First, check if deployment exists already.
+	if _, err := dc.Get(deploymentName, metav1.GetOptions{}); err != nil {
+		if !apierrs.IsNotFound(err) {
+			glog.Infof("deployments.Get for %q failed: %v", deploymentName, err)
+			return err
+		}
+		glog.Infof("Deployment %q doesn't exist, creating", deploymentName)
+	} else {
+		glog.Infof("Found existing deployment %q", deploymentName)
+		return nil
+	}
+
+	deployment := MakeAWSSQSDeployment(a.bindNamespace, deploymentName, image, awsId, awsKey, awsToken, region, queueUrl, route)
+	_, createErr := dc.Create(deployment)
+	return createErr
+}
+
+func (a *AWSSQSEventSource) deleteAWSSQSDeployment(deploymentName string) error {
+	dc := a.kubeclientset.AppsV1().Deployments(a.bindNamespace)
+
+	// First, check if deployment exists already.
+	if _, err := dc.Get(deploymentName, metav1.GetOptions{}); err != nil {
+		if !apierrs.IsNotFound(err) {
+			glog.Infof("deployments.Get for %q failed: %v", deploymentName, err)
+			return err
+		}
+		glog.Infof("Deployment %q already deleted", deploymentName)
+		return nil
+	}
+
+	return dc.Delete(deploymentName, &metav1.DeleteOptions{})
+}
+
+type parameters struct {
+	Image string `json:"image,omitempty"`
+}
+
+func main() {
+	flag.Parse()
+
+	flag.Parse()
+
+	decodedParameters, _ := base64.StdEncoding.DecodeString(os.Getenv(sources.EventSourceParametersKey))
+
+	feedNamespace := os.Getenv(sources.FeedNamespaceKey)
+	feedServiceAccountName := os.Getenv(sources.FeedServiceAccountKey)
+
+	var p parameters
+	err := json.Unmarshal(decodedParameters, &p)
+	if err != nil {
+		panic(fmt.Sprintf("can not unmarshal %q : %v", decodedParameters, err))
+	}
+
+	cfg, err := clientcmd.BuildConfigFromFlags("", "")
+	if err != nil {
+		glog.Fatalf("Error building kubeconfig: %v", err.Error())
+	}
+
+	kubeClient, err := kubernetes.NewForConfig(cfg)
+	if err != nil {
+		glog.Fatalf("Error building kubernetes clientset: %v", err.Error())
+	}
+
+	sources.RunEventSource(NewAWSSQSEventSource(kubeClient, feedNamespace, feedServiceAccountName, p.Image))
+	log.Printf("Done...")
+}

pkg/sources/awssqs/sqs/sqs.go
@@ -1,0 +1,173 @@
+/*
+Copyright 2018 The Knative Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"bytes"
+	"context"
+	"flag"
+	"fmt"
+	"net/http"
+	"net/url"
+	"os"
+
+	"github.com/golang/glog"
+	"github.com/knative/eventing/pkg/signals"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sqs"
+)
+
+const (
+	// Target for messages
+	envTarget = "TARGET"
+	// aws id
+	envAWSId = "AWS_ACCESS_KEY_ID"
+	// aws key
+	envAWSKey = "AWS_SECRET_ACCESS_KEY"
+	// aws token
+	envAWSToken = "AWS_SESSION_TOKEN"
+	// aws region
+	envAWSRegion = "AWS_REGION"
+	// aws sqs queue url
+	envAWSSQSQueue = "AWS_SQS_QUEUE_URL"
+	// message receive timeout
+	waitTimeSecond = 5
+)
+
+var (
+	// forward headers
+	forwardHeaders = []string{
+		"content-type",
+		"x-request-id",
+		"x-b3-traceid",
+		"x-b3-spanid",
+		"x-b3-parentspanid",
+		"x-b3-sampled",
+		"x-b3-flags",
+		"x-ot-span-context",
+	}
+)
+
+type message struct {
+	Message string
+}
+
+func main() {
+	flag.Parse()
+
+	// set up signals so we handle the first shutdown signal gracefully
+	stopCh := signals.SetupSignalHandler()
+
+	target := os.Getenv(envTarget)
+	awsId := os.Getenv(envAWSId)
+	awsKey := os.Getenv(envAWSKey)
+	awsToken := os.Getenv(envAWSToken)
+	awsRegion := os.Getenv(envAWSRegion)
+	queueUrl := os.Getenv(envAWSSQSQueue)
+
+	glog.Infof("Target is: %q", target)
+
+	// create aws sqs client
+	sess := session.New(&aws.Config{
+		Region:      aws.String(awsRegion),
+		Credentials: credentials.NewStaticCredentials(awsId, awsKey, awsToken),
+	})
+	sqsClient := sqs.New(sess)
+
+	glog.Infof("Start receiving messages")
+	ctx := context.Background()
+	cctx, cancel := context.WithCancel(ctx)
+	go func() {
+		for {
+			rcvParam := &sqs.ReceiveMessageInput{
+				QueueUrl:        aws.String(queueUrl),
+				WaitTimeSeconds: aws.Int64(waitTimeSecond),
+			}
+			resp, err := sqsClient.ReceiveMessageWithContext(cctx, rcvParam)
+			if err != nil {
+				glog.Warningf("failed to receive from queue %s:%v", queueUrl, err)
+				break
+			}
+
+			glog.V(5).Infof("received msg: %v", resp)
+
+			for _, message := range resp.Messages {
+				err := postMessage(target, []byte(*message.Body), message.Attributes)
+				if err != nil {
+					// retry
+					continue
+				}
+				// delete message
+				deleteParam := &sqs.DeleteMessageInput{
+					QueueUrl:      aws.String(queueUrl),
+					ReceiptHandle: message.ReceiptHandle,
+				}
+				_, err = sqsClient.DeleteMessage(deleteParam)
+				if err != nil {
+					glog.Warningf("failed to delete msg %v: %v", *message.MessageId, err)
+				}
+				glog.Infof("Message %v has beed deleted", *message.MessageId)
+			}
+		}
+	}()
+	<-stopCh
+	cancel()
+	glog.Info("exiting")
+}
+
+func postMessage(target string, data []byte, attributes map[string]*string) error {
+	url := url.URL{
+		Scheme: "http",
+		Host:   target,
+		Path:   "/",
+	}
+	req, err := http.NewRequest(http.MethodPost, url.String(), bytes.NewReader(data))
+	if err != nil {
+		return err
+	}
+	req.Header = safeHeaders(attributesToHeaders(attributes))
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	if res.StatusCode < 200 || res.StatusCode >= 300 {
+		return fmt.Errorf("%q did not accept event: got HTTP %d", target, res.StatusCode)
+	}
+	return nil
+}
+
+func attributesToHeaders(attributes map[string]*string) http.Header {
+	headers := http.Header{}
+	for name, value := range attributes {
+		headers.Set(name, *value)
+	}
+	return headers
+}
+
+func safeHeaders(raw http.Header) http.Header {
+	safe := http.Header{}
+	for _, header := range forwardHeaders {
+		if value := raw.Get(header); value != "" {
+			safe.Set(header, value)
+		}
+	}
+	return safe
+}

knative/eventing

pkg/sources/awssqs/awssqs.go
@@ -1,0 +1,218 @@
+/*
+Copyright 2018 The Knative Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"encoding/base64"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/google/uuid"
+	"github.com/knative/eventing/pkg/sources"
+
+	apierrs "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sqs"
+)
+
+const (
+	deployment = "deployment"
+)
+
+type AWSSQSEventSource struct {
+	// kubeclientset is a standard kubernetes clientset
+	kubeclientset kubernetes.Interface
+	image         string
+	// namespace where the feeding is created
+	feedNamespace string
+	// serviceAccount that the container runs as. Launches Receive Adapter with the
+	// same Service Account
+	feedServiceAccountName string
+}
+
+func NewAWSSQSEventSource(kubeclientset kubernetes.Interface, feedNamespace string, feedServiceAccountName string, image string) sources.EventSource {
+	log.Printf("Image: %q", image)
+	return &AWSSQSEventSource{kubeclientset: kubeclientset, feedNamespace: feedNamespace, feedServiceAccountName: feedServiceAccountName, image: image}
+}
+
+func (a *AWSSQSEventSource) StopFeed(trigger sources.EventTrigger, feedContext sources.FeedContext) error {
+	log.Printf("Stopping AWS SQS Events feed with context %+v", feedContext)
+
+	if deploymentName, ok := feedContext.Context[deployment].(string); ok {
+		err := a.deleteAWSSQSDeployment(deploymentName)
+		if err != nil {
+			log.Printf("Failed to delete deployment: %s", err)
+			return err
+		}
+	}
+	return nil
+}
+
+func (a *AWSSQSEventSource) StartFeed(trigger sources.EventTrigger, route string) (*sources.FeedContext, error) {
+	log.Printf("creating awssqs feed context")
+	// create aws sqs deployment
+	awsToken := ""
+	awsId := trigger.Parameters["AWS_ACCESS_KEY_ID"].(string)
+	awsKey := trigger.Parameters["AWS_SECRET_ACCESS_KEY"].(string)
+	if token, ok := trigger.Parameters["AWS_SESSION_TOKEN"].(string); ok {
+		awsToken = token
+	}
+	if len(awsId) == 0 && len(awsKey) == 0 {
+		return nil, fmt.Errorf("missing AWS_ACCESS_KEY_ID or AWS_SECRET_ACCESS_KEY")
+	}
+	resource := strings.Split(trigger.Resource, "/")
+	if len(resource) != 2 {
+		return nil, fmt.Errorf("invalid resource: must be region/queue-name")
+	}
+	region := resource[0]
+	queueName := resource[1]
+
+	// create aws sqs client
+	sess := session.New(&aws.Config{
+		Region:      aws.String(region),
+		Credentials: credentials.NewStaticCredentials(awsId, awsKey, awsToken),
+	})
+	sqsClient := sqs.New(sess)
+	// list queue
+	param := &sqs.ListQueuesInput{
+		QueueNamePrefix: &queueName,
+	}
+	resp, err := sqsClient.ListQueues(param)
+	if err != nil {
+		log.Printf("failed to list queue %q: %v", queueName, err)
+		return nil, fmt.Errorf("failed to list queue %q: %v", queueName, err)
+	}
+	queueUrl := ""
+	for i := range resp.QueueUrls {
+		q := *resp.QueueUrls[i]
+		// if both prefix and suffix match, return it
+		if strings.HasSuffix(q, queueName) {
+			queueUrl = q
+			break
+		}
+	}
+	// if queue not found, create one
+	if len(queueUrl) == 0 {
+		param := &sqs.CreateQueueInput{
+			QueueName: &queueName,
+		}
+		log.Printf("create queue %s", queueName)
+		resp, err := sqsClient.CreateQueue(param)
+		if err != nil {
+			return nil, fmt.Errorf("failed to create queue %v", err)
+		}
+		queueUrl = *resp.QueueUrl
+	}
+
+	// create a random deployment name
+	uuid, err := uuid.NewRandom()
+	if err != nil {
+		return nil, err
+	}
+	deploymentName := "awssqs-" + queueName + "-" + uuid.String()
+	err = a.createAWSSQSDeployment(deploymentName, a.image, awsId, awsKey, awsToken, region, queueUrl, route)
+	if err != nil {
+		log.Printf("Failed to create deployment: %s", err)
+		return nil, err
+	}
+	return &sources.FeedContext{
+		Context: map[string]interface{}{
+			deployment: deploymentName,
+		}}, nil
+}
+
+func (a *AWSSQSEventSource) createAWSSQSDeployment(deploymentName, image, awsId, awsKey, awsToken, region, queueUrl, route string) error {
+	dc := a.kubeclientset.AppsV1().Deployments(a.feedNamespace)
+
+	// First, check if deployment exists already.
+	if _, err := dc.Get(deploymentName, metav1.GetOptions{}); err != nil {
+		if !apierrs.IsNotFound(err) {
+			log.Printf("deployments.Get for %q failed: %v", deploymentName, err)
+			return err
+		}
+		log.Printf("Deployment %q doesn't exist, creating", deploymentName)
+	} else {
+		log.Printf("Found existing deployment %q", deploymentName)
+		return nil
+	}
+
+	deployment := MakeAWSSQSDeployment(a.feedNamespace, deploymentName, image, awsId, awsKey, awsToken, region, queueUrl, route)
+	_, createErr := dc.Create(deployment)
+	return createErr
+}
+
+func (a *AWSSQSEventSource) deleteAWSSQSDeployment(deploymentName string) error {
+	dc := a.kubeclientset.AppsV1().Deployments(a.feedNamespace)
+
+	// First, check if deployment exists already.
+	if _, err := dc.Get(deploymentName, metav1.GetOptions{}); err != nil {
+		if !apierrs.IsNotFound(err) {
+			log.Printf("deployments.Get for %q failed: %v", deploymentName, err)
+			return err
+		}
+		log.Printf("Deployment %q already deleted", deploymentName)
+		return nil
+	}
+
+	return dc.Delete(deploymentName, &metav1.DeleteOptions{})
+}
+
+type parameters struct {
+	Image string `json:"image,omitempty"`
+}
+
+func main() {
+	flag.Parse()
+
+	decodedParameters, _ := base64.StdEncoding.DecodeString(os.Getenv(sources.EventSourceParametersKey))
+
+	feedNamespace := os.Getenv(sources.FeedNamespaceKey)
+	feedServiceAccountName := os.Getenv(sources.FeedServiceAccountKey)
+
+	var p parameters
+	err := json.Unmarshal(decodedParameters, &p)
+	if err != nil {
+		log.Printf("can not unmarshal %q : %v", decodedParameters, err)
+		return
+	}
+
+	cfg, err := clientcmd.BuildConfigFromFlags("", "")
+	if err != nil {
+		log.Printf("Error building kubeconfig: %v", err.Error())
+	}
+
+	kubeClient, err := kubernetes.NewForConfig(cfg)
+	if err != nil {
+		log.Printf("Error building kubernetes clientset: %v", err.Error())
+	}
+
+	err = sources.RunEventSource(NewAWSSQSEventSource(kubeClient, feedNamespace, feedServiceAccountName, p.Image))
+	if err != nil {
+		log.Printf("failed to create event: %v", err)
+	}
+	log.Printf("Done...")
+}

pkg/sources/awssqs/sqs/sqs.go
@@ -1,0 +1,174 @@
+/*
+Copyright 2018 The Knative Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"bytes"
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+
+	"github.com/knative/pkg/signals"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sqs"
+)
+
+const (
+	// Target for messages
+	envTarget = "TARGET"
+	// aws id
+	envAWSId = "AWS_ACCESS_KEY_ID"
+	// aws key
+	envAWSKey = "AWS_SECRET_ACCESS_KEY"
+	// aws token
+	envAWSToken = "AWS_SESSION_TOKEN"
+	// aws region
+	envAWSRegion = "AWS_REGION"
+	// aws sqs queue url
+	envAWSSQSQueue = "AWS_SQS_QUEUE_URL"
+	// message receive timeout
+	waitTimeSecond = 5
+)
+
+var (
+	// forward headers
+	forwardHeaders = []string{
+		"content-type",
+		"x-request-id",
+		"x-b3-traceid",
+		"x-b3-spanid",
+		"x-b3-parentspanid",
+		"x-b3-sampled",
+		"x-b3-flags",
+		"x-ot-span-context",
+	}
+)
+
+type message struct {
+	Message string
+}
+
+func main() {
+	flag.Parse()
+
+	// set up signals so we handle the first shutdown signal gracefully
+	stopCh := signals.SetupSignalHandler()
+
+	target := os.Getenv(envTarget)
+	awsId := os.Getenv(envAWSId)
+	awsKey := os.Getenv(envAWSKey)
+	awsToken := os.Getenv(envAWSToken)
+	awsRegion := os.Getenv(envAWSRegion)
+	queueUrl := os.Getenv(envAWSSQSQueue)
+
+	log.Printf("Target is: %q", target)
+
+	// create aws sqs client
+	sess := session.New(&aws.Config{
+		Region:      aws.String(awsRegion),
+		Credentials: credentials.NewStaticCredentials(awsId, awsKey, awsToken),
+	})
+	sqsClient := sqs.New(sess)
+
+	log.Printf("Start receiving messages")
+	ctx := context.Background()
+	cctx, cancel := context.WithCancel(ctx)
+	go func() {
+		for {
+			rcvParam := &sqs.ReceiveMessageInput{
+				QueueUrl:        aws.String(queueUrl),
+				WaitTimeSeconds: aws.Int64(waitTimeSecond),
+			}
+			resp, err := sqsClient.ReceiveMessageWithContext(cctx, rcvParam)
+			if err != nil {
+				log.Printf("failed to receive from queue %s:%v", queueUrl, err)
+				continue
+			}
+
+			log.Printf("received msg: %v", resp)
+
+			for _, message := range resp.Messages {
+				err := postMessage(target, []byte(*message.Body), message.Attributes)
+				if err != nil {
+					// retry
+					log.Printf("failed to post message %s:%v", err)
+					continue
+				}
+				// delete message
+				deleteParam := &sqs.DeleteMessageInput{
+					QueueUrl:      aws.String(queueUrl),
+					ReceiptHandle: message.ReceiptHandle,
+				}
+				_, err = sqsClient.DeleteMessage(deleteParam)
+				if err != nil {
+					log.Printf("failed to delete msg %v: %v", *message.MessageId, err)
+				}
+				log.Printf("Message %v has beed deleted", *message.MessageId)
+			}
+		}
+	}()
+	<-stopCh
+	cancel()
+	log.Printf("exiting")
+}
+
+func postMessage(target string, data []byte, attributes map[string]*string) error {
+	url := url.URL{
+		Scheme: "http",
+		Host:   target,
+		Path:   "/",
+	}
+	req, err := http.NewRequest(http.MethodPost, url.String(), bytes.NewReader(data))
+	if err != nil {
+		return fmt.Errorf("creating http request failed: %v", err)
+	}
+	req.Header = safeHeaders(attributesToHeaders(attributes))
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		return fmt.Errorf("sending http request failed: %v", err)
+	}
+	if res.StatusCode < 200 || res.StatusCode >= 300 {
+		return fmt.Errorf("%q did not accept event: got HTTP %d", target, res.StatusCode)
+	}
+	return nil
+}
+
+func attributesToHeaders(attributes map[string]*string) http.Header {
+	headers := http.Header{}
+	for name, value := range attributes {
+		headers.Set(name, *value)
+	}
+	return headers
+}
+
+func safeHeaders(raw http.Header) http.Header {
+	safe := http.Header{}
+	for _, header := range forwardHeaders {
+		if value := raw.Get(header); value != "" {
+			safe.Set(header, value)
+		}
+	}
+	return safe
+}