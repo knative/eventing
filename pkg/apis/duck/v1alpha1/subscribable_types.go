85710d4058a819a8323d12d434a28c5b56b42beb
Scott Andrews <sandrews@pivotal.io>
2018-11-02 11:45:46 -0400

    polish

--- pkg/apis/duck/v1alpha1/subscribable_types.go
+++ pkg/apis/duck/v1alpha1/subscribable_types.go
@@ -35,7 +35,7 @@ type Subscribable struct {
 // Ref is a reference to the Subscription this ChannelSubscriberSpec was created for
 // SubscriberURI is the endpoint for the subscriber
 // ReplyURI is the endpoint for the reply
-// At least one of SubscriberURI and ReplyURI them must be present
+// At least one of SubscriberURI and ReplyURI must be present
 type ChannelSubscriberSpec struct {
 	// +optional
 	Ref *corev1.ObjectReference `json:"ref,omitempty"`
@@ -71,7 +71,27 @@ func (s *Subscribable) GetFullType() duck.Populatable {
 func (c *Channel) Populate() {
 	c.Spec.Subscribable = &Subscribable{
 		// Populate ALL fields
-		Subscribers: []ChannelSubscriberSpec{{nil, "call1", "sink2"}, {nil, "call2", "sink2"}},
+		Subscribers: []ChannelSubscriberSpec{{
+			Ref: &corev1.ObjectReference{
+				APIVersion: "eventing.knative.dev/v1alpha1",
+				Kind:       "Subscription",
+				Name:       "subscription1",
+				Namespace:  "default",
+				UID:        "2f9b5e8e-deb6-11e8-9f32-f2801f1b9fd1",
+			},
+			SubscriberURI: "call1",
+			ReplyURI:      "sink2",
+		}, {
+			Ref: &corev1.ObjectReference{
+				APIVersion: "eventing.knative.dev/v1alpha1",
+				Kind:       "Subscription",
+				Name:       "subscription2",
+				Namespace:  "default",
+				UID:        "34c5aec8-deb6-11e8-9f32-f2801f1b9fd1",
+			},
+			SubscriberURI: "call2",
+			ReplyURI:      "sink2",
+		}},
 	}
 }
 
