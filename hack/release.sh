9fe2b04986c2374629028c6d1f921813457461c7
Adriano Cunha <35786489+adrcunha@users.noreply.github.com>
2018-11-07 12:41:36 -0800

    Update the release script (#584)
    * Update the release script
    
    The major change is the ability to publish a new release to GitHub automatically, just like serving or build do.
    
    Bonus: update documentation about `//hack`, and add documentation about `release.sh`.
    
    * Fix handling of repo
    
    KO_DOCKER_REPO is the canonical reference.
    
    * Fix {
    
    * Address PR feedback on doc

--- hack/release.sh
+++ hack/release.sh
@@ -24,17 +24,24 @@ readonly EVENTING_RELEASE_GCR
 
 # Yaml files to generate, and the source config dir for them.
 declare -A COMPONENTS
-COMPONENTS["eventing.yaml"]="config"
-COMPONENTS["in-memory-channel.yaml"]="config/provisioners/in-memory-channel"
+COMPONENTS=(
+  ["eventing.yaml"]="config"
+  ["in-memory-channel.yaml"]="config/provisioners/in-memory-channel"
+)
 readonly COMPONENTS
 
 declare -A RELEASES
-RELEASES["release.yaml"]="eventing.yaml;in-memory-channel.yaml"
+RELEASES=(
+  ["release.yaml"]="eventing.yaml in-memory-channel.yaml"
+)
 readonly RELEASES
+
+# Set the repository
+export KO_DOCKER_REPO=${EVENTING_RELEASE_GCR}
 
 # Script entry point.
 
-parse_flags $@
+initialize $@
 
 set -o errexit
 set -o pipefail
@@ -43,11 +50,8 @@ run_validation_tests ./test/presubmit-tests.sh
 
 banner "Building the release"
 
-# Set the repository
-export KO_DOCKER_REPO=${EVENTING_RELEASE_GCR}
-
+echo "- Destination GCR: ${KO_DOCKER_REPO}"
 if (( PUBLISH_RELEASE )); then
-  echo "- Destination GCR: ${EVENTING_RELEASE_GCR}"
   echo "- Destination GCS: ${EVENTING_RELEASE_GCS}"
 fi
 
@@ -59,19 +63,20 @@ for yaml in "${!COMPONENTS[@]}"; do
   config="${COMPONENTS[${yaml}]}"
   echo "Building Knative Eventing - ${config}"
   ko resolve ${KO_FLAGS} -f ${config}/ > ${yaml}
-  tag_images_in_yaml ${yaml} ${EVENTING_RELEASE_GCR} ${TAG}
+  tag_images_in_yaml ${yaml} ${KO_DOCKER_REPO} ${TAG}
   all_yamls+=(${yaml})
 done
 
 # Assemble the release
 for yaml in "${!RELEASES[@]}"; do
   echo "Assembling Knative Eventing - ${yaml}"
-  echo -n "" > ${yaml}
-  for component in $(echo ${RELEASES[${yaml}]} | tr ";" "\n"); do
+  touch ${yaml}
+  for component in ${RELEASES[${yaml}]}; do
     echo "---" >> ${yaml}
     echo "# ${component}" >> ${yaml}
     cat ${component} >> ${yaml}
   done
+  tag_images_in_yaml ${yaml} ${KO_DOCKER_REPO} ${TAG}
   all_yamls+=(${yaml})
 done
 
@@ -87,5 +92,7 @@ for yaml in ${all_yamls[@]}; do
   echo "Publishing ${yaml}"
   publish_yaml ${yaml} ${EVENTING_RELEASE_GCS} ${TAG}
 done
+
+branch_release "Knative Eventing" "${all_yamls[@]}"
 
 echo "New release published successfully"
