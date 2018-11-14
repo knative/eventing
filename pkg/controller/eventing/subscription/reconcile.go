31ad6b34858780d72c96a3ce2b0bd38d39108c1c
Sabari Kumar Murugesan <smurugesan@vmware.com>
2018-11-14 11:58:45 -0800

    Handle deleted channels

--- pkg/controller/eventing/subscription/reconcile.go
+++ pkg/controller/eventing/subscription/reconcile.go
@@ -292,7 +292,15 @@ func (r *reconciler) syncPhysicalChannel(sub *v1alpha1.Subscription, isDeleted b
 	}
 	subscribable := r.createSubscribable(subs)
 
-	return r.patchPhysicalFrom(sub.Namespace, sub.Spec.Channel, subscribable)
+	if patchErr := r.patchPhysicalFrom(sub.Namespace, sub.Spec.Channel, subscribable); patchErr != nil {
+		if isDeleted && errors.IsNotFound(patchErr) {
+			glog.Infof("could not find channel %v\n", sub.Spec.Channel)
+			return nil
+		} else {
+			return patchErr
+		}
+	}
+	return nil
 }
 
 func (r *reconciler) listAllSubscriptionsWithPhysicalChannel(sub *v1alpha1.Subscription) ([]v1alpha1.Subscription, error) {
