2e48e2b620710cf23651836cce4dd59cde1d5e1c
Adriano Cunha <35786489+adrcunha@users.noreply.github.com>
2019-01-28 16:05:36 -0800

    Update test-infra (#759)
    Update `e2e-tests.sh` and `release.sh` so they don't break.

--- vendor/github.com/knative/test-infra/scripts/library.sh
+++ vendor/github.com/knative/test-infra/scripts/library.sh
@@ -141,21 +141,26 @@ function wait_until_pods_running() {
   return 1
 }
 
-# Waits until the given service has an external IP address.
+# Waits until the given service has an external address (IP/hostname).
 # Parameters: $1 - namespace.
 #             $2 - service name.
 function wait_until_service_has_external_ip() {
-  echo -n "Waiting until service $2 in namespace $1 has an external IP"
+  echo -n "Waiting until service $2 in namespace $1 has an external address (IP/hostname)"
   for i in {1..150}; do  # timeout after 15 minutes
     local ip=$(kubectl get svc -n $1 $2 -o jsonpath="{.status.loadBalancer.ingress[0].ip}")
     if [[ -n "${ip}" ]]; then
       echo -e "\nService $2.$1 has IP $ip"
       return 0
     fi
+    local hostname=$(kubectl get svc -n $1 $2 -o jsonpath="{.status.loadBalancer.ingress[0].hostname}")
+    if [[ -n "${hostname}" ]]; then
+      echo -e "\nService $2.$1 has hostname $hostname"
+      return 0
+    fi
     echo -n "."
     sleep 6
   done
-  echo -e "\n\nERROR: timeout waiting for service $svc.$ns to have an external IP"
+  echo -e "\n\nERROR: timeout waiting for service $svc.$ns to have an external address"
   kubectl get pods -n $1
   return 1
 }
@@ -199,11 +204,15 @@ function get_app_pods() {
 # Parameters: $1 - user
 #             $2 - cluster name
 #             $3 - cluster region
+#             $4 - cluster zone, optional
 function acquire_cluster_admin_role() {
+  echo "Acquiring cluster-admin role for user '$1'"
+  local geoflag="--region=$3"
+  [[ -n $4 ]] && geoflag="--zone=$3-$4"
   # Get the password of the admin and use it, as the service account (or the user)
   # might not have the necessary permission.
   local password=$(gcloud --format="value(masterAuth.password)" \
-      container clusters describe $2 --region=$3)
+      container clusters describe $2 ${geoflag})
   if [[ -n "${password}" ]]; then
     # Cluster created with basic authentication
     kubectl config set-credentials cluster-admin \
@@ -213,9 +222,9 @@ function acquire_cluster_admin_role() {
     local key=$(mktemp)
     echo "Certificate in ${cert}, key in ${key}"
     gcloud --format="value(masterAuth.clientCertificate)" \
-      container clusters describe $2 --region=$3 | base64 -d > ${cert}
+      container clusters describe $2 ${geoflag} | base64 -d > ${cert}
     gcloud --format="value(masterAuth.clientKey)" \
-      container clusters describe $2 --region=$3 | base64 -d > ${key}
+      container clusters describe $2 ${geoflag} | base64 -d > ${key}
     kubectl config set-credentials cluster-admin \
       --client-certificate=${cert} --client-key=${key}
   fi
@@ -226,7 +235,7 @@ function acquire_cluster_admin_role() {
       --user=$1
   # Reset back to the default account
   gcloud container clusters get-credentials \
-      $2 --region=$3 --project $(gcloud config get-value project)
+      $2 ${geoflag} --project $(gcloud config get-value project)
 }
 
 # Runs a go test and generate a junit summary.
@@ -349,26 +358,44 @@ function run_lint_tool() {
 # Parameters: $1...$n - files to inspect
 function check_links_in_markdown() {
   # https://github.com/tcort/markdown-link-check
-  run_lint_tool markdown-link-check "checking links in markdown files" -q $@
+  local config="${REPO_ROOT_DIR}/test/markdown-link-check-config.json"
+  [[ ! -e ${config} ]] && config="${_TEST_INFRA_SCRIPTS_DIR}/markdown-link-check-config.json"
+  run_lint_tool markdown-link-check "checking links in markdown files" "-c ${config} -q" $@
 }
 
 # Check format of the given markdown files.
 # Parameters: $1..$n - files to inspect
 function lint_markdown() {
   # https://github.com/markdownlint/markdownlint
-  run_lint_tool mdl "linting markdown files" "-r ~MD013" $@
+  local config="${REPO_ROOT_DIR}/test/markdown-lint-config.rc"
+  [[ ! -e ${config} ]] && config="${_TEST_INFRA_SCRIPTS_DIR}/markdown-lint-config.rc"
+  run_lint_tool mdl "linting markdown files" "-c ${config}" $@
 }
 
-# Return 0 if the given parameter is an integer, otherwise 1
-# Parameters: $1 - an integer
+# Return whether the given parameter is an integer.
+# Parameters: $1 - integer to check
 function is_int() {
   [[ -n $1 && $1 =~ ^[0-9]+$ ]]
 }
 
-# Return 0 if the given parameter is the knative release/nightly gcr, 1
-# otherwise
-# Parameters: $1 - gcr name, e.g. gcr.io/knative-nightly
+# Return whether the given parameter is the knative release/nightly GCF.
+# Parameters: $1 - full GCR name, e.g. gcr.io/knative-foo-bar
 function is_protected_gcr() {
   [[ -n $1 && "$1" =~ "^gcr.io/knative-(releases|nightly)/?$" ]]
 }
 
+# Returns the canonical path of a filesystem object.
+# Parameters: $1 - path to return in canonical form
+#             $2 - base dir for relative links; optional, defaults to current
+function get_canonical_path() {
+  # We don't use readlink because it's not available on every platform.
+  local path=$1
+  local pwd=${2:-.}
+  [[ ${path} == /* ]] || path="${pwd}/${path}"
+  echo "$(cd ${path%/*} && echo $PWD/${path##*/})"
+}
+
+# Initializations that depend on previous functions.
+# These MUST come last.
+
+readonly _TEST_INFRA_SCRIPTS_DIR="$(dirname $(get_canonical_path ${BASH_SOURCE[0]}))"
