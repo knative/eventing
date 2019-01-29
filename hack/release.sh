ebed9e0181b868ff147b7d52780ac6c3500e3b15
Ville Aikas <vaikas@google.com>
2019-01-29 21:50:12 +0000

    uncomment release

--- hack/release.sh
+++ hack/release.sh
@@ -22,8 +22,7 @@ COMPONENTS=(
   ["eventing.yaml"]="config"
   ["in-memory-channel.yaml"]="config/provisioners/in-memory-channel"
   ["kafka.yaml"]="contrib/kafka/config"
-# uncomment when gcp-pubsub is ready to be published
-#  ["gcp-pubsub.yaml"]="contrib/gcppubsub/config"
+  ["gcp-pubsub.yaml"]="contrib/gcppubsub/config"
   ["natss.yaml"]="contrib/natss/config"
 )
 readonly COMPONENTS
