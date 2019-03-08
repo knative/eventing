6b0fbffb77c7dcabfad7fae2ab37faa12ddbcf99
Adam Harwayne <harwayne@google.com>
2019-03-08 12:32:34 -0800

    Move the logging util out of contrib, in preparation for use by other Eventing controllers using it.

--- contrib/gcppubsub/pkg/dispatcher/dispatcher/reconcile.go
+++ contrib/gcppubsub/pkg/dispatcher/dispatcher/reconcile.go
@@ -28,10 +28,10 @@ import (
 
 	ccpcontroller "github.com/knative/eventing/contrib/gcppubsub/pkg/controller/clusterchannelprovisioner"
 	pubsubutil "github.com/knative/eventing/contrib/gcppubsub/pkg/util"
-	"github.com/knative/eventing/contrib/gcppubsub/pkg/util/logging"
 	eventingv1alpha1 "github.com/knative/eventing/pkg/apis/eventing/v1alpha1"
 	"github.com/knative/eventing/pkg/provisioners"
 	util "github.com/knative/eventing/pkg/provisioners"
+	"github.com/knative/eventing/pkg/utils/logging"
 	"go.uber.org/zap"
 	k8serrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/types"
