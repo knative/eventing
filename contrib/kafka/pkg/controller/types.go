b7738a4ea3e3f68e7d36d4e5ad10d31371f33a0d
Dan Sun <dsun20@bloomberg.net>
2019-03-13 18:17:43 -0700

    support partition consumer mode for kafka channel (#879)
    * support partition consumer for kafka channel
    
    * consolidate GetProvisionerConfig function
    
    * add consumer mode config test
    
    * address review comments

--- contrib/kafka/pkg/controller/types.go
+++ contrib/kafka/pkg/controller/types.go
@@ -1,5 +1,8 @@
 package controller
 
+import "github.com/bsm/sarama-cluster"
+
 type KafkaProvisionerConfig struct {
-	Brokers []string
+	Brokers      []string
+	ConsumerMode cluster.ConsumerMode
 }
