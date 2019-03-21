441cc942390661cfec2ce9148fb15916a0044994
Scott Nichols <32305648+n3wscott@users.noreply.github.com>
2019-03-21 14:44:47 -0700

    Add context from webhook invocation. (#932)
    * Stage work to add context from webhook invocation.
    
    * updating deps.
    
    * Update deps.
    
    * Update lock on build.
    
    * Updated serving and build.
    
    * Ask for build directly.

--- pkg/apis/eventing/v1alpha1/channel_validation_test.go
+++ pkg/apis/eventing/v1alpha1/channel_validation_test.go
@@ -17,6 +17,7 @@ limitations under the License.
 package v1alpha1
 
 import (
+	"context"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
@@ -200,7 +201,7 @@ func TestChannelImmutableFields(t *testing.T) {
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			got := test.new.CheckImmutableFields(test.old)
+			got := test.new.CheckImmutableFields(context.TODO(), test.old)
 			if diff := cmp.Diff(test.want.Error(), got.Error()); diff != "" {
 				t.Errorf("Validate (-want, +got) = %v", diff)
 			}
