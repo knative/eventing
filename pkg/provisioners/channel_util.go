c646dcd49ab85272d84a605abfb1dd124e24a832
Akash Verenkar <verenkar@google.com>
2019-04-11 10:36:16 -0700

    Bug fix to set clusterIp of K8s service only when it is not of type ExternalName

--- pkg/provisioners/channel_util.go
+++ pkg/provisioners/channel_util.go
@@ -69,7 +69,7 @@ type k8sServiceOption func(*corev1.Service) error
 func ExternalService(c *eventingv1alpha1.Channel) k8sServiceOption {
 	return func(svc *corev1.Service) error {
 		svc.Spec = corev1.ServiceSpec{
-			Type:         "ExternalName",
+			Type:         corev1.ServiceTypeExternalName,
 			ExternalName: names.ServiceHostName(channelDispatcherServiceName(c.Spec.Provisioner.Name), system.Namespace()),
 		}
 		return nil
@@ -125,7 +125,9 @@ func createK8sService(ctx context.Context, client runtimeClient.Client, getSvc g
 	}
 	// spec.clusterIP is immutable and is set on existing services. If we don't set this
 	// to the same value, we will encounter an error while updating.
-	svc.Spec.ClusterIP = current.Spec.ClusterIP
+	if svc.Spec.Type != corev1.ServiceTypeExternalName {
+		svc.Spec.ClusterIP = current.Spec.ClusterIP
+	}
 	if !equality.Semantic.DeepDerivative(svc.Spec, current.Spec) ||
 		!expectedLabelsPresent(current.ObjectMeta.Labels, svc.ObjectMeta.Labels) ||
 		// This DeepEqual is necessary to force update dispatcher services when upgrading from 0.5 to 0.6.
