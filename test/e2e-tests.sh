a3cbd988687dbf2787ac8f7ce7d3e6e23519a5e0
Adriano Cunha <35786489+adrcunha@users.noreply.github.com>
2019-04-18 19:49:56 -0700

    Update test/e2e-tests.sh
    Co-Authored-By: Fredy-Z <chizhg@google.com>

--- test/e2e-tests.sh
+++ test/e2e-tests.sh
@@ -97,7 +97,7 @@ function gcppubsub_setup() {
       --member=serviceAccount:${PUBSUB_SERVICE_ACCOUNT}@${PROJECT_ID}.iam.gserviceaccount.com \
       --role roles/pubsub.editor
     gcloud iam service-accounts keys create ${PUBSUB_SERVICE_ACCOUNT_KEY} \
-    --iam-account=${PUBSUB_SERVICE_ACCOUNT}@${PROJECT_ID}.iam.gserviceaccount.com
+      --iam-account=${PUBSUB_SERVICE_ACCOUNT}@${PROJECT_ID}.iam.gserviceaccount.com
     service_account_key="${PUBSUB_SERVICE_ACCOUNT_KEY}"
   fi
 
