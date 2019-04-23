0ee6bfd461fe0f7e6c9ab57d6712ababfabdbe87
Scott Nichols <nicholss@google.com>
2019-04-23 09:06:34 -0700

    Move ContainerSource API.

--- pkg/client/clientset/versioned/clientset.go
+++ pkg/client/clientset/versioned/clientset.go
@@ -20,6 +20,7 @@ package versioned
 
 import (
 	eventingv1alpha1 "github.com/knative/eventing/pkg/client/clientset/versioned/typed/eventing/v1alpha1"
+	sourcesv1alpha1 "github.com/knative/eventing/pkg/client/clientset/versioned/typed/sources/v1alpha1"
 	discovery "k8s.io/client-go/discovery"
 	rest "k8s.io/client-go/rest"
 	flowcontrol "k8s.io/client-go/util/flowcontrol"
@@ -30,6 +31,9 @@ type Interface interface {
 	EventingV1alpha1() eventingv1alpha1.EventingV1alpha1Interface
 	// Deprecated: please explicitly pick a version if possible.
 	Eventing() eventingv1alpha1.EventingV1alpha1Interface
+	SourcesV1alpha1() sourcesv1alpha1.SourcesV1alpha1Interface
+	// Deprecated: please explicitly pick a version if possible.
+	Sources() sourcesv1alpha1.SourcesV1alpha1Interface
 }
 
 // Clientset contains the clients for groups. Each group has exactly one
@@ -37,6 +41,7 @@ type Interface interface {
 type Clientset struct {
 	*discovery.DiscoveryClient
 	eventingV1alpha1 *eventingv1alpha1.EventingV1alpha1Client
+	sourcesV1alpha1  *sourcesv1alpha1.SourcesV1alpha1Client
 }
 
 // EventingV1alpha1 retrieves the EventingV1alpha1Client
@@ -48,6 +53,17 @@ func (c *Clientset) EventingV1alpha1() eventingv1alpha1.EventingV1alpha1Interfac
 // Please explicitly pick a version.
 func (c *Clientset) Eventing() eventingv1alpha1.EventingV1alpha1Interface {
 	return c.eventingV1alpha1
+}
+
+// SourcesV1alpha1 retrieves the SourcesV1alpha1Client
+func (c *Clientset) SourcesV1alpha1() sourcesv1alpha1.SourcesV1alpha1Interface {
+	return c.sourcesV1alpha1
+}
+
+// Deprecated: Sources retrieves the default version of SourcesClient.
+// Please explicitly pick a version.
+func (c *Clientset) Sources() sourcesv1alpha1.SourcesV1alpha1Interface {
+	return c.sourcesV1alpha1
 }
 
 // Discovery retrieves the DiscoveryClient
@@ -70,6 +86,10 @@ func NewForConfig(c *rest.Config) (*Clientset, error) {
 	if err != nil {
 		return nil, err
 	}
+	cs.sourcesV1alpha1, err = sourcesv1alpha1.NewForConfig(&configShallowCopy)
+	if err != nil {
+		return nil, err
+	}
 
 	cs.DiscoveryClient, err = discovery.NewDiscoveryClientForConfig(&configShallowCopy)
 	if err != nil {
@@ -83,6 +103,7 @@ func NewForConfig(c *rest.Config) (*Clientset, error) {
 func NewForConfigOrDie(c *rest.Config) *Clientset {
 	var cs Clientset
 	cs.eventingV1alpha1 = eventingv1alpha1.NewForConfigOrDie(c)
+	cs.sourcesV1alpha1 = sourcesv1alpha1.NewForConfigOrDie(c)
 
 	cs.DiscoveryClient = discovery.NewDiscoveryClientForConfigOrDie(c)
 	return &cs
@@ -92,6 +113,7 @@ func NewForConfigOrDie(c *rest.Config) *Clientset {
 func New(c rest.Interface) *Clientset {
 	var cs Clientset
 	cs.eventingV1alpha1 = eventingv1alpha1.New(c)
+	cs.sourcesV1alpha1 = sourcesv1alpha1.New(c)
 
 	cs.DiscoveryClient = discovery.NewDiscoveryClient(c)
 	return &cs
