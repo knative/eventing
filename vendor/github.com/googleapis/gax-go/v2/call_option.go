f15eeba7ced1446debc1fedeeb8e22d6a738d48c
Adam Harwayne <harwayne@google.com>
2019-05-14 15:50:35 -0700

    Update cloud.google.com/go to 0.39.0.

--- vendor/github.com/googleapis/gax-go/v2/call_option.go
+++ vendor/github.com/googleapis/gax-go/v2/call_option.go
@@ -126,10 +126,11 @@ func (bo *Backoff) Pause() time.Duration {
 	if bo.Multiplier < 1 {
 		bo.Multiplier = 2
 	}
-	// Select a duration between zero and the current max. It might seem counterintuitive to
-	// have so much jitter, but https://www.awsarchitectureblog.com/2015/03/backoff.html
-	// argues that that is the best strategy.
-	d := time.Duration(rand.Int63n(int64(bo.cur)))
+	// Select a duration between 1ns and the current max. It might seem
+	// counterintuitive to have so much jitter, but
+	// https://www.awsarchitectureblog.com/2015/03/backoff.html argues that
+	// that is the best strategy.
+	d := time.Duration(1 + rand.Int63n(int64(bo.cur)))
 	bo.cur = time.Duration(float64(bo.cur) * bo.Multiplier)
 	if bo.cur > bo.Max {
 		bo.cur = bo.Max
