e043ae5613df8d3a971db7541a53588d0f8a2503
nachocano <nachoacano@gmail.com>
2019-05-23 10:01:32 -0700

    wip, not compiling

---
+++ contrib/kafka/cmd/channel_dispatcher/main1.go
@@ -0,0 +1,98 @@
+/*
+Copyright 2019 The Knative Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"flag"
+	"github.com/knative/eventing/contrib/kafka/pkg/utils"
+	"log"
+
+	"github.com/knative/eventing/contrib/kafka/pkg/controller"
+	"github.com/knative/eventing/contrib/kafka/pkg/dispatcher"
+	"github.com/knative/eventing/pkg/apis/eventing/v1alpha1"
+	"github.com/knative/eventing/pkg/channelwatcher"
+	"github.com/knative/eventing/pkg/tracing"
+	"github.com/knative/pkg/configmap"
+	"github.com/knative/pkg/signals"
+	"github.com/knative/pkg/system"
+	"go.uber.org/zap"
+	"k8s.io/client-go/kubernetes"
+	"sigs.k8s.io/controller-runtime/pkg/client/config"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+)
+
+// TODO update the dispatcher in follow up PR.
+func main() {
+	flag.Parse()
+	logger, err := zap.NewProduction()
+	if err != nil {
+		log.Fatalf("Unable to create logger: %v", err)
+	}
+	kafkaConfig, err := utils.GetKafkaConfig("/etc/config-kafka")
+	if err != nil {
+		logger.Fatal("Unable to load provisioner config", zap.Error(err))
+	}
+
+	mgr, err := manager.New(config.GetConfigOrDie(), manager.Options{})
+	if err != nil {
+		logger.Fatal("Unable to create manager.", zap.Error(err))
+	}
+
+	kafkaDispatcher, err := dispatcher.NewDispatcher(kafkaConfig.Brokers, kafkaConfig.ConsumerMode, logger)
+	if err != nil {
+		logger.Fatal("Unable to create kafka dispatcher", zap.Error(err))
+	}
+	if err = mgr.Add(kafkaDispatcher); err != nil {
+		logger.Fatal("Unable to add the kafka dispatcher", zap.Error(err))
+	}
+
+	if err := v1alpha1.AddToScheme(mgr.GetScheme()); err != nil {
+		logger.Fatal("Unable to add scheme for eventing apis.", zap.Error(err))
+	}
+
+	// Zipkin tracing.
+	kc := kubernetes.NewForConfigOrDie(mgr.GetConfig())
+	configMapWatcher := configmap.NewInformedWatcher(kc, system.Namespace())
+	if err = tracing.SetupDynamicZipkinPublishing(logger.Sugar(), configMapWatcher, "kafka-ch-dispatcher"); err != nil {
+		logger.Fatal("Error setting up Zipkin publishing", zap.Error(err))
+	}
+
+	if err = channelwatcher.New(mgr, logger, channelwatcher.UpdateConfigWatchHandler(kafkaDispatcher.UpdateConfig, shouldWatch)); err != nil {
+		logger.Fatal("Unable to create channel watcher.", zap.Error(err))
+	}
+
+	// set up signals so we handle the first shutdown signal gracefully
+	stopCh := signals.SetupSignalHandler()
+
+	// configMapWatcher does not block, so start it first.
+	if err = configMapWatcher.Start(stopCh); err != nil {
+		logger.Fatal("Failed to start ConfigMap watcher", zap.Error(err))
+	}
+
+	// Start blocks forever.
+	err = mgr.Start(stopCh)
+	if err != nil {
+		logger.Fatal("Manager.Start() returned an error", zap.Error(err))
+	}
+	logger.Info("Exiting...")
+}
+
+func shouldWatch(ch *v1alpha1.Channel) bool {
+	return ch.Spec.Provisioner != nil &&
+		ch.Spec.Provisioner.Namespace == "" &&
+		ch.Spec.Provisioner.Name == controller.Name
+}
