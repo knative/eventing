e053fe5b7852b95ce3144cfb4eaf8ee262f07f0a
Adam Harwayne <harwayne@google.com>
2019-05-29 11:22:10 -0700

    Add comments.

--- pkg/configmap/store.go
+++ pkg/configmap/store.go
@@ -5,13 +5,18 @@ import (
 	v1 "k8s.io/api/core/v1"
 )
 
+// TODO Move this to knative/pkg.
+
+// DefaultConstructors are something.
 type DefaultConstructors map[*v1.ConfigMap]interface{}
 
+// DefaultUntypedStore is an UntypedStore with default values for ConfigMaps that do not exist.
 type DefaultUntypedStore struct {
 	store      *configmap.UntypedStore
 	defaultCMs []v1.ConfigMap
 }
 
+// NewDefaultUntypedStore creates a new DefaultUntypedStore.
 func NewDefaultUntypedStore(
 	name string,
 	logger configmap.Logger,
@@ -29,6 +34,8 @@ func NewDefaultUntypedStore(
 	}
 }
 
+// WatchConfigs uses the provided configmap.DefaultingWatcher to setup watches for the config maps
+// provided in defaultCMs.
 func (s *DefaultUntypedStore) WatchConfigs(w configmap.DefaultingWatcher) {
 	for _, cm := range s.defaultCMs {
 		w.WatchWithDefault(cm, s.store.OnConfigChanged)
