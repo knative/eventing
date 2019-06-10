11f7fdf7f929a8d5ea3af2df8bb6a35866858291
Scott Nichols <nicholss@google.com>
2019-06-10 14:03:02 -0700

    update deps.

--- pkg/reconciler/broker/broker_test.go
+++ pkg/reconciler/broker/broker_test.go
@@ -1610,9 +1610,9 @@ func TestReconcileCRD(t *testing.T) {
 	}
 
 	defer logtesting.ClearAll()
-	table.Test(t, MakeFactory(func(listers *Listers, opt reconciler.Options) controller.Reconciler {
+	table.Test(t, MakeFactory(func(ctx context.Context, listers *Listers, cmw configmap.Watcher) controller.Reconciler {
 		return &Reconciler{
-			Base:                      reconciler.NewBase(opt, controllerAgentName),
+			Base:                      reconciler.NewBase(ctx, controllerAgentName, cmw),
 			subscriptionLister:        listers.GetSubscriptionLister(),
 			brokerLister:              listers.GetBrokerLister(),
 			channelLister:             listers.GetChannelLister(),
