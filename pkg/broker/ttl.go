48bb3d3c77789f4878b60c71f3951e1c7ad31034
Mike Helmick <helmick@google.com>
2019-06-24 10:20:14 -0700

    Treat the broker TTL attribute as case insensitive, in line with how cloudevents attributes should be handled. See   https://github.com/cloudevents/spec/pull/321 for context.

--- pkg/broker/ttl.go
+++ pkg/broker/ttl.go
@@ -17,16 +17,28 @@
 package broker
 
 import (
+	"strings"
+
 	cloudevents "github.com/cloudevents/sdk-go"
 )
 
 const (
 	// V02TTLAttribute is the name of the CloudEvents 0.2 extension attribute used to store the
 	// Broker's TTL (number of times a single event can reply through a Broker continuously).
-	// Extensions should be formatted as described in net/textproto.CanonicalMIMEHeaderKey, as
-	// they may have their case changed in transit.
-	V02TTLAttribute = "Knativebrokerttl"
+	V02TTLAttribute = "knativebrokerttl"
 )
+
+// GetTTL finds the TTL in the EventContext using a case insensitive comparison
+// for the key. The second return param, is the case preserved key that matched.
+// Depending on the encoding/transport, the extension case could be changed.
+func GetTTL(ctx cloudevents.EventContext) (interface{}, string) {
+	for k, v := range ctx.AsV02().Extensions {
+		if lower := strings.ToLower(k); lower == V02TTLAttribute {
+			return v, k
+		}
+	}
+	return nil, V02TTLAttribute
+}
 
 // SetTTL sets the TTL into the EventContext. ttl should be a positive integer.
 func SetTTL(ctx cloudevents.EventContext, ttl interface{}) (cloudevents.EventContext, error) {
