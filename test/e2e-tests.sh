2002acddd31db57244283bce6a0a7384392aa4c1
Chi Zhang <chizhg@google.com>
2019-06-23 18:42:13 -0700

    test

--- test/e2e-tests.sh
+++ test/e2e-tests.sh
@@ -28,12 +28,12 @@ source $(dirname $0)/e2e-common.sh
 
 # Override the default install_test_resources function by only installing Channel CRDs
 function install_test_resources() {
-  install_channel_crds() || return 1
+  install_channel_crds || return 1
 }
 
 # Override the default uninstall_test_resources function by only uninstalling Channel CRDs
 function uninstall_test_resources() {
-  uninstall_channel_crds()
+  uninstall_channel_crds
 }
 
 initialize $@ --skip-istio-addon
