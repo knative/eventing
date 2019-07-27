1c887318e624b7379c908e92fa20df92de41572f
montaro <a.hamouda@sap.com>
2019-07-27 23:03:34 +0200

    Move contrib/natss to knative/eventing-contrib
    Remove NATS config
    
    Remove NATS from tests
    
    Update update-codegen.sh

--- test/common/client.go
+++ test/common/client.go
@@ -22,7 +22,6 @@ package common
 import (
 	"testing"
 
-	natsschannel "github.com/knative/eventing/contrib/natss/pkg/client/clientset/versioned"
 	eventing "github.com/knative/eventing/pkg/client/clientset/versioned"
 	"k8s.io/client-go/dynamic"
 	"knative.dev/pkg/test"
@@ -33,7 +32,6 @@ type Client struct {
 	Kube     *test.KubeClient
 	Eventing *eventing.Clientset
 	Dynamic  dynamic.Interface
-	Natss    *natsschannel.Clientset
 
 	Namespace string
 	T         *testing.T
@@ -59,11 +57,6 @@ func NewClient(configPath string, clusterName string, namespace string, t *testi
 	}
 
 	client.Dynamic, err = dynamic.NewForConfig(cfg)
-	if err != nil {
-		return nil, err
-	}
-
-	client.Natss, err = natsschannel.NewForConfig(cfg)
 	if err != nil {
 		return nil, err
 	}
