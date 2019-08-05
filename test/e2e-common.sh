7eaf9d590c0823f83fbcc211a40fba01455c34a5
Chi Zhang <chizhg@google.com>
2019-08-05 15:28:55 -0700

    Make e2e-tests.sh work on MAC (#1641)
    * make sed work on MAC
    
    * remove elif

--- test/e2e-common.sh
+++ test/e2e-common.sh
@@ -21,6 +21,11 @@ source $(dirname $0)/../vendor/knative.dev/test-infra/scripts/e2e-tests.sh
 # If gcloud is not available make it a no-op, not an error.
 which gcloud &> /dev/null || gcloud() { echo "[ignore-gcloud $*]" 1>&2; }
 
+# Let sed work on mac.
+if [ "$(uname)" == "Darwin" ]; then
+  sed=gsed
+fi
+
 # Eventing main config.
 readonly EVENTING_CONFIG="config/"
 
