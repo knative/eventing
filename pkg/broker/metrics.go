3ca08bc9ed9f69b304558beb4f814e3356110650
grac3gao <52978759+grac3gao@users.noreply.github.com>
2019-08-28 17:15:46 -0700

    Update pkg/broker/metrics.go
    Co-Authored-By: Adam Harwayne <harwayne@google.com>

--- pkg/broker/metrics.go
+++ pkg/broker/metrics.go
@@ -27,7 +27,7 @@ const (
 	// TraceParent is a documented extension for CloudEvent to include traces.
 	// https://github.com/cloudevents/spec/blob/v0.3/extensions/distributed-tracing.md#traceparent
 	// The format is: 00-4bf92f3577b34da6a3ce929d0e0e4736-00f067aa0ba902b7-01,
-	// which stands for version(00" is the current version)-traceID-spanID-trace options
+	// which stands for version("00" is the current version)-traceID-spanID-trace options
 	TraceParent = "traceparent"
 )
 
