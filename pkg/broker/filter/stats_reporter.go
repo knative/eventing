f73ddce5c16bae200117fb315ad51ee3380c1576
Sayan Hazra <sayan.hazra@sap.com>
2019-09-03 21:57:44 +0200

    Changes based on PR review

--- pkg/broker/filter/stats_reporter.go
+++ pkg/broker/filter/stats_reporter.go
@@ -190,6 +190,7 @@ func (r *reporter) ReportEventProcessingTime(args *ReportArgs, d time.Duration) 
 	if err != nil {
 		return err
 	}
+
 	// convert time.Duration in nanoseconds to milliseconds.
 	metrics.Record(ctx, processingTimeInMsecM.M(float64(d/time.Millisecond)))
 	return nil
