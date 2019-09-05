3dced9aa4fd07edc3147b0f1860827f4d6d8da41
Alek Slominski <aslom@us.ibm.com>
2019-09-05 17:27:27 -0400

    remove commented out code

--- test/conformance/alias.go
+++ test/conformance/alias.go
@@ -18,7 +18,6 @@ package conformance
 import "knative.dev/eventing/test/common"
 import "knative.dev/eventing/test"
 
-//var runTests = common.RunTests
 var setup = common.Setup
 var tearDown = common.TearDown
 var getChannelTypeMeta = common.GetChannelTypeMeta
