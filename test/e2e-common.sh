fd4e160296cdc0b2cc96d97a92d025642aa03492
Adam Harwayne <harwayne@google.com>
2019-09-18 12:19:40 -0700

    Switch to KNATIVE_MONITORING_RELEASE.

--- test/e2e-common.sh
+++ test/e2e-common.sh
@@ -57,7 +57,7 @@ END
 
   echo "Installing Knative Monitoring"
   kubectl create namespace istio-system
-  kubectl apply --filename https://github.com/knative/serving/releases/download/v0.8.0/monitoring-tracing-zipkin-in-mem.yaml || return 1
+  kubectl apply --filename "${KNATIVE_MONITORING_RELEASE}" || return 1
   wait_until_pods_running istio-system || fail_test "Knative Monitoring did not come up"
 }
 
