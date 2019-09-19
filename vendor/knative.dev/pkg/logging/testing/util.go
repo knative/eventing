94e691a6311d24ba49f188ab7ec08760ff6dae54
Tara Gu <guw@ibm.com>
2019-09-19 10:57:39 -0400

    Remove changes in vendor pkg

--- vendor/knative.dev/pkg/logging/testing/util.go
+++ vendor/knative.dev/pkg/logging/testing/util.go
@@ -18,6 +18,7 @@ package testing
 
 import (
 	"context"
+	"sync"
 	"testing"
 
 	"go.uber.org/zap"
@@ -26,14 +27,39 @@ import (
 	"knative.dev/pkg/logging"
 )
 
+var (
+	loggers = make(map[string]*zap.SugaredLogger)
+	m       sync.Mutex
+)
+
 // TestLogger gets a logger to use in unit and end to end tests
 func TestLogger(t *testing.T) *zap.SugaredLogger {
+	m.Lock()
+	defer m.Unlock()
+
+	logger, ok := loggers[t.Name()]
+
+	if ok {
+		return logger
+	}
+
 	opts := zaptest.WrapOptions(
 		zap.AddCaller(),
 		zap.Development(),
 	)
 
-	return zaptest.NewLogger(t, opts).Sugar()
+	logger = zaptest.NewLogger(t, opts).Sugar().Named(t.Name())
+	loggers[t.Name()] = logger
+
+	return logger
+}
+
+// ClearAll removes all the testing loggers.
+// `go test -count=X` executes runs in the same process, thus the map
+// persists between the runs, but the `t` will no longer be valid and will
+// cause a panic deep inside testing code.
+func ClearAll() {
+	loggers = make(map[string]*zap.SugaredLogger)
 }
 
 // TestContextWithLogger returns a context with a logger to be used in tests
