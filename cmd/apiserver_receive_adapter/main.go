83e01a5b491df29de273000d7f31e811c226c9e9
Matt Moore <mattmoor@google.com>
2019-09-23 16:05:15 -0700

    Ensure all of the Eventing binaries expose the new Go memstats. (#1946)
    ```release-note
    Expose Go's runtime.MemStats as opencensus metrics.
    ```
    
    See: https://github.com/knative/pkg/pull/711

--- cmd/apiserver_receive_adapter/main.go
+++ cmd/apiserver_receive_adapter/main.go
@@ -17,14 +17,18 @@ limitations under the License.
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
+	"log"
 	"strings"
+	"time"
 
 	// Uncomment the following line to load the gcp plugin
 	// (only required to authenticate against GKE clusters).
 	// _ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 	"github.com/kelseyhightower/envconfig"
+	"go.opencensus.io/stats/view"
 	"go.uber.org/zap"
 	"k8s.io/apimachinery/pkg/api/meta"
 	"k8s.io/apimachinery/pkg/runtime/schema"
@@ -91,6 +95,14 @@ func main() {
 	if err != nil {
 		panic(fmt.Sprintf("Error processing env var: %s", err))
 	}
+
+	// Report stats on Go memory usage every 30 seconds.
+	msp := metrics.NewMemStatsAll()
+	msp.Start(context.Background(), 30*time.Second)
+	if err := view.Register(msp.DefaultViews()...); err != nil {
+		log.Fatalf("Error exporting go memstats view: %v", err)
+	}
+
 	// Convert json logging.Config to logging.Config.
 	loggingConfig, err := logging.JsonToLoggingConfig(env.LoggingConfigJson)
 	if err != nil {
