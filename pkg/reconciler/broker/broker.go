b1dd55ba039ad0937f3a19d8357b452fbbb5638c
mattmoor-sockpuppet <mattmoor+sockpuppet@google.com>
2019-10-23 14:12:36 -0700

    golang format tools (#2080)
    Produced via:
      `gofmt -s -w $(find -path './vendor' -prune -o -type f -name '*.go' -print))`
      `goimports -w $(find -name '*.go' | grep -v vendor)`
    /assign n3wscott

--- pkg/reconciler/broker/broker.go
+++ pkg/reconciler/broker/broker.go
@@ -23,7 +23,7 @@ import (
 	"time"
 
 	"go.uber.org/zap"
-	"k8s.io/api/apps/v1"
+	v1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/equality"
 	apierrs "k8s.io/apimachinery/pkg/api/errors"
