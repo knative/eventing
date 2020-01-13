e13bf3fb75b75b18d81813e680588bb15bda5b5a
Chris Suszyński <ksuszyns@redhat.com>
2020-01-13 14:29:15 +0100

    Delete non required Istio files

--- third_party/istio-1.4.3/download-istio.sh
+++ third_party/istio-1.4.3/download-istio.sh
@@ -35,25 +35,6 @@ helm template --namespace=istio-system \
   | sed 's/[ \t]*$//' \
   > ../istio-crds.yaml
 
-# Create a custom cluster local gateway, based on the Istio custom-gateway template.
-helm template --namespace=istio-system install/kubernetes/helm/istio --values ../values-extras.yaml \
-  `# Removing trailing whitespaces to make automation happy` \
-  | sed 's/[ \t]*$//' \
-  > ../istio-knative-extras.yaml
-
-# A template with sidecar injection enabled.
-helm template --namespace=istio-system install/kubernetes/helm/istio --values ../values.yaml \
-  `# Removing trailing whitespaces to make automation happy` \
-  | sed 's/[ \t]*$//' \
-  > ../istio-ci-mesh.yaml
-
-# A lighter template, with just pilot/gateway.
-# Based on install/kubernetes/helm/istio/values-istio-minimal.yaml
-helm template --namespace=istio-system install/kubernetes/helm/istio --values ../values-lean.yaml \
-  `# Removing trailing whitespaces to make automation happy` \
-  | sed 's/[ \t]*$//' \
-  > ../istio-ci-no-mesh.yaml
-
 # An even lighter template, with just pilot/gateway and small resource requests.
 # Based on install/kubernetes/helm/istio/values-istio-minimal.yaml
 helm template --namespace=istio-system install/kubernetes/helm/istio --values ../values-local.yaml \
@@ -68,9 +49,4 @@ rm istio-${ISTIO_VERSION}-linux.tar.gz
 
 # Add in the `istio-system` namespace to reduce number of commands.
 patch istio-crds.yaml namespace.yaml.patch
-patch istio-ci-mesh.yaml namespace.yaml.patch
-patch istio-ci-no-mesh.yaml namespace.yaml.patch
 patch istio-minimal.yaml namespace.yaml.patch
-
-# Increase termination drain duration seconds.
-patch -l istio-ci-mesh.yaml drain-seconds.yaml.patch
