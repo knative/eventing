f6d7da1cc9c62fdf62d0fdb0f8654e8035bc0715
Chen Shou <cshou@google.com>
2020-01-27 21:40:56 +0000

    add unit tests for broker resources

---
+++ pkg/reconciler/broker/resources/filter_test.go
@@ -0,0 +1,130 @@
+/*
+Copyright 2020 The Knative Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package resources
+
+import (
+	"os"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	eventingv1alpha1 "knative.dev/eventing/pkg/apis/eventing/v1alpha1"
+	"knative.dev/eventing/pkg/reconciler/service"
+)
+
+func TestMakeFilterServiceArgs(t *testing.T) {
+	_ = os.Setenv("SYSTEM_NAMESPACE", "my-system-ns")
+	b := &eventingv1alpha1.Broker{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "default",
+			Namespace: "my-ns",
+		},
+	}
+	want := &service.Args{
+		ServiceMeta: metav1.ObjectMeta{
+			Namespace: "my-ns",
+			Name:      "default-broker-filter",
+			Labels: map[string]string{
+				"eventing.knative.dev/broker":     "default",
+				"eventing.knative.dev/brokerRole": "filter",
+			},
+		},
+		DeployMeta: metav1.ObjectMeta{
+			Namespace: "my-ns",
+			Name:      "default-broker-filter",
+			Labels: map[string]string{
+				"eventing.knative.dev/broker":     "default",
+				"eventing.knative.dev/brokerRole": "filter",
+			},
+		},
+		PodSpec: corev1.PodSpec{
+			ServiceAccountName: "my-serviceaccount",
+			Containers: []corev1.Container{
+				{
+					Name:  "filter",
+					Image: "image.example.com/filter",
+					LivenessProbe: &corev1.Probe{
+						Handler: corev1.Handler{
+							HTTPGet: &corev1.HTTPGetAction{
+								Path: "/healthz",
+							},
+						},
+						InitialDelaySeconds: 5,
+						PeriodSeconds:       2,
+						FailureThreshold:    3,
+						TimeoutSeconds:      10,
+						SuccessThreshold:    1,
+					},
+					ReadinessProbe: &corev1.Probe{
+						Handler: corev1.Handler{
+							HTTPGet: &corev1.HTTPGetAction{
+								Path: "/readyz",
+							},
+						},
+						InitialDelaySeconds: 5,
+						PeriodSeconds:       2,
+						FailureThreshold:    3,
+						TimeoutSeconds:      10,
+						SuccessThreshold:    1,
+					},
+					Env: []corev1.EnvVar{
+						{
+							Name:  "SYSTEM_NAMESPACE",
+							Value: "my-system-ns",
+						},
+						{
+							Name:  "NAMESPACE",
+							Value: "my-ns",
+						},
+						{
+							Name:  "POD_NAME",
+							Value: "default-broker-filter",
+						},
+						{
+							Name:  "CONTAINER_NAME",
+							Value: "filter",
+						},
+						{
+							Name:  "BROKER",
+							Value: "default",
+						},
+						{
+							Name:  "METRICS_DOMAIN",
+							Value: "knative.dev/internal/eventing",
+						},
+					},
+					Ports: []corev1.ContainerPort{
+						{
+							ContainerPort: 8080,
+						},
+					},
+				},
+			},
+		},
+	}
+
+	got := MakeFilterServiceArgs(&FilterArgs{
+		Broker:             b,
+		Image:              "image.example.com/filter",
+		ServiceAccountName: "my-serviceaccount",
+	})
+
+	if diff := cmp.Diff(want, got); diff != "" {
+		t.Errorf("Unexpected filter service arguments (-want, +got): %s", diff)
+	}
+}
