0a14f717ab4f6a5d29a59b86a670e83f6a82c265
Ville Aikas <11279988+vaikas@users.noreply.github.com>
2020-02-20 18:52:06 -0800

    add conversions + webhook for inmemorychannels (#2621)

--- pkg/apis/messaging/v1alpha1/zz_generated.deepcopy.go
+++ pkg/apis/messaging/v1alpha1/zz_generated.deepcopy.go
@@ -214,6 +214,11 @@ func (in *InMemoryChannelSpec) DeepCopyInto(out *InMemoryChannelSpec) {
 		*out = new(duckv1alpha1.Subscribable)
 		(*in).DeepCopyInto(*out)
 	}
+	if in.Delivery != nil {
+		in, out := &in.Delivery, &out.Delivery
+		*out = new(v1beta1.DeliverySpec)
+		(*in).DeepCopyInto(*out)
+	}
 	return
 }
 
