a0221c2d8d05e8b159d958c4d609d6358e2c5dda
Chi Zhang <chizhg@google.com>
2020-02-24 15:47:41 -0800

    no wait in ko delete

--- test/performance/performance-tests.sh
+++ test/performance/performance-tests.sh
@@ -53,7 +53,7 @@ function update_benchmark() {
   kubectl patch configmap config-mako -n "${TEST_NAMESPACE}" -p '{"data":{"environment":"prod"}}' || abort "failed to patch config-mako configmap"
 
   echo ">> Updating benchmark $1"
-  ko delete -f "${benchmark_path}"/${TEST_CONFIG_VARIANT} --ignore-not-found=true
+  ko delete -f "${benchmark_path}"/${TEST_CONFIG_VARIANT} --ignore-not-found=true --wait=false
   ko apply -f "${benchmark_path}"/${TEST_CONFIG_VARIANT} || abort "failed to apply benchmark $1"
 }
 
