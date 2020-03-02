e6b6d1c21a15e63569708007d0e79f590d2bcc60
Ville Aikas <vaikas@vmware.com>
2020-03-02 13:16:13 -0800

    undo the conversion to v1alpha1

--- pkg/reconciler/subscription/subscription.go
+++ pkg/reconciler/subscription/subscription.go
@@ -26,7 +26,6 @@ import (
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/types"
 
 	"knative.dev/pkg/apis/duck"
@@ -349,20 +348,9 @@ func (r *Reconciler) trackAndFetchChannel(ctx context.Context, sub *v1alpha1.Sub
 func (r *Reconciler) getChannel(ctx context.Context, sub *v1alpha1.Subscription) (*eventingduckv1alpha1.ChannelableCombined, pkgreconciler.Event) {
 	logging.FromContext(ctx).Warn("GETTING channel", zap.Any("channel", sub.Spec.Channel))
 
-	// let's use the v1alpha1 API for now, until we migrate this reconciler
-	// to work directly on v1beta1 object
-	channel := sub.Spec.Channel
-	if channel.GroupVersionKind().Version == "v1beta1" {
-		channel.SetGroupVersionKind(schema.GroupVersionKind{
-			Group:   channel.GroupVersionKind().Group,
-			Version: "v1alpha1",
-			Kind:    channel.GroupVersionKind().Kind,
-		})
-	}
-
 	// 1. Track the channel pointed by subscription.
 	//   a. If channel is a Channel.messaging.knative.dev
-	obj, err := r.trackAndFetchChannel(ctx, sub, channel)
+	obj, err := r.trackAndFetchChannel(ctx, sub, sub.Spec.channel)
 	if err != nil {
 		return nil, err
 	}
