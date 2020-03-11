032a8824251b8ac1ca274c58646af4eb40ef082d
Ville Aikas <vaikas@vmware.com>
2020-03-11 10:43:25 -0700

    Demo class based filters for 2737

--- pkg/reconciler/broker/controller.go
+++ pkg/reconciler/broker/controller.go
@@ -91,7 +91,7 @@ func NewController(
 		filterServiceAccountName:  env.FilterServiceAccount,
 		brokerClass:               env.BrokerClass,
 	}
-	impl := brokerreconciler.NewImpl(ctx, r)
+	impl := brokerreconciler.NewImpl(ctx, r, env.BrokerClass)
 
 	r.Logger.Info("Setting up event handlers")
 
@@ -99,6 +99,8 @@ func NewController(
 	r.channelableTracker = duck.NewListableTracker(ctx, channelable.Get, impl.EnqueueKey, controller.GetTrackerLease(ctx))
 	r.addressableTracker = duck.NewListableTracker(ctx, addressable.Get, impl.EnqueueKey, controller.GetTrackerLease(ctx))
 	r.uriResolver = resolver.NewURIResolver(ctx, impl.EnqueueKey)
+
+	classFilter := pkgreconciler.AnnotationFilterFunc(eventing.ChannelBrokerClassValue, env.BrokerClass, true)
 
 	brokerInformer.Informer().AddEventHandler(controller.HandleAll(impl.Enqueue))
 
