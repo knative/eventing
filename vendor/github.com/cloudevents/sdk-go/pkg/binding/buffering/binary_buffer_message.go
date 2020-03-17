cd8c43c32f900169b54fa0843aa424c67473d310
slinkydeveloper <francescoguard@gmail.com>
2020-03-17 17:43:10 +0100

    Updated deps
    Signed-off-by: Francesco Guardiani <francescoguard@gmail.com>

---
+++ vendor/github.com/cloudevents/sdk-go/pkg/binding/buffering/binary_buffer_message.go
@@ -0,0 +1,106 @@
+package buffering
+
+import (
+	"bytes"
+	"context"
+	"io"
+
+	"github.com/valyala/bytebufferpool"
+
+	"github.com/cloudevents/sdk-go/pkg/binding"
+	"github.com/cloudevents/sdk-go/pkg/binding/spec"
+)
+
+var binaryMessagePool bytebufferpool.Pool
+
+// binaryBufferedMessage implements a binary-mode message as a simple struct.
+// This message implementation is used by CopyMessage and BufferMessage
+type binaryBufferedMessage struct {
+	metadata   map[spec.Attribute]interface{}
+	extensions map[string]interface{}
+	body       *bytebufferpool.ByteBuffer
+}
+
+func (m *binaryBufferedMessage) Start(ctx context.Context) error {
+	m.metadata = make(map[spec.Attribute]interface{}, 4)
+	m.extensions = make(map[string]interface{})
+	return nil
+}
+
+func (m *binaryBufferedMessage) End(ctx context.Context) error {
+	return nil
+}
+
+func (m *binaryBufferedMessage) GetParent() binding.Message {
+	return nil
+}
+
+func (m *binaryBufferedMessage) ReadEncoding() binding.Encoding {
+	return binding.EncodingBinary
+}
+
+func (m *binaryBufferedMessage) ReadStructured(context.Context, binding.StructuredWriter) error {
+	return binding.ErrNotStructured
+}
+
+func (m *binaryBufferedMessage) ReadBinary(ctx context.Context, b binding.BinaryWriter) (err error) {
+	err = b.Start(ctx)
+	if err != nil {
+		return
+	}
+	for k, v := range m.metadata {
+		err = b.SetAttribute(k, v)
+		if err != nil {
+			return
+		}
+	}
+	for k, v := range m.extensions {
+		err = b.SetExtension(k, v)
+		if err != nil {
+			return
+		}
+	}
+	if m.body != nil {
+		err = b.SetData(bytes.NewReader(m.body.Bytes()))
+		if err != nil {
+			return
+		}
+	}
+	return b.End(ctx)
+}
+
+func (b *binaryBufferedMessage) Finish(error) error {
+	if b.body != nil {
+		binaryMessagePool.Put(b.body)
+	}
+	return nil
+}
+
+// Binary Encoder
+func (b *binaryBufferedMessage) SetData(data io.Reader) error {
+	buf := binaryMessagePool.Get()
+	w, err := io.Copy(buf, data)
+	if err != nil {
+		return err
+	}
+	if w == 0 {
+		binaryMessagePool.Put(buf)
+		return nil
+	}
+	b.body = buf
+	return nil
+}
+
+func (b *binaryBufferedMessage) SetAttribute(attribute spec.Attribute, value interface{}) error {
+	// If spec version we need to change to right context struct
+	b.metadata[attribute] = value
+	return nil
+}
+
+func (b *binaryBufferedMessage) SetExtension(name string, value interface{}) error {
+	b.extensions[name] = value
+	return nil
+}
+
+var _ binding.Message = (*binaryBufferedMessage)(nil) // Test it conforms to the interface
+var _ binding.BinaryWriter = (*binaryBufferedMessage)(nil)
---
+++ vendor/github.com/cloudevents/sdk-go/pkg/protocol/http/write_request.go
@@ -0,0 +1,78 @@
+package http
+
+import (
+	"context"
+	"io"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/cloudevents/sdk-go/pkg/binding"
+	"github.com/cloudevents/sdk-go/pkg/binding/format"
+	"github.com/cloudevents/sdk-go/pkg/binding/spec"
+	"github.com/cloudevents/sdk-go/pkg/types"
+)
+
+// Fill the provided httpRequest with the message m.
+// Using context you can tweak the encoding processing (more details on binding.Write documentation).
+func WriteRequest(ctx context.Context, m binding.Message, httpRequest *http.Request, transformers ...binding.TransformerFactory) error {
+	structuredWriter := (*httpRequestWriter)(httpRequest)
+	binaryWriter := (*httpRequestWriter)(httpRequest)
+
+	_, err := binding.Write(
+		ctx,
+		m,
+		structuredWriter,
+		binaryWriter,
+		transformers...,
+	)
+	return err
+}
+
+type httpRequestWriter http.Request
+
+func (b *httpRequestWriter) SetStructuredEvent(ctx context.Context, format format.Format, event io.Reader) error {
+	b.Header.Set(ContentType, format.MediaType())
+	b.Body = ioutil.NopCloser(event)
+	return nil
+}
+
+func (b *httpRequestWriter) Start(ctx context.Context) error {
+	return nil
+}
+
+func (b *httpRequestWriter) End(ctx context.Context) error {
+	return nil
+}
+
+func (b *httpRequestWriter) SetData(reader io.Reader) error {
+	b.Body = ioutil.NopCloser(reader)
+	return nil
+}
+
+func (b *httpRequestWriter) SetAttribute(attribute spec.Attribute, value interface{}) error {
+	// Http headers, everything is a string!
+	s, err := types.Format(value)
+	if err != nil {
+		return err
+	}
+
+	if attribute.Kind() == spec.DataContentType {
+		b.Header.Add(ContentType, s)
+	} else {
+		b.Header.Add(prefix+attribute.Name(), s)
+	}
+	return nil
+}
+
+func (b *httpRequestWriter) SetExtension(name string, value interface{}) error {
+	// Http headers, everything is a string!
+	s, err := types.Format(value)
+	if err != nil {
+		return err
+	}
+	b.Header.Add(prefix+name, s)
+	return nil
+}
+
+var _ binding.StructuredWriter = (*httpRequestWriter)(nil) // Test it conforms to the interface
+var _ binding.BinaryWriter = (*httpRequestWriter)(nil)     // Test it conforms to the interface
