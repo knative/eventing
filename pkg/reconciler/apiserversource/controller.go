a0e635064ad2cb9f9c3e28b3a556c2e64a52501b
Scott Nichols <32305648+n3wscott@users.noreply.github.com>
2020-03-24 10:41:09 -0700

    migrate apiserversource off reconciler.Base (#2834)

--- pkg/reconciler/apiserversource/controller.go
+++ pkg/reconciler/apiserversource/controller.go
@@ -21,16 +21,17 @@ import (
 
 	"github.com/kelseyhightower/envconfig"
 	"k8s.io/client-go/tools/cache"
-	"knative.dev/eventing/pkg/apis/sources/v1alpha1"
-	"knative.dev/eventing/pkg/reconciler"
 	"knative.dev/pkg/configmap"
 	"knative.dev/pkg/controller"
 	"knative.dev/pkg/logging"
 	"knative.dev/pkg/metrics"
 	"knative.dev/pkg/resolver"
 
+	"knative.dev/eventing/pkg/apis/sources/v1alpha1"
+
 	apiserversourceinformer "knative.dev/eventing/pkg/client/injection/informers/sources/v1alpha1/apiserversource"
 	apiserversourcereconciler "knative.dev/eventing/pkg/client/injection/reconciler/sources/v1alpha1/apiserversource"
+	kubeclient "knative.dev/pkg/client/injection/kube/client"
 	deploymentinformer "knative.dev/pkg/client/injection/kube/informers/apps/v1/deployment"
 )
 
@@ -58,7 +59,7 @@ func NewController(
 	apiServerSourceInformer := apiserversourceinformer.Get(ctx)
 
 	r := &Reconciler{
-		Base:                  reconciler.NewBase(ctx, controllerAgentName, cmw),
+		kubeClientSet:         kubeclient.Get(ctx),
 		apiserversourceLister: apiServerSourceInformer.Lister(),
 		source:                GetCfgHost(ctx),
 		loggingContext:        ctx,
@@ -66,7 +67,7 @@ func NewController(
 
 	env := &envConfig{}
 	if err := envconfig.Process("", env); err != nil {
-		r.Logger.Panicf("unable to process APIServerSource's required environment variables: %v", err)
+		logging.FromContext(ctx).Panicf("unable to process APIServerSource's required environment variables: %v", err)
 	}
 	r.receiveAdapterImage = env.Image
 
@@ -74,7 +75,7 @@ func NewController(
 
 	r.sinkResolver = resolver.NewURIResolver(ctx, impl.EnqueueKey)
 
-	r.Logger.Info("Setting up event handlers")
+	logging.FromContext(ctx).Info("Setting up event handlers")
 	apiServerSourceInformer.Informer().AddEventHandler(controller.HandleAll(impl.Enqueue))
 
 	deploymentInformer.Informer().AddEventHandler(cache.FilteringResourceEventHandler{
