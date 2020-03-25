dce9f7bfdd72412ceb180c74e1754b2affb5d436
Lionel Villard <villard@us.ibm.com>
2020-03-25 15:55:51 -0400

    use generated reconciler (#2843)

--- pkg/reconciler/pingsource/jobrunner/pingsource_test.go
+++ pkg/reconciler/pingsource/jobrunner/pingsource_test.go
@@ -21,20 +21,21 @@ import (
 	"sync"
 	"testing"
 
-	kncetesting "knative.dev/eventing/pkg/kncloudevents/testing"
-	"knative.dev/pkg/source"
-
 	"github.com/robfig/cron"
 	"k8s.io/apimachinery/pkg/runtime"
+	sourcesv1alpha2 "knative.dev/eventing/pkg/apis/sources/v1alpha2"
+	fakeeventingclient "knative.dev/eventing/pkg/client/injection/client/fake"
+	"knative.dev/eventing/pkg/client/injection/reconciler/sources/v1alpha2/pingsource"
+	kncetesting "knative.dev/eventing/pkg/kncloudevents/testing"
+	. "knative.dev/eventing/pkg/reconciler/testing"
 	"knative.dev/pkg/apis"
 	duckv1 "knative.dev/pkg/apis/duck/v1"
 	"knative.dev/pkg/configmap"
 	"knative.dev/pkg/controller"
+	"knative.dev/pkg/logging"
 	logtesting "knative.dev/pkg/logging/testing"
 	. "knative.dev/pkg/reconciler/testing"
-
-	sourcesv1alpha2 "knative.dev/eventing/pkg/apis/sources/v1alpha2"
-	. "knative.dev/eventing/pkg/reconciler/testing"
+	"knative.dev/pkg/source"
 )
 
 const (
@@ -103,12 +104,15 @@ func TestAllCases(t *testing.T) {
 	ce := kncetesting.NewTestClient()
 
 	table.Test(t, MakeFactory(func(ctx context.Context, listers *Listers, cmw configmap.Watcher) controller.Reconciler {
-		return &Reconciler{
+		r := &Reconciler{
 			pingsourceLister: listers.GetPingSourceV1alpha2Lister(),
 			cronRunner:       NewCronJobsRunner(ce, reporter, logger),
 			entryidMu:        sync.Mutex{},
 			entryids:         make(map[string]cron.EntryID),
 		}
+		return pingsource.NewReconciler(ctx, logging.FromContext(ctx),
+			fakeeventingclient.Get(ctx), listers.GetPingSourceV1alpha2Lister(),
+			controller.GetEventRecorder(ctx), r)
 	}, false, logger))
 
 }
