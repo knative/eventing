253e0c07440b8e3e3f1b09a6c24bbc8bd2a32621
Matt Moore <mattmoor@vmware.com>
2020-03-31 21:10:37 -0700

    Migrate mtnamespace to genreconciler

--- pkg/reconciler/mtnamespace/namespace.go
+++ pkg/reconciler/mtnamespace/namespace.go
@@ -20,20 +20,17 @@ import (
 	"context"
 	"fmt"
 
-	"go.uber.org/zap"
 	corev1 "k8s.io/api/core/v1"
-	apierrs "k8s.io/apimachinery/pkg/api/errors"
 	k8serrors "k8s.io/apimachinery/pkg/api/errors"
-	corev1listers "k8s.io/client-go/listers/core/v1"
-	"k8s.io/client-go/tools/cache"
-	"k8s.io/client-go/tools/record"
-	"knative.dev/pkg/controller"
 
 	"knative.dev/eventing/pkg/apis/eventing/v1beta1"
 	clientset "knative.dev/eventing/pkg/client/clientset/versioned"
 	eventinglisters "knative.dev/eventing/pkg/client/listers/eventing/v1beta1"
 	"knative.dev/eventing/pkg/logging"
 	"knative.dev/eventing/pkg/reconciler/mtnamespace/resources"
+	namespacereconciler "knative.dev/pkg/client/injection/kube/reconciler/core/v1/namespace"
+	"knative.dev/pkg/controller"
+	pkgreconciler "knative.dev/pkg/reconciler"
 )
 
 const (
@@ -48,58 +45,15 @@ type Reconciler struct {
 	eventingClientSet clientset.Interface
 
 	// listers index properties about resources
-	namespaceLister corev1listers.NamespaceLister
-	brokerLister    eventinglisters.BrokerLister
-
-	recorder record.EventRecorder
+	brokerLister eventinglisters.BrokerLister
 }
 
-// Check that our Reconciler implements controller.Reconciler
-var _ controller.Reconciler = (*Reconciler)(nil)
+// Check that our Reconciler implements namespacereconciler.Interface
+var _ namespacereconciler.Interface = (*Reconciler)(nil)
 
-// Reconcile compares the actual state with the desired, and attempts to
-// converge the two. It then updates the Status block of the Namespace resource
-// with the current status of the resource.
-func (r *Reconciler) Reconcile(ctx context.Context, key string) error {
-	_, name, err := cache.SplitMetaNamespaceKey(key)
-	if err != nil {
-		logging.FromContext(ctx).Error("invalid resource key")
-		return nil
-	}
-
-	// Get the namespace resource with this namespace/name
-	original, err := r.namespaceLister.Get(name)
-	if apierrs.IsNotFound(err) {
-		// The resource may no longer exist, in which case we stop processing.
-		logging.FromContext(ctx).Error("namespace key in work queue no longer exists")
-		return nil
-	} else if err != nil {
-		return err
-	}
-
-	if original.Labels[resources.InjectionLabelKey] == resources.InjectionDisabledLabelValue {
+func (r *Reconciler) ReconcileKind(ctx context.Context, ns *corev1.Namespace) pkgreconciler.Event {
+	if ns.Labels[resources.InjectionLabelKey] == resources.InjectionDisabledLabelValue {
 		logging.FromContext(ctx).Debug("Not reconciling Namespace")
-		return nil
-	}
-
-	// Don't modify the informers copy
-	ns := original.DeepCopy()
-
-	// Reconcile this copy of the Namespace.
-	reconcileErr := r.reconcile(ctx, ns)
-	if reconcileErr != nil {
-		logging.FromContext(ctx).Error("Error reconciling Namespace", zap.Error(reconcileErr))
-		r.recorder.Eventf(ns, corev1.EventTypeWarning, namespaceReconcileFailure, "Failed to reconcile Namespace: %v", reconcileErr)
-	} else {
-		logging.FromContext(ctx).Debug("Namespace reconciled")
-		r.recorder.Eventf(ns, corev1.EventTypeNormal, namespaceReconciled, "Namespace reconciled: %q", ns.Name)
-	}
-
-	return reconcileErr
-}
-
-func (r *Reconciler) reconcile(ctx context.Context, ns *corev1.Namespace) error {
-	if ns.DeletionTimestamp != nil {
 		return nil
 	}
 
@@ -121,7 +75,7 @@ func (r *Reconciler) reconcileBroker(ctx context.Context, ns *corev1.Namespace) 
 		if err != nil {
 			return nil, err
 		}
-		r.recorder.Event(ns, corev1.EventTypeNormal, brokerCreated,
+		controller.GetEventRecorder(ctx).Event(ns, corev1.EventTypeNormal, brokerCreated,
 			"Default eventing.knative.dev Broker created.")
 		return b, nil
 	} else if err != nil {
