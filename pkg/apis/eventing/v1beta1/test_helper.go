654033c68c4abddcd0d5765294177f4c54cc989b
Ville Aikas <vaikas@vmware.com>
2020-05-01 12:50:49 -0700

    remove unused cruft

--- pkg/apis/eventing/v1beta1/test_helper.go
+++ pkg/apis/eventing/v1beta1/test_helper.go
@@ -17,15 +17,10 @@
 package v1beta1
 
 import (
-	v1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 
-	duckv1alpha1 "knative.dev/eventing/pkg/apis/duck/v1alpha1"
 	messagingv1beta1 "knative.dev/eventing/pkg/apis/messaging/v1beta1"
 	"knative.dev/pkg/apis"
-	duckv1 "knative.dev/pkg/apis/duck/v1"
-	pkgduckv1alpha1 "knative.dev/pkg/apis/duck/v1alpha1"
-	duckv1beta1 "knative.dev/pkg/apis/duck/v1beta1"
 )
 
 type testHelper struct{}
@@ -58,37 +53,10 @@ func (testHelper) ReadySubscriptionStatus() *messagingv1beta1.SubscriptionStatus
 	return ss
 }
 
-func (testHelper) ReadyChannelStatus() *duckv1alpha1.ChannelableStatus {
-	cs := &duckv1alpha1.ChannelableStatus{
-		Status: duckv1.Status{},
-		AddressStatus: pkgduckv1alpha1.AddressStatus{
-			Address: &pkgduckv1alpha1.Addressable{
-				Addressable: duckv1beta1.Addressable{
-					URL: &apis.URL{Scheme: "http", Host: "foo"},
-				},
-				Hostname: "foo",
-			},
-		},
-		SubscribableTypeStatus: duckv1alpha1.SubscribableTypeStatus{}}
-	return cs
-}
-
 func (t testHelper) ReadyBrokerStatus() *BrokerStatus {
 	bs := &BrokerStatus{}
 	bs.SetAddress(apis.HTTP("example.com"))
 	return bs
-}
-
-func (t testHelper) AvailableDeployment() *v1.Deployment {
-	d := &v1.Deployment{}
-	d.Name = "available"
-	d.Status.Conditions = []v1.DeploymentCondition{
-		{
-			Type:   v1.DeploymentAvailable,
-			Status: "True",
-		},
-	}
-	return d
 }
 
 func (t testHelper) UnknownBrokerStatus() *BrokerStatus {
