69cf66e4188ecea76f04b8e47e07f04bc76acd02
Chris Suszyński <ksuszyns@redhat.com>
2020-05-08 13:33:29 +0200

    Updating codegen

--- vendor/github.com/phayes/freeport/freeport.go
+++ vendor/github.com/phayes/freeport/freeport.go
@@ -28,3 +28,22 @@ func GetPort() int {
 	}
 	return port
 }
+
+// GetFreePort asks the kernel for free open ports that are ready to use.
+func GetFreePorts(count int) ([]int, error) {
+	var ports []int
+	for i := 0; i < count; i++ {
+		addr, err := net.ResolveTCPAddr("tcp", "localhost:0")
+		if err != nil {
+			return nil, err
+		}
+
+		l, err := net.ListenTCP("tcp", addr)
+		if err != nil {
+			return nil, err
+		}
+		defer l.Close()
+		ports = append(ports, l.Addr().(*net.TCPAddr).Port)
+	}
+	return ports, nil
+}
