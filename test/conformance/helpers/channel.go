c1f2ea6a214d6b9fad4920db923d331de97f6941
Ali Ok <aliok@redhat.com>
2020-05-15 09:54:55 +0300

    Get rid of hardcoded `Channel` GVK strings

--- test/conformance/helpers/channel.go
+++ test/conformance/helpers/channel.go
@@ -25,6 +25,9 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	eventingduckv1alpha1 "knative.dev/eventing/pkg/apis/duck/v1alpha1"
 	eventingduckv1beta1 "knative.dev/eventing/pkg/apis/duck/v1beta1"
+
+	messagingv1alpha1 "knative.dev/eventing/pkg/apis/messaging/v1alpha1"
+	messagingv1beta1 "knative.dev/eventing/pkg/apis/messaging/v1beta1"
 )
 
 const (
@@ -32,14 +35,17 @@ const (
 )
 
 var (
+	channelv1alpha1GVK = (&messagingv1alpha1.Channel{}).GetGroupVersionKind()
+	channelv1beta1GVK  = (&messagingv1beta1.Channel{}).GetGroupVersionKind()
+
 	channelv1alpha1 = metav1.TypeMeta{
-		Kind:       "Channel",
-		APIVersion: "messaging.knative.dev/v1alpha1",
+		Kind:       channelv1alpha1GVK.Kind,
+		APIVersion: channelv1alpha1GVK.GroupVersion().String(),
 	}
 
 	channelv1beta1 = metav1.TypeMeta{
-		Kind:       "Channel",
-		APIVersion: "messaging.knative.dev/v1beta1",
+		Kind:       channelv1beta1GVK.Kind,
+		APIVersion: channelv1beta1GVK.GroupVersion().String(),
 	}
 )
 
