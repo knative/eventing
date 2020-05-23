f26a164a87fb9a786b39a1579950978714e47bd0
Weston Haught <whaught@google.com>
2020-05-23 08:51:51 -0700

    Fix test types

--- pkg/apis/eventing/v1beta1/register.go
+++ pkg/apis/eventing/v1beta1/register.go
@@ -47,8 +47,8 @@ func addKnownTypes(scheme *runtime.Scheme) error {
 	scheme.AddKnownTypes(SchemeGroupVersion,
 		&Broker{},
 		&BrokerList{},
-		//		&EventType{},
-		//		&EventTypeList{},
+		&EventType{},
+		&EventTypeList{},
 		&Trigger{},
 		&TriggerList{},
 	)
