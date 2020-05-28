ce2a1b581e07a4ebf6845bd979e4e482ab0f5152
Lukas Berk <lberk@redhat.com>
2020-05-28 15:52:24 -0400

    Drop unused vars, add additional test
    Add an additional test (adding broker to trigger-no-broker and
    ensuring status reflects subscriberURI)
    
    Drop unused variables from function parameters
    
    Abstract component names to variables

--- test/conformance/helpers/broker_control_plane_test_helper.go
+++ test/conformance/helpers/broker_control_plane_test_helper.go
@@ -18,6 +18,7 @@ package helpers
 
 import (
 	"testing"
+	"time"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
@@ -38,45 +39,51 @@ func BrokerV1Beta1ControlPlaneTestHelperWithChannelTestRunner(
 	channelTestRunner.RunTests(t, lib.FeatureBasic, func(t *testing.T, channel metav1.TypeMeta) {
 		client := lib.Setup(t, true, setupClient...)
 		defer lib.TearDown(client)
+		brokerName := "br"
+		triggerNoBroker := "trigger-no-broker"
+		triggerWithBroker := "trigger-with-broker"
 
 		t.Run("Trigger V1Beta1 can be crated before Broker (with attributes filter)", func(t *testing.T) {
-			TriggerV1Beta1BeforeBrokerHelper(t, brokerClass, client, channel)
+			TriggerV1Beta1BeforeBrokerHelper(t, triggerNoBroker, client)
 		})
 
 		t.Run("Broker V1Beta1 can be created and progresses to Ready", func(t *testing.T) {
-			BrokerV1Beta1CreatedToReadyHelper(t, brokerClass, client, channel)
+			BrokerV1Beta1CreatedToReadyHelper(t, brokerName, brokerClass, client, channel)
 		})
 
 		t.Run("Ready Broker V1Beta1 is Addressable", func(t *testing.T) {
-			ReadyBrokerV1Beta1AvailableHelper(t, brokerClass, client, channel)
+			ReadyBrokerV1Beta1AvailableHelper(t, brokerName, client)
 		})
 
 		t.Run("Trigger V1Beta1 with Ready broker progresses to Ready", func(t *testing.T) {
-			TriggerV1Beta1ReadyBrokerReadyHelper(t, brokerClass, client, channel)
+			TriggerV1Beta1ReadyBrokerReadyHelper(t, triggerWithBroker, brokerName, client)
+		})
+
+		t.Run("Ready Trigger V1Beta1 (no Broker) set Broker and includes status.subscriber Uri", func(t *testing.T) {
+			TriggerV1Beta1ReadyAfterBrokerIncludesSubURI(t, triggerNoBroker, brokerName, client)
 		})
 
 		t.Run("Ready Trigger V1Beta1 includes status.subscriber Uri", func(t *testing.T) {
-			TriggerV1Beta1ReadyIncludesSubURI(t, brokerClass, client, channel)
+			TriggerV1Beta1ReadyIncludesSubURI(t, triggerWithBroker, client)
 		})
 	})
 
 }
 
-func TriggerV1Beta1BeforeBrokerHelper(t *testing.T, brokerClass string, client *lib.Client, channel metav1.TypeMeta) {
+func TriggerV1Beta1BeforeBrokerHelper(t *testing.T, triggerName string, client *lib.Client) {
 	const etLogger = "logger"
 	const loggerPodName = "logger-pod"
 
 	logPod := resources.EventRecordPod(loggerPodName)
 	client.CreatePodOrFail(logPod, lib.WithService(loggerPodName))
 	client.WaitForAllTestResourcesReadyOrFail() //Can't do this for the trigger because it's not 'ready' yet
-	client.CreateTriggerOrFail("trigger-no-broker",
-		resources.WithAttributesTriggerFilter(eventingv1beta1.TriggerAnyFilter, etLogger, map[string]interface{}{}),
-		resources.WithSubscriberServiceRefForTrigger(loggerPodName),
+	client.CreateTriggerOrFailV1Beta1(triggerName,
+		resources.WithAttributesTriggerFilterV1Beta1(eventingv1beta1.TriggerAnyFilter, etLogger, map[string]interface{}{}),
+		resources.WithSubscriberServiceRefForTriggerV1Beta1(loggerPodName),
 	)
-	client.Tracker.Clean(true)
 }
 
-func BrokerV1Beta1CreatedToReadyHelper(t *testing.T, brokerClass string, client *lib.Client, channel metav1.TypeMeta) {
+func BrokerV1Beta1CreatedToReadyHelper(t *testing.T, brokerName, brokerClass string, client *lib.Client, channel metav1.TypeMeta) {
 	const defaultCMPName = "eventing"
 
 	// Create the Broker.
@@ -87,7 +94,7 @@ func BrokerV1Beta1CreatedToReadyHelper(t *testing.T, brokerClass string, client 
 	}
 
 	// Create a configmap used by the broker.
-	client.CreateBrokerConfigMapOrFail("br", &channel)
+	client.CreateBrokerConfigMapOrFail(brokerName, &channel)
 
 	broker := client.CreateBrokerV1Beta1OrFail(
 		"br",
@@ -99,34 +106,54 @@ func BrokerV1Beta1CreatedToReadyHelper(t *testing.T, brokerClass string, client 
 
 }
 
-func ReadyBrokerV1Beta1AvailableHelper(t *testing.T, brokerClass string, client *lib.Client, channel metav1.TypeMeta) {
-	client.WaitForResourceReadyOrFail("br", lib.BrokerTypeMeta)
-	obj := resources.NewMetaResource("br", client.Namespace, lib.BrokerTypeMeta)
+func ReadyBrokerV1Beta1AvailableHelper(t *testing.T, brokerName string, client *lib.Client) {
+	client.WaitForResourceReadyOrFail(brokerName, lib.BrokerTypeMeta)
+	obj := resources.NewMetaResource(brokerName, client.Namespace, lib.BrokerTypeMeta)
 	_, err := duck.GetAddressableURI(client.Dynamic, obj)
 	if err != nil {
 		t.Fatalf("Broker is not addressable %v", err)
 	}
 }
 
-func TriggerV1Beta1ReadyBrokerReadyHelper(t *testing.T, brokerClass string, client *lib.Client, channel metav1.TypeMeta) {
+func TriggerV1Beta1ReadyBrokerReadyHelper(t *testing.T, triggerName, brokerName string, client *lib.Client) {
 	const etLogger = "logger"
 	const loggerPodName = "logger-pod"
-	const defaultCMPName = "eventing"
 
-	trigger := client.CreateTriggerOrFail("trigger-with-broker",
-		resources.WithAttributesTriggerFilter(eventingv1beta1.TriggerAnyFilter, etLogger, map[string]interface{}{}),
-		resources.WithSubscriberServiceRefForTrigger(loggerPodName),
-		resources.WithBroker("br"),
+	trigger := client.CreateTriggerOrFailV1Beta1(triggerName,
+		resources.WithAttributesTriggerFilterV1Beta1(eventingv1beta1.TriggerAnyFilter, etLogger, map[string]interface{}{}),
+		resources.WithSubscriberServiceRefForTriggerV1Beta1(loggerPodName),
+		resources.WithBrokerV1Beta1(brokerName),
 	)
 
 	client.WaitForResourceReadyOrFail(trigger.Name, lib.TriggerTypeMeta)
 }
 
-func TriggerV1Beta1ReadyIncludesSubURI(t *testing.T, brokerClass string, client *lib.Client, channel metav1.TypeMeta) {
-	client.WaitForResourceReadyOrFail("trigger-with-broker", lib.TriggerTypeMeta)
-	tr, err := client.Eventing.EventingV1beta1().Triggers(client.Namespace).Get("trigger-with-broker", metav1.GetOptions{})
+func TriggerV1Beta1ReadyAfterBrokerIncludesSubURI(t *testing.T, triggerName, brokerName string, client *lib.Client) {
+	tr, err := client.Eventing.EventingV1beta1().Triggers(client.Namespace).Get(triggerName, metav1.GetOptions{})
 	if err != nil {
-		t.Fatalf("Error: Could not get trigger %s: %v", "trigger-with-broker", err)
+		t.Fatalf("Error: Could not get trigger %s: %v", triggerName, err)
+	}
+	tr.Spec.Broker = brokerName
+	_, err = client.Eventing.EventingV1beta1().Triggers(client.Namespace).Update(tr)
+	if err != nil {
+		t.Fatalf("Error: Could not update trigger %s: %v", triggerName, err)
+	}
+	time.Sleep(1 * time.Second)
+	tr, err = client.Eventing.EventingV1beta1().Triggers(client.Namespace).Get(triggerName, metav1.GetOptions{})
+	if err != nil {
+		t.Fatalf("Error: Could not get trigger %s: %v", triggerName, err)
+	}
+	if tr.Status.SubscriberURI == nil {
+		t.Fatalf("Error: trigger.Status.SubscriberURI is nil but Broker Addressable & Ready")
+	}
+
+}
+
+func TriggerV1Beta1ReadyIncludesSubURI(t *testing.T, triggerName string, client *lib.Client) {
+	client.WaitForResourceReadyOrFail(triggerName, lib.TriggerTypeMeta)
+	tr, err := client.Eventing.EventingV1beta1().Triggers(client.Namespace).Get(triggerName, metav1.GetOptions{})
+	if err != nil {
+		t.Fatalf("Error: Could not get trigger %s: %v", tr.Name, err)
 	}
 	if tr.Status.SubscriberURI == nil {
 		t.Fatalf("Error: trigger.Status.SubscriberURI is nil but resource reported Ready")
