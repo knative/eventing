7c7a2ccdaeb9aa2d414608df784351421bbb5278
Jihui Nie <56172920+jihuin@users.noreply.github.com>
2020-05-29 01:43:02 -0700

    Use env var WEBHOOK_PORT to configure the webhook port (#3158)

--- cmd/webhook/main.go
+++ cmd/webhook/main.go
@@ -343,7 +343,7 @@ func main() {
 	// Set up a signal context with our webhook options
 	ctx := webhook.WithOptions(signals.NewContext(), webhook.Options{
 		ServiceName: logconfig.WebhookName(),
-		Port:        8443,
+		Port:        webhook.PortFromEnv(8443),
 		// SecretName must match the name of the Secret created in the configuration.
 		SecretName: "eventing-webhook-certs",
 	})
