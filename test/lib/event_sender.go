b77bbbc1d79307aaeb79c428a4a06b9909512ae3
slinkydeveloper <francescoguard@gmail.com>
2020-06-01 15:26:55 +0200

    fmt + copyright
    Signed-off-by: Francesco Guardiani <francescoguard@gmail.com>

--- test/lib/event_sender.go
+++ test/lib/event_sender.go
@@ -1,3 +1,19 @@
+/*
+Copyright 2020 The Knative Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package lib
 
 import (
@@ -31,7 +47,7 @@ func (c *Client) SendEvent(
 	senderName string,
 	uri string,
 	event cloudevents.Event,
-    option ...sender.EventSenderOption,
+	option ...sender.EventSenderOption,
 ) error {
 	namespace := c.Namespace
 	pod, err := sender.EventSenderPod("event-sender", senderName, uri, event, option...)
@@ -44,4 +60,3 @@ func (c *Client) SendEvent(
 	}
 	return nil
 }
-
