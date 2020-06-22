456b92b1df4572850046230979e570757e951a63
Ahmed Abdalla <aabdelre@redhat.com>
2020-06-22 12:58:37 +0200

    Add Sources CRD labels conformance test

--- test/flags/sources.go
+++ test/flags/sources.go
@@ -1,3 +1,19 @@
+/*
+Copyright 2020 The Knative Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package flags
 
 import (
@@ -15,7 +31,7 @@ func (sources *Sources) String() string {
 	return fmt.Sprint(*sources)
 }
 
-// Set converts the input string to Sources.
+// Set appends the input string to Sources.
 func (sources *Sources) Set(value string) error {
 	for _, source := range strings.Split(value, ",") {
 		source := strings.TrimSpace(source)
@@ -28,7 +44,7 @@ func (sources *Sources) Set(value string) error {
 			Kind:       split[1],
 		}
 		if !isValidSource(tm.Kind) {
-			log.Fatalf("The given source name %q is invalid, tests cannot be run.\n", source)
+			log.Fatalf("The given Source name %q is invalid, tests cannot be run.\n", source)
 		}
 
 		*sources = append(*sources, tm)
