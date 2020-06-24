cd1d960917f10436612e36955d4b4848c51bf91c
Ali Ok <aliok@redhat.com>
2020-06-24 19:28:27 +0300

    Improve eventing v1beta1 conversion tests (#3395)

--- pkg/apis/eventing/v1beta1/trigger_conversion.go
+++ pkg/apis/eventing/v1beta1/trigger_conversion.go
@@ -25,16 +25,16 @@ import (
 )
 
 // ConvertTo implements apis.Convertible
-func (source *Trigger) ConvertTo(ctx context.Context, to apis.Convertible) error {
+func (source *Trigger) ConvertTo(_ context.Context, to apis.Convertible) error {
 	switch sink := to.(type) {
 	case *v1.Trigger:
 		sink.Spec.Broker = source.Spec.Broker
 		sink.Spec.Subscriber = source.Spec.Subscriber
 		if source.Spec.Filter != nil {
 			sink.Spec.Filter = &v1.TriggerFilter{}
-		}
-		for k, v := range source.Spec.Filter.Attributes {
-			sink.Spec.Filter.Attributes[k] = v
+			for k, v := range source.Spec.Filter.Attributes {
+				sink.Spec.Filter.Attributes[k] = v
+			}
 		}
 		sink.Status.Status = source.Status.Status
 		sink.Status.SubscriberURI = source.Status.SubscriberURI
@@ -45,16 +45,16 @@ func (source *Trigger) ConvertTo(ctx context.Context, to apis.Convertible) error
 }
 
 // ConvertFrom implements apis.Convertible
-func (sink *Trigger) ConvertFrom(ctx context.Context, from apis.Convertible) error {
+func (sink *Trigger) ConvertFrom(_ context.Context, from apis.Convertible) error {
 	switch source := from.(type) {
 	case *v1.Trigger:
 		sink.Spec.Broker = source.Spec.Broker
 		sink.Spec.Subscriber = source.Spec.Subscriber
 		if source.Spec.Filter != nil {
 			sink.Spec.Filter = &TriggerFilter{}
-		}
-		for k, v := range source.Spec.Filter.Attributes {
-			sink.Spec.Filter.Attributes[k] = v
+			for k, v := range source.Spec.Filter.Attributes {
+				sink.Spec.Filter.Attributes[k] = v
+			}
 		}
 		sink.Status.Status = source.Status.Status
 		sink.Status.SubscriberURI = source.Status.SubscriberURI
--- pkg/apis/eventing/v1beta1/trigger_conversion_test.go
+++ pkg/apis/eventing/v1beta1/trigger_conversion_test.go
@@ -19,10 +19,36 @@ package v1beta1
 import (
 	"context"
 	"testing"
+
+	v1 "knative.dev/eventing/pkg/apis/eventing/v1"
 )
+
+func TestTriggerConversion(t *testing.T) {
+	objv1betav1, objv1 := &Trigger{}, &v1.Trigger{}
+
+	if err := objv1betav1.ConvertTo(context.Background(), objv1); err != nil {
+		t.Errorf("ConvertTo() = %#v, unexpected error", objv1betav1)
+	}
+
+	if err := objv1betav1.ConvertFrom(context.Background(), objv1); err != nil {
+		t.Errorf("ConvertFrom() = %#v, unexpected error", objv1)
+	}
+}
 
 func TestTriggerConversionBadType(t *testing.T) {
 	good, bad := &Trigger{}, &Broker{}
+
+	if err := good.ConvertTo(context.Background(), bad); err == nil {
+		t.Errorf("ConvertTo() = %#v, wanted error", bad)
+	}
+
+	if err := good.ConvertFrom(context.Background(), bad); err == nil {
+		t.Errorf("ConvertFrom() = %#v, wanted error", good)
+	}
+}
+
+func TestTriggerConversionBadVersion(t *testing.T) {
+	good, bad := &Trigger{}, &Trigger{}
 
 	if err := good.ConvertTo(context.Background(), bad); err == nil {
 		t.Errorf("ConvertTo() = %#v, wanted error", bad)
