b257007147e0e23130df1e344bc1b9981306ab6d
Lionel Villard <villard@us.ibm.com>
2020-07-16 11:30:33 -0400

    use git apply instead of patch (#3595)
    * use git apply instead of patch
    
    * do like serving

--- hack/update-deps.sh
+++ hack/update-deps.sh
@@ -62,7 +62,6 @@ export GOFLAGS=-mod=vendor
 
 update_licenses third_party/VENDOR-LICENSE "./..."
 
-
 # Patch k8s leader-election fixing graceful release
 # More information: https://github.com/kubernetes/kubernetes/pull/91942
-patch vendor/k8s.io/client-go/tools/leaderelection/leaderelection.go hack/k8s-client-go.patch
+git apply ${ROOT_DIR}/hack/k8s-client-go.patch
