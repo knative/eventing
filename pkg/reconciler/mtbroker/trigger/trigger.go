f47a5a0d13a1232e09b8949d9133b6582d4ab1a3
Ville Aikas <vaikas@vmware.com>
2020-07-24 04:22:13 -0700

    use tracker instead

--- pkg/reconciler/mtbroker/trigger/trigger.go
+++ pkg/reconciler/mtbroker/trigger/trigger.go
@@ -89,6 +89,9 @@ func (r *Reconciler) ReconcileKind(ctx context.Context, t *eventingv1.Trigger) p
 		// Everything is cleaned up by the garbage collector.
 		return nil
 	}
+	// Start tracking the broker
+	r.trackBroker(ctx, t)
+
 	b, err := r.brokerLister.Brokers(t.Namespace).Get(t.Spec.Broker)
 	if err != nil {
 		if apierrs.IsNotFound(err) {
@@ -101,12 +104,12 @@ func (r *Reconciler) ReconcileKind(ctx context.Context, t *eventingv1.Trigger) p
 			return err
 		}
 	}
+
 	// If it's not my brokerclass, ignore
 	if b.Annotations[eventing.BrokerClassKey] != eventing.MTChannelBrokerClassValue {
 		logging.FromContext(ctx).Infow("Ignoring trigger %s/%s", t.Namespace, t.Name)
 		return nil
 	}
-
 	t.Status.PropagateBrokerCondition(b.Status.GetTopLevelCondition())
 
 	// If Broker is not ready, we're done, but once it becomes ready, we'll get requeued.
@@ -152,6 +155,22 @@ func (r *Reconciler) ReconcileKind(ctx context.Context, t *eventingv1.Trigger) p
 		return err
 	}
 
+	return nil
+}
+
+func (r *Reconciler) trackBroker(ctx context.Context, t *eventingv1.Trigger) error {
+	trackKResource := r.kresourceTracker.TrackInNamespace(t)
+	brokerObjRef := corev1.ObjectReference{
+		Kind:       brokerGVK.Kind,
+		APIVersion: brokerGVK.GroupVersion().String(),
+		Name:       t.Spec.Broker,
+		Namespace:  t.Namespace,
+	}
+
+	if err := trackKResource(brokerObjRef); err != nil {
+		return fmt.Errorf("Failed to track broker %q : %s", t.Spec.Broker, err)
+	}
+	logging.FromContext(ctx).Infow("Tracking:", zap.Any("Broker", brokerObjRef))
 	return nil
 }
 
