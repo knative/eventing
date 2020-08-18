9f3fb3ffde17dbea586e4f4f16ac99eafce2a8b7
Nicolas Lopez <ngiraldo@google.com>
2020-08-18 02:57:13 -0400

    kubectl explain for EventType (#3857)

--- pkg/apis/eventing/v1beta1/eventtype_types.go
+++ pkg/apis/eventing/v1beta1/eventtype_types.go
@@ -29,6 +29,7 @@ import (
 // +genreconciler
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
+// EventType represents a type of event that can be consumed from a Broker.
 type EventType struct {
 	metav1.TypeMeta `json:",inline"`
 	// +optional
