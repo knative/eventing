4a589d36eb0742710a8b90c7a26067d3c59f357f
slinkydeveloper <francescoguard@gmail.com>
2020-10-05 15:49:31 +0200

    Other stuff
    Signed-off-by: Francesco Guardiani <francescoguard@gmail.com>

--- test/lib/recordevents/resources.go
+++ test/lib/recordevents/resources.go
@@ -3,29 +3,29 @@ package recordevents
 import (
 	"fmt"
 
-	"k8s.io/api/core/v1"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/uuid"
 	"knative.dev/pkg/test"
 )
 
 // EventRecordPod creates a Pod that stores received events for test retrieval.
-func EventRecordPod(name string, namespace string, serviceAccountName string) *v1.Pod {
+func EventRecordPod(name string, namespace string, serviceAccountName string) *corev1.Pod {
 	return recordEventsPod("recordevents", name, namespace, serviceAccountName)
 }
 
-func recordEventsPod(imageName string, name string, namespace string, serviceAccountName string) *v1.Pod {
-	return &v1.Pod{
+func recordEventsPod(imageName string, name string, namespace string, serviceAccountName string) *corev1.Pod {
+	return &corev1.Pod{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:   name,
 			Labels: map[string]string{"e2etest": string(uuid.NewUUID())},
 		},
-		Spec: v1.PodSpec{
-			Containers: []v1.Container{{
+		Spec: corev1.PodSpec{
+			Containers: []corev1.Container{{
 				Name:            imageName,
 				Image:           test.ImagePath(imageName),
-				ImagePullPolicy: v1.PullAlways,
-				Env: []v1.EnvVar{{
+				ImagePullPolicy: corev1.PullAlways,
+				Env: []corev1.EnvVar{{
 					Name:  "SYSTEM_NAMESPACE",
 					Value: namespace,
 				}, {
@@ -33,11 +33,11 @@ func recordEventsPod(imageName string, name string, namespace string, serviceAcc
 					Value: "recorder-" + name,
 				}, {
 					Name:  "K8S_EVENT_SINK",
-					Value: fmt.Sprintf("{\"apiVersion\": \"v1\", \"kind\": \"Pod\", \"name\": \"%s\", \"namespace\": \"%s\"}", name, namespace),
+					Value: fmt.Sprintf("{\"apiVersion\": \"corev1\", \"kind\": \"Pod\", \"name\": \"%s\", \"namespace\": \"%s\"}", name, namespace),
 				}},
 			}},
 			ServiceAccountName: serviceAccountName,
-			RestartPolicy:      v1.RestartPolicyAlways,
+			RestartPolicy:      corev1.RestartPolicyAlways,
 		},
 	}
 }
