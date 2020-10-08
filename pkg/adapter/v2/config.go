57358d4385f29040729d4a4d502d8c5972d07f36
Lionel Villard <villard@us.ibm.com>
2020-10-08 19:00:21 -0400

    fix crash when pingsource adapter is scaled up without any sources (#4242)
    * fix crash when pingsource adapter is scaled up without any sources
    
    * K_SINK_TIMEOUT now accepts any string value

--- pkg/adapter/v2/config.go
+++ pkg/adapter/v2/config.go
@@ -90,7 +90,10 @@ type EnvConfig struct {
 	LeaderElectionConfigJson string `envconfig:"K_LEADER_ELECTION_CONFIG"`
 
 	// Time in seconds to wait for sink to respond
-	EnvSinkTimeout int `envconfig:"K_SINK_TIMEOUT"`
+	EnvSinkTimeout string `envconfig:"K_SINK_TIMEOUT"`
+
+	// cached zap logger
+	logger *zap.SugaredLogger
 }
 
 // EnvConfigAccessor defines accessors for the minimal
@@ -121,7 +124,7 @@ type EnvConfigAccessor interface {
 	// GetLeaderElectionConfig returns leader election configuration.
 	GetLeaderElectionConfig() (*kle.ComponentConfig, error)
 
-	// Get the name of the adapter.
+	// Get the timeout to apply on a request to a sink
 	GetSinktimeout() int
 }
 
@@ -141,18 +144,20 @@ func (e *EnvConfig) GetMetricsConfig() (*metrics.ExporterOptions, error) {
 }
 
 func (e *EnvConfig) GetLogger() *zap.SugaredLogger {
-	loggingConfig, err := logging.JsonToLoggingConfig(e.LoggingConfigJson)
-	if err != nil {
-		// Use default logging config.
-		if loggingConfig, err = logging.NewConfigFromMap(map[string]string{}); err != nil {
-			// If this fails, there is no recovering.
-			panic(err)
+	if e.logger == nil {
+		loggingConfig, err := logging.JsonToLoggingConfig(e.LoggingConfigJson)
+		if err != nil {
+			// Use default logging config.
+			if loggingConfig, err = logging.NewConfigFromMap(map[string]string{}); err != nil {
+				// If this fails, there is no recovering.
+				panic(err)
+			}
 		}
+
+		logger, _ := logging.NewLoggerFromConfig(loggingConfig, e.Component)
+		e.logger = logger
 	}
-
-	logger, _ := logging.NewLoggerFromConfig(loggingConfig, e.Component)
-
-	return logger
+	return e.logger
 }
 
 func (e *EnvConfig) GetSink() string {
@@ -168,7 +173,11 @@ func (e *EnvConfig) GetName() string {
 }
 
 func (e *EnvConfig) GetSinktimeout() int {
-	return e.EnvSinkTimeout
+	if duration, err := strconv.Atoi(e.EnvSinkTimeout); err == nil {
+		return duration
+	}
+	e.GetLogger().Warn("Sink timeout configuration is invalid, default to -1 (no timeout)")
+	return -1
 }
 
 func (e *EnvConfig) SetupTracing(logger *zap.SugaredLogger) error {
