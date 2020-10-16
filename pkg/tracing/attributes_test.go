eca0035fa3f26324a05dd18e534a8aac96fe4d4d
runzexia <ryder.xia@sap.com>
2020-10-16 18:30:56 +0800

    tracing ut (#4320)
    * tracing ut
    
    Signed-off-by: runzexia <root@junwuhui.cn>
    
    * fix comments
    
    Signed-off-by: RyderXia <ryder.xia@sap.com>

--- pkg/tracing/attributes_test.go
+++ pkg/tracing/attributes_test.go
@@ -17,7 +17,10 @@ limitations under the License.
 package tracing
 
 import (
+	"reflect"
 	"testing"
+
+	"go.opencensus.io/trace"
 
 	"k8s.io/apimachinery/pkg/types"
 )
@@ -41,5 +44,78 @@ func TestTriggerMessagingDestination(t *testing.T) {
 	want := "trigger:triggername.triggerns"
 	if want != got {
 		t.Errorf("unexpected messaging destination: want %q, got %q", want, got)
+	}
+}
+
+func TestMessagingMessageIDAttribute(t *testing.T) {
+	tests := []struct {
+		name string
+		ID   string
+		want trace.Attribute
+	}{
+		{
+			name: "foo",
+			ID:   "foo",
+			want: trace.StringAttribute("messaging.message_id", "foo"),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := MessagingMessageIDAttribute(tt.ID); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("MessagingMessageIDAttribute() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestBrokerMessagingDestinationAttribute(t *testing.T) {
+
+	tests := []struct {
+		name          string
+		namespaceName types.NamespacedName
+		want          trace.Attribute
+	}{
+		{
+			name: "foo",
+			namespaceName: types.NamespacedName{
+				Namespace: "default",
+				Name:      "foo",
+			},
+
+			want: trace.StringAttribute("messaging.destination", "broker:foo.default"),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := BrokerMessagingDestinationAttribute(tt.namespaceName); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("BrokerMessagingDestinationAttribute() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestTriggerMessagingDestinationAttribute(t *testing.T) {
+
+	tests := []struct {
+		name          string
+		namespaceName types.NamespacedName
+		want          trace.Attribute
+	}{
+		{
+			name: "foo",
+			namespaceName: types.NamespacedName{
+				Namespace: "default",
+				Name:      "foo",
+			},
+
+			want: trace.StringAttribute("messaging.destination", "trigger:foo.default"),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := TriggerMessagingDestinationAttribute(tt.namespaceName); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("TriggerMessagingDestinationAttribute() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
