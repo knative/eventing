a6d13e7ddd04810c7b75840a30fd05d6ba293748
Matt Moore <mattmoor@vmware.com>
2020-10-20 09:49:07 -0700

    Add dummy.go files for restructured IMC. (#4349)
    * Add dummy.go files for restructured IMC.
    
    * https -> http

--- config/channels/in-memory-channel/deployments/dummy.go
+++ config/channels/in-memory-channel/deployments/dummy.go
@@ -14,6 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// Package channel is a placeholder that allows us to pull in config files
+// Package deployments is a placeholder that allows us to pull in config files
 // via go mod vendor.
-package channel
+package deployments
