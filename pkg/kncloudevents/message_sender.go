52a4eac7781d33346e9cf25332777e3449e0daa5
Francesco Guardiani <francescoguard@gmail.com>
2020-10-28 20:27:34 +0100

    Fix issue #4425 properly creating the request using the right constructor (#4426) (#4427)
    Signed-off-by: Francesco Guardiani <francescoguard@gmail.com>
    
    (cherry picked from commit 55029a6b4c9516b76d51b7c01580055416decaca)
    Signed-off-by: Francesco Guardiani <francescoguard@gmail.com>

--- pkg/kncloudevents/message_sender.go
+++ pkg/kncloudevents/message_sender.go
@@ -139,7 +139,12 @@ func (s *HttpMessageSender) SendWithRetries(req *nethttp.Request, config *RetryC
 		},
 	}
 
-	return retryableClient.Do(&retryablehttp.Request{Request: req})
+	retryableReq, err := retryablehttp.FromRequest(req)
+	if err != nil {
+		return nil, err
+	}
+
+	return retryableClient.Do(retryableReq)
 }
 
 func NoRetries() RetryConfig {
