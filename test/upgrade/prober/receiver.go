ed06afa9a0787200ccea63d01f304be98ea714fd
Chris Suszyński <ksuszyns@redhat.com>
2020-10-30 14:19:51 +0100

    Use deployment to avoid disparity in effective user
    On OpenShift we've observed a disparity when using pods vs deployments.
    Using both of those can lead to having different effective user for a
    bare pods and pods managed by deployment.
    
    That leads to differences in reading a config file by wathola
    components, as `~` points to different places sender and
    receiver+forwarder.
    
    This changes the code to avoid using bare pods for wathola components.

--- test/upgrade/prober/receiver.go
+++ test/upgrade/prober/receiver.go
@@ -20,6 +20,7 @@ import (
 	"fmt"
 
 	"github.com/wavesoftware/go-ensure"
+	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/intstr"
@@ -34,61 +35,22 @@ var (
 )
 
 func (p *prober) deployReceiver(ctx context.Context) {
-	p.deployReceiverPod(ctx)
+	p.deployReceiverDeployment(ctx)
 	p.deployReceiverService(ctx)
 }
 
-func (p *prober) deployReceiverPod(ctx context.Context) {
-	p.log.Infof("Deploy of receiver pod: %v", receiverName)
-	pod := &corev1.Pod{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      receiverName,
-			Namespace: p.config.Namespace,
-			Labels: map[string]string{
-				"app": receiverName,
-			},
-		},
-		Spec: corev1.PodSpec{
-			Volumes: []corev1.Volume{
-				{
-					Name: p.config.ConfigMapName,
-					VolumeSource: corev1.VolumeSource{
-						ConfigMap: &corev1.ConfigMapVolumeSource{
-							LocalObjectReference: corev1.LocalObjectReference{
-								Name: p.config.ConfigMapName,
-							},
-						},
-					},
-				},
-			},
-			Containers: []corev1.Container{
-				{
-					Name:  "receiver",
-					Image: pkgTest.ImagePath(receiverName),
-					VolumeMounts: []corev1.VolumeMount{
-						{
-							Name:      p.config.ConfigMapName,
-							ReadOnly:  true,
-							MountPath: p.config.ConfigMountPoint,
-						},
-					},
-					ReadinessProbe: &corev1.Probe{
-						Handler: corev1.Handler{
-							HTTPGet: &corev1.HTTPGetAction{
-								Path: p.config.HealthEndpoint,
-								Port: intstr.FromInt(watholaconfig.DefaultReceiverPort),
-							},
-						},
-					},
-				},
-			},
-		},
-	}
-	_, err := p.client.Kube.CreatePod(ctx, pod)
+func (p *prober) deployReceiverDeployment(ctx context.Context) {
+	p.log.Info("Deploy of receiver deployment: ", receiverName)
+	deployment := p.createReceiverDeployment()
+	_, err := p.client.Kube.AppsV1().
+		Deployments(deployment.Namespace).
+		Create(ctx, deployment, metav1.CreateOptions{})
 	ensure.NoError(err)
 
-	testlib.WaitFor(fmt.Sprintf("receiver be ready: %v", receiverName), func() error {
-		return pkgTest.WaitForPodRunning(ctx, p.client.Kube, receiverName, p.client.Namespace)
+	testlib.WaitFor(fmt.Sprint("receiver deployment be ready: ", receiverName), func() error {
+		return pkgTest.WaitForDeploymentScale(
+			ctx, p.client.Kube, receiverName, p.client.Namespace, 1,
+		)
 	})
 }
 
@@ -129,5 +91,59 @@ func (p *prober) deployReceiverService(ctx context.Context) {
 		panic(fmt.Errorf("couldn't find a node port for service: %v", receiverName))
 	} else {
 		p.log.Debugf("Node port for service: %v is %v", receiverName, receiverNodePort)
+	}
+}
+
+func (p *prober) createReceiverDeployment() *appsv1.Deployment {
+	var replicas int32 = 1
+	return &appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      receiverName,
+			Namespace: p.config.Namespace,
+		},
+		Spec: appsv1.DeploymentSpec{
+			Replicas: &replicas,
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"app": receiverName,
+				},
+			},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{
+						"app": receiverName,
+					},
+				},
+				Spec: corev1.PodSpec{
+					Volumes: []corev1.Volume{{
+						Name: p.config.ConfigMapName,
+						VolumeSource: corev1.VolumeSource{
+							ConfigMap: &corev1.ConfigMapVolumeSource{
+								LocalObjectReference: corev1.LocalObjectReference{
+									Name: p.config.ConfigMapName,
+								},
+							},
+						},
+					}},
+					Containers: []corev1.Container{{
+						Name:  "receiver",
+						Image: pkgTest.ImagePath(receiverName),
+						VolumeMounts: []corev1.VolumeMount{{
+							Name:      p.config.ConfigMapName,
+							ReadOnly:  true,
+							MountPath: p.config.ConfigMountPoint,
+						}},
+						ReadinessProbe: &corev1.Probe{
+							Handler: corev1.Handler{
+								HTTPGet: &corev1.HTTPGetAction{
+									Path: p.config.HealthEndpoint,
+									Port: intstr.FromInt(watholaconfig.DefaultReceiverPort),
+								},
+							},
+						},
+					}},
+				},
+			},
+		},
 	}
 }
