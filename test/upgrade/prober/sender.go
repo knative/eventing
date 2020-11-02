766a24c641666a1ca2b2a2e9035ecc36591fa00c
Chris Suszynski <ksuszyns@redhat.com>
2020-11-02 11:18:35 +0100

    Use deployment to avoid disparity in effective user (#4445)
    On OpenShift we've observed a disparity when using pods vs deployments.
    Using both of those can lead to having different effective user for a
    bare pods and pods managed by deployment.
    
    That leads to differences in reading a config file by wathola
    components, as `~` points to different places sender and
    receiver+forwarder.
    
    This changes the code to avoid using bare pods for wathola components.

--- test/upgrade/prober/sender.go
+++ test/upgrade/prober/sender.go
@@ -20,6 +20,7 @@ import (
 	"fmt"
 
 	"github.com/wavesoftware/go-ensure"
+	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	testlib "knative.dev/eventing/test/lib"
@@ -29,53 +30,68 @@ import (
 var senderName = "wathola-sender"
 
 func (p *prober) deploySender(ctx context.Context) {
-	p.log.Infof("Deploy sender pod: %v", senderName)
-	pod := &corev1.Pod{
+	p.log.Info("Deploy sender deployment: ", senderName)
+	var replicas int32 = 1
+	deployment := &appsv1.Deployment{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      senderName,
 			Namespace: p.config.Namespace,
 		},
-		Spec: corev1.PodSpec{
-			Volumes: []corev1.Volume{
-				{
-					Name: p.config.ConfigMapName,
-					VolumeSource: corev1.VolumeSource{
-						ConfigMap: &corev1.ConfigMapVolumeSource{
-							LocalObjectReference: corev1.LocalObjectReference{
-								Name: p.config.ConfigMapName,
-							},
-						},
-					},
+		Spec: appsv1.DeploymentSpec{
+			Replicas: &replicas,
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"app": senderName,
 				},
 			},
-			Containers: []corev1.Container{
-				{
-					Name:  "sender",
-					Image: pkgTest.ImagePath(senderName),
-					VolumeMounts: []corev1.VolumeMount{
-						{
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{
+						"app": senderName,
+					},
+				},
+				Spec: corev1.PodSpec{
+					Volumes: []corev1.Volume{{
+						Name: p.config.ConfigMapName,
+						VolumeSource: corev1.VolumeSource{
+							ConfigMap: &corev1.ConfigMapVolumeSource{
+								LocalObjectReference: corev1.LocalObjectReference{
+									Name: p.config.ConfigMapName,
+								},
+							},
+						},
+					}},
+					Containers: []corev1.Container{{
+						Name:  "sender",
+						Image: pkgTest.ImagePath(senderName),
+						VolumeMounts: []corev1.VolumeMount{{
 							Name:      p.config.ConfigMapName,
 							ReadOnly:  true,
 							MountPath: p.config.ConfigMountPoint,
-						},
-					},
+						}},
+					}},
 				},
 			},
 		},
 	}
-	_, err := p.client.Kube.CoreV1().Pods(p.client.Namespace).
-		Create(ctx, pod, metav1.CreateOptions{})
+
+	_, err := p.client.Kube.AppsV1().
+		Deployments(p.client.Namespace).
+		Create(ctx, deployment, metav1.CreateOptions{})
 	ensure.NoError(err)
 
-	testlib.WaitFor(fmt.Sprintf("sender pod be ready: %v", senderName), func() error {
-		return pkgTest.WaitForPodRunning(ctx, p.client.Kube, senderName, p.client.Namespace)
+	testlib.WaitFor(fmt.Sprint("sender deployment be ready: ", senderName), func() error {
+		return pkgTest.WaitForDeploymentScale(
+			ctx, p.client.Kube, senderName, p.client.Namespace, int(replicas),
+		)
 	})
 }
 
 func (p *prober) removeSender(ctx context.Context) {
-	p.log.Infof("Remove of sender pod: %v", senderName)
+	p.log.Info("Remove of sender deployment: ", senderName)
 
-	err := p.client.Kube.CoreV1().Pods(p.client.Namespace).
+	err := p.client.Kube.AppsV1().
+		Deployments(p.client.Namespace).
 		Delete(ctx, senderName, metav1.DeleteOptions{})
 	ensure.NoError(err)
 }
