fd20fbcbdbdade5e1c37d24f58ee2c48c22f7b5d
Victor Agababov <vagababov@gmail.com>
2020-11-13 09:20:01 -0800

    Various small test nits (#4520)
    * Various small test nits
    
    * 1
    
    * 2
    
    * 3

--- pkg/apis/sources/v1/container_conversion_test.go
+++ pkg/apis/sources/v1/container_conversion_test.go
@@ -25,10 +25,10 @@ func TestContainerSourceConversionBadType(t *testing.T) {
 	good, bad := &ContainerSource{}, &testObject{}
 
 	if err := good.ConvertTo(context.Background(), bad); err == nil {
-		t.Errorf("ConvertTo() = %#v, wanted error", bad)
+		t.Error("ConvertTo() = nil, wanted error")
 	}
 
 	if err := good.ConvertFrom(context.Background(), bad); err == nil {
-		t.Errorf("ConvertFrom() = %#v, wanted error", good)
+		t.Error("ConvertFrom() = nil, wanted error")
 	}
 }
