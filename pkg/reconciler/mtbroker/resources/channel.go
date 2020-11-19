3bf5a51d0e9d646b1254d650f8492cea68337957
Scott Nichols <snichols@vmware.com>
2020-11-19 13:19:52 -0800

    [Release 0.19] Fix MTBroker Channel and IMC Channel Services length issue. (#4559)
    * Make channels and services of the correct length
    
    * rm fmt

--- pkg/reconciler/mtbroker/resources/channel.go
+++ pkg/reconciler/mtbroker/resources/channel.go
@@ -18,7 +18,6 @@ package resources
 
 import (
 	"encoding/json"
-	"fmt"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
@@ -30,7 +29,7 @@ import (
 // BrokerChannelName creates a name for the Channel for a Broker for a given
 // Channel type.
 func BrokerChannelName(brokerName, channelType string) string {
-	return fmt.Sprintf("%s-kne-%s", brokerName, channelType)
+	return kmeta.ChildName(brokerName, "-kne-"+channelType)
 }
 
 // test
