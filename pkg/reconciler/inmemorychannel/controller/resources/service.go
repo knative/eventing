36a112f75d98955563f6a92f721cdde06bba815f
Scott Nichols <snichols@vmware.com>
2020-11-19 12:33:44 -0800

    Make channels and services of the correct length

--- pkg/reconciler/inmemorychannel/controller/resources/service.go
+++ pkg/reconciler/inmemorychannel/controller/resources/service.go
@@ -1,3 +1,19 @@
+/*
+Copyright 2020 The Knative Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package resources
 
 import (
@@ -21,7 +37,7 @@ const (
 type K8sServiceOption func(*corev1.Service) error
 
 func CreateChannelServiceName(name string) string {
-	return fmt.Sprintf("%s-kn-channel", name)
+	return kmeta.ChildName(name, "-kn-channel")
 }
 
 // ExternalService is a functional option for CreateK8sService to create a K8s service of type ExternalName
