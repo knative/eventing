b75d03a6339f6da3177fb2648277b46739f1cbe3
Ansu Varghese <avarghese@us.ibm.com>
2021-01-28 05:55:31 -0500

    Adding new source conformance testcase for CRD Source Registry Spec (#4780)
    * Adding new source conformance testcase for CRD Source Registry Spec
    
    Signed-off-by: Ansu Varghese <avarghese@us.ibm.com>
    
    * Fixing code style check
    
    Signed-off-by: Ansu Varghese <avarghese@us.ibm.com>

--- test/conformance/helpers/metadata.go
+++ test/conformance/helpers/metadata.go
@@ -18,6 +18,7 @@ package helpers
 
 import (
 	"context"
+	"encoding/json"
 
 	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -43,4 +44,62 @@ func objectHasRequiredLabel(client *testlib.Client, object metav1.TypeMeta, key 
 		client.T.Errorf("error while getting %q:%v", object, err)
 	}
 	return crd.Labels[key] == value
+}
+
+type EventTypesAnnotationJsonValue struct {
+	Type        string `json:"type"`
+	Schema      string `json:"schema,omitempty"`
+	Description string `json:"description,omitempty"`
+}
+
+func ValidateAnnotations(client *testlib.Client, object metav1.TypeMeta, annotationsKey string) {
+	if !objectHasValidAnnotation(client, object, annotationsKey) {
+		//CRD Spec says Source CRDs "SHOULD" use a standard annotation - so can't enforce it. Nothing to do here
+		//client.T.Fatalf("can't find annotation '%s' in CRD %q", annotationsKey, object)
+		client.T.Logf("can't find annotation '%s' in CRD %q", annotationsKey, object)
+	}
+}
+
+func objectHasValidAnnotation(client *testlib.Client, object metav1.TypeMeta, key string) bool {
+	gvr, _ := meta.UnsafeGuessKindToResource(object.GroupVersionKind())
+	crdName := gvr.Resource + "." + gvr.Group
+
+	crd, err := client.Apiextensions.CustomResourceDefinitions().Get(context.Background(), crdName, metav1.GetOptions{
+		TypeMeta: metav1.TypeMeta{},
+	})
+	if err != nil {
+		client.T.Errorf("error while getting %q:%v", object, err)
+	}
+
+	if annotationsValue, found := crd.Annotations[key]; found {
+		client.T.Logf("found annotation key %s in object %q", key, object)
+		if !annotationIsValidJSONArray(client, annotationsValue) {
+			client.T.Fatalf("annotation value %s in CRD %q does not meet CRD Specs", annotationsValue, object)
+		}
+		return true
+	}
+	return false
+}
+
+func annotationIsValidJSONArray(client *testlib.Client, value string) bool {
+	if !json.Valid([]byte(value)) {
+		client.T.Fatalf("invalid JSON array in annotation %s", value)
+		return false
+	}
+
+	var valueUnmarshalled []EventTypesAnnotationJsonValue
+	if err := json.Unmarshal([]byte(value), &valueUnmarshalled); err != nil {
+		client.T.Fatalf("error unmarshalling JSON array %s to Go type", value)
+		return false
+	}
+
+	//Each object contain fields type: String (Mandatory) schema: String (Optional) description: String (Optional)
+	for _, valueObject := range valueUnmarshalled {
+		if valueObject.Type == "" {
+			client.T.Fatal("required Type field in JSON object is empty")
+			return false
+		}
+	}
+
+	return true
 }
