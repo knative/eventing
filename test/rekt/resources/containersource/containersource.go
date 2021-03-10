43610eb2eb7a445a5f78a55cccd41e6aec4efa7f
Scott Nichols <snichols@vmware.com>
2021-03-10 10:59:31 -0800

    starting to add  container source.

---
+++ test/rekt/resources/containersource/containersource.go
@@ -0,0 +1,101 @@
+/*
+Copyright 2021 The Knative Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package containersource
+
+import (
+	"context"
+	"time"
+
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	duckv1 "knative.dev/pkg/apis/duck/v1"
+	"knative.dev/reconciler-test/pkg/feature"
+	"knative.dev/reconciler-test/pkg/k8s"
+	"knative.dev/reconciler-test/pkg/manifest"
+)
+
+type CfgFn func(map[string]interface{})
+
+func Gvr() schema.GroupVersionResource {
+	return schema.GroupVersionResource{Group: "sources.knative.dev", Version: "v1", Resource: "containersources"}
+}
+
+// Install will create a ContainerSource resource, augmented with the config fn options.
+func Install(name string, sink *duckv1.Destination, opts ...CfgFn) feature.StepFn {
+	cfg := map[string]interface{}{
+		"name": name,
+		"sink": sink,
+	}
+	for _, fn := range opts {
+		fn(cfg)
+	}
+	return func(ctx context.Context, t feature.T) {
+		if _, err := manifest.InstallLocalYaml(ctx, cfg); err != nil {
+			t.Fatal(err)
+		}
+	}
+}
+
+type EzTemplate struct {
+	Image string
+	Args  []string
+	Env   map[string]string
+}
+
+// WithTemplate adds the template related config to a ContainerSource spec.
+func WithTemplate(template *EzTemplate) CfgFn {
+	return func(cfg map[string]interface{}) {
+		if _, set := cfg["template"]; !set {
+			cfg["template"] = map[string]interface{}{}
+		}
+		ceOverrides := cfg["ceOverrides"].(map[string]interface{})
+
+		if extensions != nil {
+			if _, set := ceOverrides["extensions"]; !set {
+				ceOverrides["ref"] = map[string]interface{}{}
+			}
+			ceExt := ceOverrides["extensions"].(map[string]interface{})
+			for k, v := range extensions {
+				ceExt[k] = v
+			}
+		}
+	}
+}
+
+// WithExtensions adds the ceOVerrides related config to a ContainerSource spec.
+func WithExtensions(extensions map[string]string) CfgFn {
+	return func(cfg map[string]interface{}) {
+		if _, set := cfg["ceOverrides"]; !set {
+			cfg["ceOverrides"] = map[string]interface{}{}
+		}
+		ceOverrides := cfg["ceOverrides"].(map[string]interface{})
+
+		if extensions != nil {
+			if _, set := ceOverrides["extensions"]; !set {
+				ceOverrides["ref"] = map[string]interface{}{}
+			}
+			ceExt := ceOverrides["extensions"].(map[string]interface{})
+			for k, v := range extensions {
+				ceExt[k] = v
+			}
+		}
+	}
+}
+
+// IsReady tests to see if a PingSource becomes ready within the time given.
+func IsReady(name string, interval, timeout time.Duration) feature.StepFn {
+	return k8s.IsReady(Gvr(), name, interval, timeout)
+}
