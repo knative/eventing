df48abb414c4f22effb4e25a54b544d5a90f0e9b
Scott Nichols <snichols@vmware.com>
2021-03-17 13:20:54 -0700

    Schema tool for Eventing v1 (#5087)
    * adding schema tool to eventing for eventing v1 resources
    
    * run tool for broker
    
    * pull main discriptions back
    
    * add conditions back
    
    * trigger.status.subscriberUri is optional

---
+++ cmd/schema/main.go
@@ -0,0 +1,36 @@
+/*
+Copyright 2021 The Knative Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"log"
+
+	"knative.dev/hack/schema/commands"
+	"knative.dev/hack/schema/registry"
+
+	eventingv1 "knative.dev/eventing/pkg/apis/eventing/v1"
+)
+
+// schema is a tool to dump the schema for Eventing resources.
+func main() {
+	registry.Register(&eventingv1.Broker{})
+	registry.Register(&eventingv1.Trigger{})
+
+	if err := commands.New("knative.dev/eventing").Execute(); err != nil {
+		log.Fatal("Error during command execution: ", err)
+	}
+}
--- go.mod
+++ go.mod
@@ -31,7 +31,6 @@ require (
 	go.uber.org/zap v1.16.0
 	golang.org/x/sync v0.0.0-20201207232520-09787c993a3a
 	google.golang.org/grpc v1.36.0
-	gopkg.in/yaml.v3 v3.0.0-20200615113413-eeeca48fe776 // indirect
 	k8s.io/api v0.19.7
 	k8s.io/apiextensions-apiserver v0.19.7
 	k8s.io/apimachinery v0.19.7
@@ -39,6 +38,7 @@ require (
 	k8s.io/client-go v0.19.7
 	k8s.io/utils v0.0.0-20200729134348-d5654de09c73
 	knative.dev/hack v0.0.0-20210309141825-9b73a256fd9a
+	knative.dev/hack/schema v0.0.0-20210309141825-9b73a256fd9a
 	knative.dev/pkg v0.0.0-20210315160101-6a33a1ab29ac
 	knative.dev/reconciler-test v0.0.0-20210317002242-cfb4db568965
 	sigs.k8s.io/yaml v1.2.0
