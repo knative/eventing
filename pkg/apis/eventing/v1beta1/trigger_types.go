6b9ff096a39c9527888b47ba04ffde088e86c5f8
Scott Nichols <snichols@vmware.com>
2021-03-17 12:43:38 -0700

    trigger.status.subscriberUri is optional

--- pkg/apis/eventing/v1beta1/trigger_types.go
+++ pkg/apis/eventing/v1beta1/trigger_types.go
@@ -120,6 +120,7 @@ type TriggerStatus struct {
 	duckv1.Status `json:",inline"`
 
 	// SubscriberURI is the resolved URI of the receiver for this Trigger.
+	// +optional
 	SubscriberURI *apis.URL `json:"subscriberUri,omitempty"`
 }
 
