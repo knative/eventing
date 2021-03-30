91526a6bb56c181ff1ef23173749bded7e2f83cf
Scott Nichols <snichols@vmware.com>
2021-03-30 08:58:39 -0700

    adding spec language for channel data plane tests

--- test/rekt/features/channel/data_plane.go
+++ test/rekt/features/channel/data_plane.go
@@ -36,9 +36,58 @@ func DataPlaneChannel(channelName string) *feature.Feature {
 
 	f.Setup("Set Channel Name", setChannelableName(channelName))
 
-	f.Stable("Channel Subscriber Status").
-		Must("The ready field of the subscriber identified by its uid MUST be set to True when the subscription is ready to be processed",
-			todo)
+	f.Stable("Input").
+		Must("Every Channel MUST expose either an HTTP or HTTPS endpoint.", todo).
+		Must("The endpoint(s) MUST conform to 0.3 or 1.0 CloudEvents specification.", todo).
+		MustNot("The Channel MUST NOT perform an upgrade of the passed in version. It MUST emit the event with the same version.", todo).
+		Must("It MUST support both Binary Content Mode and Structured Content Mode of the HTTP Protocol Binding for CloudEvents.", todo).
+		May("When dispatching the event, the channel MAY use a different HTTP Message mode of the one used by the event.", todo).
+		// For example, It MAY receive an event in Structured Content Mode and dispatch in Binary Content Mode.
+		May("The HTTP(S) endpoint MAY be on any port, not just the standard 80 and 443.", todo).
+		May("Channels MAY expose other, non-HTTP endpoints in addition to HTTP at their discretion.", todo)
+		// (e.g. expose a gRPC endpoint to accept events)
+
+	f.Stable("Generic").
+		Must("If a Channel receives an event queueing request and is unable to parse a valid CloudEvent, then it MUST reject the request.", todo)
+
+	f.Stable("HTTP").
+		Must("Channels MUST reject all HTTP event queueing requests with a method other than POST responding with HTTP status code 405 Method Not Supported.", todo).
+		Must("The HTTP event queueing request's URL MUST correspond to a single, unique Channel at any given moment in time.", todo).
+		May("This MAY be done via the host, path, query string, or any combination of these.", todo).
+		Must("If a Channel receives a request that does not correspond to a known channel, then it MUST respond with a 404 Not Found.", todo).
+		Must("The Channel MUST respond with 202 Accepted if the event queueing request is accepted by the server.", todo).
+		Must("If a Channel receives an event queueing request and is unable to parse a valid CloudEvent, then it MUST respond with 400 Bad Request.", todo)
+
+	f.Stable("Output").
+		Must("Channels MUST output CloudEvents.", todo).
+		Must("The output MUST match the CloudEvent version of the Input.", todo).
+		MustNot("Channels MUST NOT alter an event that goes through them.", todo).
+		Must("All CloudEvent attributes, including the data attribute, MUST be received at the subscriber identical to how they were received by the Channel.", todo).
+		// except:  The extension attribute knativehistory, which the channel MAY modify to append its hostname
+		Should("Every Channel SHOULD support sending events via Binary Content Mode or Structured Content Mode of the HTTP Protocol Binding for CloudEvents.", todo).
+		Must("Channels MUST send events to all subscribers which are marked with a status of ready: True in the channel's status.subscribers.", todo).
+		Must("The events MUST be sent to the subscriberURI field of spec.subscribers.", todo)
+		// Each channel implementation will have its own quality of service guarantees (e.g. at least once, at most once, etc) which SHOULD be documented.
+
+	f.Stable("Retries").
+		Should("Channels SHOULD retry resending CloudEvents when they fail to either connect or send CloudEvents to subscribers.", todo).
+		Should("Channels SHOULD support various retry configuration parameters: [the maximum number of retries]", todo).
+		Should("Channels SHOULD support various retry configuration parameters: [the time in-between retries]", todo).
+		Should("Channels SHOULD support various retry configuration parameters: [the backoff rate]", todo)
+
+	f.Stable("Observability").
+		Should("Channels SHOULD expose a variety of metrics: [Number of malformed incoming event queueing events (400 Bad Request responses)]", todo).
+		Should("Channels SHOULD expose a variety of metrics: [Number of accepted incoming event queuing events (202 Accepted responses)]", todo).
+		Should("Channels SHOULD expose a variety of metrics: [Number of egress CloudEvents produced (with the former metric, used to derive channel queue size)]", todo).
+		Should("Metrics SHOULD be enabled by default, with a configuration parameter included to disable them if desired.", todo).
+		Must("The Channel MUST recognize and pass through all tracing information from sender to subscribers using W3C Tracecontext.", todo).
+		Should("The Channel SHOULD sample and write traces to the location specified in config-tracing.", todo).
+		Should("Spans emitted by the Channel SHOULD follow the OpenTelemetry Semantic Conventions for Messaging Systems whenever possible.", todo).
+		Should("Spans emitted by the Channel SHOULD set the knative attributes.", todo)
+		// messaging.system: "knative"
+		// messaging.destination: url to which the event is being routed
+		// messaging.protocol: the name of the underlying transport protocol
+		// messaging.message_id: the event ID
 
 	return f
 }
