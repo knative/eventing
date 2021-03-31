5aacf4fdcd453c469a38aa261fe7a23637d5b959
Scott Nichols <snichols@vmware.com>
2021-03-31 10:27:22 -0700

    Channel Data Plane Conformance Template (#5183)
    * break apart helpers
    
    * adding spec language for channel data plane tests

--- test/rekt/features/channel/control_plane.go
+++ test/rekt/features/channel/control_plane.go
@@ -18,33 +18,16 @@ package channel
 
 import (
 	"context"
-	"fmt"
-	"strings"
 
 	"github.com/google/go-cmp/cmp"
-	"go.uber.org/zap"
-	authv1 "k8s.io/api/authorization/v1"
-	apiextv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/runtime/schema"
-	"k8s.io/apimachinery/pkg/types"
-	"k8s.io/client-go/dynamic"
 	duckv1 "knative.dev/eventing/pkg/apis/duck/v1"
 	"knative.dev/eventing/pkg/apis/messaging"
-	messagingclientsetv1 "knative.dev/eventing/pkg/client/clientset/versioned/typed/messaging/v1"
-	eventingclient "knative.dev/eventing/pkg/client/injection/client"
 	"knative.dev/eventing/test/rekt/features/knconf"
 	"knative.dev/eventing/test/rekt/resources/account_role"
 	"knative.dev/eventing/test/rekt/resources/channel_impl"
 	"knative.dev/pkg/apis"
 	"knative.dev/pkg/apis/duck"
-	apiextensionsclient "knative.dev/pkg/client/injection/apiextensions/client"
-	kubeclient "knative.dev/pkg/client/injection/kube/client"
-	"knative.dev/pkg/injection/clients/dynamicclient"
-	"knative.dev/reconciler-test/pkg/environment"
 	"knative.dev/reconciler-test/pkg/feature"
-	"knative.dev/reconciler-test/pkg/state"
 )
 
 func ControlPlaneConformance(channelName string) *feature.FeatureSet {
@@ -57,12 +40,6 @@ func ControlPlaneConformance(channelName string) *feature.FeatureSet {
 
 	return fs
 }
-
-func todo(ctx context.Context, t feature.T) {
-	t.Log("TODO, Implement this.")
-}
-
-func noop(ctx context.Context, t feature.T) {}
 
 func ControlPlaneChannel(channelName string) *feature.Feature {
 	f := feature.NewFeatureNamed("Conformance")
@@ -136,143 +113,6 @@ func ControlPlaneChannel(channelName string) *feature.Feature {
 			todo)
 
 	return f
-}
-
-type EventingClient struct {
-	Channels    messagingclientsetv1.ChannelInterface
-	ChannelImpl dynamic.ResourceInterface
-}
-
-func Client(ctx context.Context) *EventingClient {
-	env := environment.FromContext(ctx)
-
-	mc := eventingclient.Get(ctx).MessagingV1()
-	dc := dynamicclient.Get(ctx)
-
-	return &EventingClient{
-		Channels:    mc.Channels(env.Namespace()),
-		ChannelImpl: dc.Resource(channel_impl.GVR()).Namespace(env.Namespace()),
-	}
-}
-
-const (
-	ChannelableNameKey = "channelableName"
-)
-
-func setChannelableName(name string) feature.StepFn {
-	return func(ctx context.Context, t feature.T) {
-		state.SetOrFail(ctx, t, ChannelableNameKey, name)
-	}
-}
-
-func getChannelable(ctx context.Context, t feature.T) *duckv1.Channelable {
-	c := Client(ctx)
-	name := state.GetStringOrFail(ctx, t, ChannelableNameKey)
-
-	obj, err := c.ChannelImpl.Get(ctx, name, metav1.GetOptions{})
-	if err != nil {
-		t.Errorf("failed to get ChannelImpl, %v", err)
-	}
-
-	channel := &duckv1.Channelable{}
-	if err = runtime.DefaultUnstructuredConverter.FromUnstructured(obj.Object, channel); err != nil {
-		t.Fatalf("Failed to convert channelImpl to Channelable: %v", err)
-	}
-	channel.ResourceVersion = channel_impl.GVR().Version
-	channel.APIVersion = channel_impl.GVR().GroupVersion().String()
-
-	return channel
-}
-
-func patchChannelable(ctx context.Context, t feature.T, before, after *duckv1.Channelable) {
-	patch, err := duck.CreateMergePatch(before, after)
-	if err != nil {
-		t.Fatalf("Failed to create merge patch: %v", err)
-	}
-	// If there is nothing to patch, we are good, just return.
-	// Empty patch is {}, hence we check for that.
-	if len(patch) <= 2 {
-		return
-	}
-
-	_, err = Client(ctx).ChannelImpl.Patch(ctx, before.GetName(), types.MergePatchType, patch, metav1.PatchOptions{})
-	if err != nil {
-		t.Fatal("Failed to patch the ChannelImpl", zap.Error(err), zap.Any("patch", patch))
-	}
-}
-
-func crdOfChannel(ctx context.Context, t feature.T) *apiextv1.CustomResourceDefinition {
-	gvr := channel_impl.GVR()
-	name := strings.Join([]string{gvr.Resource, gvr.Group}, ".")
-
-	crd, err := apiextensionsclient.Get(ctx).ApiextensionsV1().CustomResourceDefinitions().Get(ctx, name, metav1.GetOptions{})
-	if err != nil {
-		t.Log(err)
-		t.FailNow()
-	}
-	return crd
-}
-
-func crdOfChannelIsNamespaced(ctx context.Context, t feature.T) {
-	crd := crdOfChannel(ctx, t)
-
-	if crd.Spec.Scope != apiextv1.NamespaceScoped {
-		t.Logf("%q CRD is not namespaced", crd.Name)
-		t.Fail()
-	}
-}
-
-func crdOfChannelIsLabeled(key, want string) feature.StepFn {
-	return func(ctx context.Context, t feature.T) {
-		crd := crdOfChannel(ctx, t)
-
-		if got, found := crd.Labels[key]; !found {
-			t.Logf("%q CRD does not have label %q", crd.Name, key)
-			t.Fail()
-		} else if got != want {
-			t.Logf("%q CRD label %q expected to be %s, got %s", crd.Name, key, want, got)
-			t.Fail()
-		}
-	}
-}
-
-func crdOfChannelHasCategory(want string) feature.StepFn {
-	return func(ctx context.Context, t feature.T) {
-		crd := crdOfChannel(ctx, t)
-
-		for _, got := range crd.Spec.Names.Categories {
-			if got == want {
-				// Success!
-				return
-			}
-		}
-		t.Logf("%q CRD does not have Category %q", crd.Name, want)
-	}
-}
-
-// TODO: move this to framework?
-func ServiceAccountSubjectAccessReviewAllowedOrFail(ctx context.Context, t feature.T, gvr schema.GroupVersionResource, subresource string, saName string, verb string) {
-	env := environment.FromContext(ctx)
-	kube := kubeclient.Get(ctx)
-
-	r, err := kube.AuthorizationV1().SubjectAccessReviews().Create(ctx, &authv1.SubjectAccessReview{
-		Spec: authv1.SubjectAccessReviewSpec{
-			User: fmt.Sprintf("system:serviceaccount:%s:%s", env.Namespace(), saName),
-			ResourceAttributes: &authv1.ResourceAttributes{
-				Verb:        verb,
-				Group:       gvr.Group,
-				Version:     gvr.Version,
-				Resource:    gvr.Resource,
-				Subresource: subresource,
-			},
-		},
-	}, metav1.CreateOptions{})
-	if err != nil {
-		t.Fatalf("Error while checking if %q is not allowed on %s.%s/%s subresource:%q. err: %q", verb, gvr.Resource, gvr.Group, gvr.Version, subresource, err)
-	}
-	if !r.Status.Allowed {
-		t.Fatalf("Operation %q is not allowed on %s.%s/%s subresource:%q", verb, gvr.Resource, gvr.Group, gvr.Version, subresource)
-	}
 }
 
 func serviceAccountIsChannelableManipulator(name string) feature.StepFn {
---
+++ test/rekt/features/channel/data_plane.go
@@ -0,0 +1,93 @@
+/*
+Copyright 2021 The Knative Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package channel
+
+import (
+	"knative.dev/reconciler-test/pkg/feature"
+)
+
+func DataPlaneConformance(channelName string) *feature.FeatureSet {
+	fs := &feature.FeatureSet{
+		Name: "Knative Channel Specification - Data Plane",
+		Features: []feature.Feature{
+			*DataPlaneChannel(channelName),
+		},
+	}
+
+	return fs
+}
+
+func DataPlaneChannel(channelName string) *feature.Feature {
+	f := feature.NewFeatureNamed("Conformance")
+
+	f.Setup("Set Channel Name", setChannelableName(channelName))
+
+	f.Stable("Input").
+		Must("Every Channel MUST expose either an HTTP or HTTPS endpoint.", todo).
+		Must("The endpoint(s) MUST conform to 0.3 or 1.0 CloudEvents specification.", todo).
+		MustNot("The Channel MUST NOT perform an upgrade of the passed in version. It MUST emit the event with the same version.", todo).
+		Must("It MUST support both Binary Content Mode and Structured Content Mode of the HTTP Protocol Binding for CloudEvents.", todo).
+		May("When dispatching the event, the channel MAY use a different HTTP Message mode of the one used by the event.", todo).
+		// For example, It MAY receive an event in Structured Content Mode and dispatch in Binary Content Mode.
+		May("The HTTP(S) endpoint MAY be on any port, not just the standard 80 and 443.", todo).
+		May("Channels MAY expose other, non-HTTP endpoints in addition to HTTP at their discretion.", todo)
+		// (e.g. expose a gRPC endpoint to accept events)
+
+	f.Stable("Generic").
+		Must("If a Channel receives an event queueing request and is unable to parse a valid CloudEvent, then it MUST reject the request.", todo)
+
+	f.Stable("HTTP").
+		Must("Channels MUST reject all HTTP event queueing requests with a method other than POST responding with HTTP status code 405 Method Not Supported.", todo).
+		Must("The HTTP event queueing request's URL MUST correspond to a single, unique Channel at any given moment in time.", todo).
+		May("This MAY be done via the host, path, query string, or any combination of these.", todo).
+		Must("If a Channel receives a request that does not correspond to a known channel, then it MUST respond with a 404 Not Found.", todo).
+		Must("The Channel MUST respond with 202 Accepted if the event queueing request is accepted by the server.", todo).
+		Must("If a Channel receives an event queueing request and is unable to parse a valid CloudEvent, then it MUST respond with 400 Bad Request.", todo)
+
+	f.Stable("Output").
+		Must("Channels MUST output CloudEvents.", todo).
+		Must("The output MUST match the CloudEvent version of the Input.", todo).
+		MustNot("Channels MUST NOT alter an event that goes through them.", todo).
+		Must("All CloudEvent attributes, including the data attribute, MUST be received at the subscriber identical to how they were received by the Channel.", todo).
+		// except:  The extension attribute knativehistory, which the channel MAY modify to append its hostname
+		Should("Every Channel SHOULD support sending events via Binary Content Mode or Structured Content Mode of the HTTP Protocol Binding for CloudEvents.", todo).
+		Must("Channels MUST send events to all subscribers which are marked with a status of ready: True in the channel's status.subscribers.", todo).
+		Must("The events MUST be sent to the subscriberURI field of spec.subscribers.", todo)
+		// Each channel implementation will have its own quality of service guarantees (e.g. at least once, at most once, etc) which SHOULD be documented.
+
+	f.Stable("Retries").
+		Should("Channels SHOULD retry resending CloudEvents when they fail to either connect or send CloudEvents to subscribers.", todo).
+		Should("Channels SHOULD support various retry configuration parameters: [the maximum number of retries]", todo).
+		Should("Channels SHOULD support various retry configuration parameters: [the time in-between retries]", todo).
+		Should("Channels SHOULD support various retry configuration parameters: [the backoff rate]", todo)
+
+	f.Stable("Observability").
+		Should("Channels SHOULD expose a variety of metrics: [Number of malformed incoming event queueing events (400 Bad Request responses)]", todo).
+		Should("Channels SHOULD expose a variety of metrics: [Number of accepted incoming event queuing events (202 Accepted responses)]", todo).
+		Should("Channels SHOULD expose a variety of metrics: [Number of egress CloudEvents produced (with the former metric, used to derive channel queue size)]", todo).
+		Should("Metrics SHOULD be enabled by default, with a configuration parameter included to disable them if desired.", todo).
+		Must("The Channel MUST recognize and pass through all tracing information from sender to subscribers using W3C Tracecontext.", todo).
+		Should("The Channel SHOULD sample and write traces to the location specified in config-tracing.", todo).
+		Should("Spans emitted by the Channel SHOULD follow the OpenTelemetry Semantic Conventions for Messaging Systems whenever possible.", todo).
+		Should("Spans emitted by the Channel SHOULD set the knative attributes.", todo)
+		// messaging.system: "knative"
+		// messaging.destination: url to which the event is being routed
+		// messaging.protocol: the name of the underlying transport protocol
+		// messaging.message_id: the event ID
+
+	return f
+}
