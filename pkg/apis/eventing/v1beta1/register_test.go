9fa2c4de5bb705ec8bff214ec474eaad3e9e927f
Ville Aikas <vaikas@vmware.com>
2021-04-05 10:47:50 -0700

    add ut for register

---
+++ pkg/apis/eventing/v1beta1/register_test.go
@@ -0,0 +1,35 @@
+/*
+Copyright 2021 The Knative Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1beta1
+
+import (
+	"testing"
+)
+
+func TestKind(t *testing.T) {
+	schemaGroupKind := Kind("EventType")
+	if schemaGroupKind.Kind != "EventType" || schemaGroupKind.Group != "eventing.knative.dev" {
+		t.Errorf("Unexpected GroupKind: %+v", schemaGroupKind)
+	}
+}
+
+func TestResource(t *testing.T) {
+	schemaGroupResource := Resource("EventType")
+	if schemaGroupResource.Group != "eventing.knative.dev" || schemaGroupResource.Resource != "EventType" {
+		t.Errorf("Unexpected GroupResource: %+v", schemaGroupResource)
+	}
+}
