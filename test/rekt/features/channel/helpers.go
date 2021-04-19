30e0d448fa70ba3d9ceb165b08fa5f3a57499167
Scott Nichols <snichols@vmware.com>
2021-04-19 10:58:48 -0700

    drop unused function

--- test/rekt/features/channel/helpers.go
+++ test/rekt/features/channel/helpers.go
@@ -28,7 +28,6 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/types"
-	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/client-go/dynamic"
 	duckv1 "knative.dev/eventing/pkg/apis/duck/v1"
 	messagingclientsetv1 "knative.dev/eventing/pkg/client/clientset/versioned/typed/messaging/v1"
@@ -91,23 +90,6 @@ func getChannelable(ctx context.Context, t feature.T) *duckv1.Channelable {
 	channel.APIVersion = channel_impl.GVR().GroupVersion().String()
 
 	return channel
-}
-
-func getChannelableWithStatus(ctx context.Context, t feature.T) *duckv1.Channelable {
-	interval, timeout := environment.PollTimingsFromContext(ctx)
-	var ch *duckv1.Channelable
-	err := wait.PollImmediate(interval, timeout, func() (bool, error) {
-		ch = getChannelable(ctx, t)
-		if ch.Status.ObservedGeneration != ch.Generation {
-			// keep polling.
-			return false, nil
-		}
-		return true, nil
-	})
-	if err != nil {
-		t.Fatal(err)
-	}
-	return ch
 }
 
 func patchChannelable(ctx context.Context, t feature.T, before, after *duckv1.Channelable) {
