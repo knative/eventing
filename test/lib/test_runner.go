c47c13b58792eddce58d85f07156c6f82ef5c248
Pierangelo Di Pilato <pierangelodipilato@gmail.com>
2021-09-09 19:27:18 +0200

    Increase MaxNamespaceSkip in test runner
    ```
    test_runner.go:163: Couldn't initialize clients: unable to find available namespace
    ```

--- test/lib/test_runner.go
+++ test/lib/test_runner.go
@@ -48,7 +48,7 @@ import (
 const (
 	podLogsDir         = "pod-logs"
 	testPullSecretName = "kn-eventing-test-pull-secret"
-	MaxNamespaceSkip   = 50
+	MaxNamespaceSkip   = 100
 	MaxRetries         = 5
 	RetrySleepDuration = 2 * time.Second
 )
