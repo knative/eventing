953ef3342869d199ed8bbbce19caab9cae2aa5a0
gabriel <gfreites@vmware.com>
2021-10-06 10:19:58 -0500

    created basic template for broker prefer header

---
+++ test/e2e/helpers/broker_prefer_reply_header_helper.go
@@ -0,0 +1,95 @@
+/*
+ * Copyright 2021 The Knative Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package helpers
+
+import (
+	"context"
+	"testing"
+
+	cloudevents "github.com/cloudevents/sdk-go/v2"
+	"github.com/google/uuid"
+
+	testlib "knative.dev/eventing/test/lib"
+	"knative.dev/eventing/test/lib/recordevents"
+	"knative.dev/eventing/test/lib/resources"
+)
+
+func BrokerPreferHeaderCheck(ctx context.Context, t *testing.T, creator BrokerCreator, options ...recordevents.EventRecordOption) {
+	const (
+		triggerName = "trigger"
+		eventRecord = "event-record"
+		senderName  = "sender"
+
+		eventType   = "type"
+		eventSource = "http://source.com"
+		eventBody   = `{"msg":"broker-redelivery"}`
+	)
+
+	tests := []struct {
+		name string
+	}{
+		{
+			name: "test messag without prefer header",
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			client := testlib.Setup(t, true)
+			defer testlib.TearDown(client)
+
+			brokerName := creator(client, "v1")
+
+			// Wait for broker ready.
+			client.WaitForResourceReadyOrFail(brokerName, testlib.BrokerTypeMeta)
+
+			// Create event tracker that should receive all events.
+			allEventTracker, _ := recordevents.StartEventRecordOrFail(
+				ctx,
+				client,
+				eventRecord,
+				options...,
+			)
+
+			client.CreateTriggerOrFail(
+				triggerName,
+				resources.WithBroker(brokerName),
+				resources.WithSubscriberServiceRefForTrigger(eventRecord),
+			)
+
+			client.WaitForAllTestResourcesReadyOrFail(ctx)
+
+			// send CloudEvent to the broker
+			eventToSend := cloudevents.NewEvent()
+			eventToSend.SetID(uuid.New().String())
+			eventToSend.SetType(eventType)
+			eventToSend.SetSource(eventSource)
+			if err := eventToSend.SetData(cloudevents.ApplicationJSON, []byte(eventBody)); err != nil {
+				t.Fatal("Cannot set the payload of the event:", err.Error())
+			}
+
+			client.SendEventToAddressable(ctx, senderName, brokerName, testlib.BrokerTypeMeta, eventToSend)
+
+			allEventTracker.AssertAtLeast(
+				1,
+				recordevents.MatchKind(recordevents.EventReceived),
+				recordevents.HasAdditionalHeader("Prefer", "Reply"),
+			)
+
+			t.Fatal("TESTT")
+		})
+	}
+}
