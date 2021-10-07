1176375f9f6536e6d5c85d8ec8b84f3f931a7c72
Gabriel Freites <gfreites@vmware.com>
2021-10-07 07:30:10 -0500

    fixed copyright header formats

--- test/e2e/broker_prefer_reply_header_test.go
+++ test/e2e/broker_prefer_reply_header_test.go
@@ -1,7 +1,8 @@
 // +build e2e
 
 /*
-Copyright 2019 The Knative Authors
+Copyright 2021 The Knative Authors
+
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
