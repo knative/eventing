2c76f2c76debe4842125eeb59eb16de23782570d
Gabriel Freites <gfreites@vmware.com>
2021-10-25 07:06:50 -0500

    fixing pr comments

--- test/rekt/features/trigger/trigger_sink_resolution.go
+++ test/rekt/features/trigger/trigger_sink_resolution.go
@@ -53,7 +53,7 @@ func SourceToTriggerSinkWithDLS(triggerName string) *feature.Feature {
 	// Setup Probes
 	f.Setup("install recorder", prober.ReceiverInstall("sink"))
 
-	// Setup data plane
+	// Setup topology
 	f.Setup("install broker with DLS", broker.Install(
 		brokerName,
 		broker.WithEnvConfig()...,
@@ -68,7 +68,7 @@ func SourceToTriggerSinkWithDLS(triggerName string) *feature.Feature {
 	// Resources ready.
 	f.Setup("trigger goes ready", trigger.IsReady(triggerName))
 
-	// Install events after data plane is ready.
+	// Install events after topology is ready.
 	f.Setup("install source", prober.SenderInstall("source"))
 
 	// After we have finished sending.
@@ -110,11 +110,11 @@ func SourceToTriggerSinkWithDLSDontUseBrokers(triggerName string) *feature.Featu
 	f.Setup("install trigger recorder", prober.ReceiverInstall(triggerSinkName))
 	f.Setup("install brokers recorder", prober.ReceiverInstall(brokerSinkName))
 
-	// Setup data plane
+	// Setup topology
 	brokerConfig := append(
 		broker.WithEnvConfig(),
 		delivery.WithDeadLetterSink(prober.AsKReference(brokerSinkName), ""))
-	f.Setup("update broker with DLS", broker.Install(
+	f.Setup("install broker with DLS", broker.Install(
 		brokerName,
 		brokerConfig...,
 	))
@@ -128,7 +128,7 @@ func SourceToTriggerSinkWithDLSDontUseBrokers(triggerName string) *feature.Featu
 	// Resources ready.
 	f.Setup("trigger goes ready", trigger.IsReady(triggerName))
 
-	// Install events after data plane is ready.
+	// Install events after topology is ready.
 	f.Setup("install source", prober.SenderInstall("source"))
 
 	// After we have finished sending.
