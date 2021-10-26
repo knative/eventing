a20ff702ad22fdb403323d6c143ec6b26fb1fa32
knative-automation <automation@knative.team>
2021-10-25 22:57:20 -0700

    upgrade to latest dependencies (#5845)
    bumping knative.dev/pkg ba2b2b1...819d556:
      > 819d556 Allow configuring bounded staleness for dynamic injection. (# 2323)
    bumping knative.dev/hack ced8ce7...02860f5:
      > 02860f5 Update community files (# 87)
    bumping knative.dev/hack/schema ced8ce7...02860f5:
      > 02860f5 Update community files (# 87)
    
    Signed-off-by: Knative Automation <automation@knative.team>

--- pkg/client/injection/informers/sources/v1/apiserversource/apiserversource.go
+++ pkg/client/injection/informers/sources/v1/apiserversource/apiserversource.go
@@ -50,7 +50,7 @@ func withInformer(ctx context.Context) (context.Context, controller.Informer) {
 }
 
 func withDynamicInformer(ctx context.Context) context.Context {
-	inf := &wrapper{client: client.Get(ctx)}
+	inf := &wrapper{client: client.Get(ctx), resourceVersion: injection.GetResourceVersion(ctx)}
 	return context.WithValue(ctx, Key{}, inf)
 }
 
@@ -68,6 +68,8 @@ type wrapper struct {
 	client versioned.Interface
 
 	namespace string
+
+	resourceVersion string
 }
 
 var _ v1.ApiServerSourceInformer = (*wrapper)(nil)
@@ -82,13 +84,21 @@ func (w *wrapper) Lister() sourcesv1.ApiServerSourceLister {
 }
 
 func (w *wrapper) ApiServerSources(namespace string) sourcesv1.ApiServerSourceNamespaceLister {
-	return &wrapper{client: w.client, namespace: namespace}
+	return &wrapper{client: w.client, namespace: namespace, resourceVersion: w.resourceVersion}
+}
+
+// SetResourceVersion allows consumers to adjust the minimum resourceVersion
+// used by the underlying client.  It is not accessible via the standard
+// lister interface, but can be accessed through a user-defined interface and
+// an implementation check e.g. rvs, ok := foo.(ResourceVersionSetter)
+func (w *wrapper) SetResourceVersion(resourceVersion string) {
+	w.resourceVersion = resourceVersion
 }
 
 func (w *wrapper) List(selector labels.Selector) (ret []*apissourcesv1.ApiServerSource, err error) {
 	lo, err := w.client.SourcesV1().ApiServerSources(w.namespace).List(context.TODO(), metav1.ListOptions{
-		LabelSelector: selector.String(),
-		// TODO(mattmoor): Incorporate resourceVersion bounds based on staleness criteria.
+		LabelSelector:   selector.String(),
+		ResourceVersion: w.resourceVersion,
 	})
 	if err != nil {
 		return nil, err
@@ -101,6 +111,6 @@ func (w *wrapper) List(selector labels.Selector) (ret []*apissourcesv1.ApiServer
 
 func (w *wrapper) Get(name string) (*apissourcesv1.ApiServerSource, error) {
 	return w.client.SourcesV1().ApiServerSources(w.namespace).Get(context.TODO(), name, metav1.GetOptions{
-		// TODO(mattmoor): Incorporate resourceVersion bounds based on staleness criteria.
+		ResourceVersion: w.resourceVersion,
 	})
 }
