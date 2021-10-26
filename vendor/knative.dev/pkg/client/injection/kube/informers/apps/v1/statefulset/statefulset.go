02ede23c148f3b9c988d5632b1086a06527d4957
aavarghese <avarghese@us.ibm.com>
2021-10-26 17:47:17 -0400

    Codegen update
    Signed-off-by: aavarghese <avarghese@us.ibm.com>

--- vendor/knative.dev/pkg/client/injection/kube/informers/apps/v1/statefulset/statefulset.go
+++ vendor/knative.dev/pkg/client/injection/kube/informers/apps/v1/statefulset/statefulset.go
@@ -50,7 +50,7 @@ func withInformer(ctx context.Context) (context.Context, controller.Informer) {
 }
 
 func withDynamicInformer(ctx context.Context) context.Context {
-	inf := &wrapper{client: client.Get(ctx)}
+	inf := &wrapper{client: client.Get(ctx), resourceVersion: injection.GetResourceVersion(ctx)}
 	return context.WithValue(ctx, Key{}, inf)
 }
 
@@ -68,6 +68,8 @@ type wrapper struct {
 	client kubernetes.Interface
 
 	namespace string
+
+	resourceVersion string
 }
 
 var _ v1.StatefulSetInformer = (*wrapper)(nil)
@@ -82,13 +84,21 @@ func (w *wrapper) Lister() appsv1.StatefulSetLister {
 }
 
 func (w *wrapper) StatefulSets(namespace string) appsv1.StatefulSetNamespaceLister {
-	return &wrapper{client: w.client, namespace: namespace}
+	return &wrapper{client: w.client, namespace: namespace, resourceVersion: w.resourceVersion}
+}
+
+// SetResourceVersion allows consumers to adjust the minimum resourceVersion
+// used by the underlying client.  It is not accessible via the standard
+// lister interface, but can be accessed through a user-defined interface and
+// an implementation check e.g. rvs, ok := foo.(ResourceVersionSetter)
+func (w *wrapper) SetResourceVersion(resourceVersion string) {
+	w.resourceVersion = resourceVersion
 }
 
 func (w *wrapper) List(selector labels.Selector) (ret []*apiappsv1.StatefulSet, err error) {
 	lo, err := w.client.AppsV1().StatefulSets(w.namespace).List(context.TODO(), metav1.ListOptions{
-		LabelSelector: selector.String(),
-		// TODO(mattmoor): Incorporate resourceVersion bounds based on staleness criteria.
+		LabelSelector:   selector.String(),
+		ResourceVersion: w.resourceVersion,
 	})
 	if err != nil {
 		return nil, err
@@ -101,6 +111,6 @@ func (w *wrapper) List(selector labels.Selector) (ret []*apiappsv1.StatefulSet, 
 
 func (w *wrapper) Get(name string) (*apiappsv1.StatefulSet, error) {
 	return w.client.AppsV1().StatefulSets(w.namespace).Get(context.TODO(), name, metav1.GetOptions{
-		// TODO(mattmoor): Incorporate resourceVersion bounds based on staleness criteria.
+		ResourceVersion: w.resourceVersion,
 	})
 }
