74749a6bc5b05533a4a63652b27a23d4f6004f15
Lukas Kranabetter <l.kranabetter@netconomy.net>
2022-04-21 15:46:01 +0200

    feat(ESB-6090): Fix merge fails.

--- test/rekt/channel_test.go
+++ test/rekt/channel_test.go
@@ -269,25 +269,6 @@ func TestGenericChannelDeadLetterSink(t *testing.T) {
 	env.Test(ctx, t, channel.AsDeadLetterSink(createSubscriberFn))
 }
 
-// TestGenericChannelDeadLetterSink tests if the events that cannot be delivered end up in
-// the dead letter sink.
-func TestGenericChannelDeadLetterSink(t *testing.T) {
-	t.Parallel()
-
-	ctx, env := global.Environment(
-		knative.WithKnativeNamespace(system.Namespace()),
-		knative.WithLoggingConfig,
-		knative.WithTracingConfig,
-		k8s.WithEventListener,
-		environment.Managed(t),
-	)
-
-	createSubscriberFn := func(ref *duckv1.KReference, uri string) manifest.CfgFn {
-		return subscription.WithSubscriber(ref, uri)
-	}
-	env.Test(ctx, t, channel.DeadLetterSinkGenericChannel(createSubscriberFn))
-}
-
 /*
 TestEventTransformationForSubscription tests the following scenario:
 
