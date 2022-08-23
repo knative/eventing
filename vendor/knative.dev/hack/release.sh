291e961ba5e45a4a4333165e73a367d4ed4a382b
knative-automation <automation@knative.team>
2022-08-23 09:39:17 -0400

    upgrade to latest dependencies (#6493)
    bumping knative.dev/hack e9a8475...d35c718:
      > d35c718 [release-1.7] go run... (# 211)
    bumping knative.dev/hack/schema e9a8475...d35c718:
      > d35c718 [release-1.7] go run... (# 211)
    
    Signed-off-by: Knative Automation <automation@knative.team>
    
    Signed-off-by: Knative Automation <automation@knative.team>

--- vendor/knative.dev/hack/release.sh
+++ vendor/knative.dev/hack/release.sh
@@ -107,7 +107,7 @@ export GITHUB_TOKEN=""
 # Convenience function to run the hub tool.
 # Parameters: $1..$n - arguments to hub.
 function hub_tool() {
-  gorun github.com/github/hub@v2.14.2 "$@"
+  go_run github.com/github/hub@v2.14.2 "$@"
 }
 
 # Shortcut to "git push" that handles authentication.
