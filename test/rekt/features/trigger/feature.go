e1866d7660ee917b869df4b206282c1b2eb1f622
Teresaliu <changyan.liu@intel.com>
2022-09-02 14:00:17 +0800

    Knative trigger dependency annotations test to rekt (#6506)
    * Knative trigger dependency annotations test to rekt
    
    * remove old e2e trigger annotation test
    
    * fix sandbox bug

---
+++ test/rekt/features/trigger/feature.go
@@ -0,0 +1,85 @@
+/*
+Copyright 2022 The Knative Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package trigger
+
+import (
+	"context"
+
+	"github.com/cloudevents/sdk-go/v2/test"
+	"knative.dev/eventing/test/rekt/resources/broker"
+	"knative.dev/eventing/test/rekt/resources/pingsource"
+	"knative.dev/eventing/test/rekt/resources/trigger"
+	"knative.dev/reconciler-test/pkg/eventshub"
+	"knative.dev/reconciler-test/pkg/eventshub/assert"
+	"knative.dev/reconciler-test/pkg/feature"
+	"knative.dev/reconciler-test/pkg/manifest"
+	"knative.dev/reconciler-test/resources/svc"
+)
+
+// This test is for avoiding regressions on the trigger dependency annotation functionality.
+func TriggerDependencyAnnotation() *feature.Feature {
+	sink := feature.MakeRandomK8sName("sink")
+	triggerName := feature.MakeRandomK8sName("triggerName")
+
+	f := new(feature.Feature)
+
+	//Install the broker
+	brokerName := feature.MakeRandomK8sName("broker")
+	f.Setup("install broker", broker.Install(brokerName, broker.WithEnvConfig()...))
+	f.Requirement("broker is ready", broker.IsReady(brokerName))
+	f.Requirement("broker is addressable", broker.IsAddressable(brokerName))
+
+	psourcename := "test-ping-source-annotation"
+	dependencyAnnotation := `{"kind":"PingSource","name":"test-ping-source-annotation","apiVersion":"sources.knative.dev/v1"}`
+	annotations := map[string]interface{}{
+		"knative.dev/dependency": dependencyAnnotation,
+	}
+
+	// Add the annotation to trigger and point the Trigger subscriber to the sink svc.
+	f.Setup("install sink", eventshub.Install(sink, eventshub.StartReceiver))
+	cfg := []manifest.CfgFn{
+		trigger.WithSubscriber(svc.AsKReference(sink), ""),
+		trigger.WithAnnotations(annotations),
+	}
+
+	// Install the trigger
+	f.Setup("install trigger", trigger.Install(triggerName, brokerName, cfg...))
+
+	f.Setup("trigger goes ready", trigger.IsReady(triggerName))
+
+	f.Setup("install pingsource", func(ctx context.Context, t feature.T) {
+		brokeruri, err := broker.Address(ctx, brokerName)
+		if err != nil {
+			t.Error("failed to get address of broker", err)
+		}
+		cfg := []manifest.CfgFn{
+			pingsource.WithSchedule("*/1 * * * *"),
+			pingsource.WithSink(nil, brokeruri.String()),
+			pingsource.WithData("text/plain", "Test trigger-annotation"),
+		}
+		pingsource.Install(psourcename, cfg...)(ctx, t)
+	})
+	f.Setup("PingSource goes ready", pingsource.IsReady(psourcename))
+
+	f.Stable("pingsource as event source to test trigger with annotations").
+		Must("delivers events on broker with URI", assert.OnStore(sink).MatchEvent(
+			test.HasType("dev.knative.sources.ping"),
+			test.DataContains("Test trigger-annotation"),
+		).AtLeast(1))
+
+	return f
+}
--- test/rekt/resources/trigger/trigger.go
+++ test/rekt/resources/trigger/trigger.go
@@ -79,6 +79,7 @@ func WithSubscriber(ref *duckv1.KReference, uri string) manifest.CfgFn {
 	}
 }
 
+// Deprecated
 // WithAnnotation adds an annotation to the trigger
 func WithAnnotation(key string, value string) manifest.CfgFn {
 	return func(cfg map[string]interface{}) {
@@ -86,6 +87,46 @@ func WithAnnotation(key string, value string) manifest.CfgFn {
 			cfg["annotations"] = map[string]interface{}{}
 		}
 		(cfg["annotations"].(map[string]interface{}))[key] = value
+	}
+}
+
+// WithAnnotations adds annotations to the trigger
+func WithAnnotations(annotations map[string]interface{}) manifest.CfgFn {
+	return func(cfg map[string]interface{}) {
+		if _, set := cfg["ceOverrides"]; !set {
+			cfg["ceOverrides"] = map[string]interface{}{}
+		}
+		ceOverrides := cfg["ceOverrides"].(map[string]interface{})
+
+		if annotations != nil {
+			if _, set := ceOverrides["annotations"]; !set {
+				ceOverrides["annotations"] = map[string]interface{}{}
+			}
+			ceExt := ceOverrides["annotations"].(map[string]interface{})
+			for k, v := range annotations {
+				ceExt[k] = v
+			}
+		}
+	}
+}
+
+// WithExtensions adds the ceOverrides related config to a ContainerSource spec.
+func WithExtensions(extensions map[string]interface{}) manifest.CfgFn {
+	return func(cfg map[string]interface{}) {
+		if _, set := cfg["ceOverrides"]; !set {
+			cfg["ceOverrides"] = map[string]interface{}{}
+		}
+		ceOverrides := cfg["ceOverrides"].(map[string]interface{})
+
+		if extensions != nil {
+			if _, set := ceOverrides["extensions"]; !set {
+				ceOverrides["extensions"] = map[string]interface{}{}
+			}
+			ceExt := ceOverrides["extensions"].(map[string]interface{})
+			for k, v := range extensions {
+				ceExt[k] = v
+			}
+		}
 	}
 }
 
