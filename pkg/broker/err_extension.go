07ab86366c0456a52778995061b87eb0ecaedb8f
changyan.liu@intel.com <changyan.liu@intel.com>
2022-10-26 15:04:02 +0000

    code format

--- pkg/broker/err_extension.go
+++ pkg/broker/err_extension.go
@@ -13,10 +13,12 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+
 package broker
 
 import "net/url"
 
+// ErrExtensionInfo struct store the broker-filter's destination and responsebody
 type ErrExtensionInfo struct {
 	ErrDestination  *url.URL `json:"errdestination"`
 	ErrResponseBody []byte   `json:"errresponsebody"`
