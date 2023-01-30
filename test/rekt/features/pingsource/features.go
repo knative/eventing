67e990fe51142420e4e6aa6fef0b6dfd923a988e
Teresaliu <changyan.liu@intel.com>
2023-01-30 11:59:16 +0000

    pingsource rekt test code cleanup

--- test/rekt/features/pingsource/features.go
+++ test/rekt/features/pingsource/features.go
@@ -26,7 +26,7 @@ import (
 	"knative.dev/reconciler-test/pkg/eventshub/assert"
 	"knative.dev/reconciler-test/pkg/feature"
 	"knative.dev/reconciler-test/pkg/manifest"
-	"knative.dev/reconciler-test/resources/svc
+	"knative.dev/reconciler-test/resources/svc"
 
 	sourcesv1 "knative.dev/eventing/pkg/apis/sources/v1"
 	"knative.dev/eventing/test/rekt/resources/broker"
@@ -132,26 +132,22 @@ func SendsEventsWithEventTypes() *feature.Feature {
 }
 
 func SendsOneOffsEvent() *feature.Feature {
-	source := feature.MakeRandomK8sName("pingsource")
+	source := feature.MakeRandomK8sName("oneoff pingsource")
 	sink := feature.MakeRandomK8sName("sink")
 	f := feature.NewFeature()
 
 	f.Setup("install sink", eventshub.Install(sink, eventshub.StartReceiver))
 
 	cfg := []manifest.CfgFn{
-		pingsource.WithDataBase64("text/plain", "aGVsbG8sIHdvcmxkIQ=="),
-		pingsource.WithSink(&duckv1.KReference{
-			Kind:       "Service",
-			Name:       sink,
-			APIVersion: "v1",
-		}, ""),
 		pingsource.WithDate((time.Now().Add(1 * time.Minute)).Format("2006-01-02T15:04:05.000Z")),
+		pingsource.WithDataBase64("text/plain", "aGVsbG8sIHdvcmxkIQ=="),
+		pingsource.WithSink(svc.AsKReference(sink), ""),
 	}
-	f.Setup("install pingsource", pingsource.Install(source, cfg...))
 
+	f.Requirement("install pingsource", pingsource.Install(source, cfg...))
 	f.Requirement("pingsource goes ready", pingsource.IsReady(source))
 
-	f.Stable("pingsource as event source").
+	f.Stable("OneOff PingSource as event source").
 		Must("delivers events", assert.OnStore(sink).MatchEvent(
 			test.HasType("dev.knative.sources.ping"),
 		).AtLeast(1))
