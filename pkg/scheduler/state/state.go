738a1fc3b5d86d5e6c9be7231d36831489d98a5d
Knative Prow Robot <knative-prow-robot@google.com>
2023-02-07 14:46:47 +0000

    [release-1.8] Improve scheduler logging for state and pending vpods (#6735)
    This is an automated cherry-pick of #6729
    
    Signed-off-by: Pierangelo Di Pilato <pierdipi@redhat.com>
    Co-authored-by: Pierangelo Di Pilato <pierdipi@redhat.com>

--- pkg/scheduler/state/state.go
+++ pkg/scheduler/state/state.go
@@ -18,8 +18,8 @@ package state
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
-	"fmt"
 	"strconv"
 	"time"
 
@@ -319,10 +319,13 @@ func (s *stateBuilder) State(reserved map[types.NamespacedName]map[string]int32)
 		}
 	}
 
-	s.logger.Infow("cluster state info", zap.String("NumPods", fmt.Sprint(scale.Spec.Replicas)), zap.String("NumZones", fmt.Sprint(len(zoneMap))), zap.String("NumNodes", fmt.Sprint(len(nodeToZoneMap))), zap.String("Schedulable", fmt.Sprint(schedulablePods)))
-	return &State{FreeCap: free, SchedulablePods: schedulablePods.List(), LastOrdinal: last, Capacity: s.capacity, Replicas: scale.Spec.Replicas, NumZones: int32(len(zoneMap)), NumNodes: int32(len(nodeToZoneMap)),
+	state := &State{FreeCap: free, SchedulablePods: schedulablePods.List(), LastOrdinal: last, Capacity: s.capacity, Replicas: scale.Spec.Replicas, NumZones: int32(len(zoneMap)), NumNodes: int32(len(nodeToZoneMap)),
 		SchedulerPolicy: s.schedulerPolicy, SchedPolicy: s.schedPolicy, DeschedPolicy: s.deschedPolicy, NodeToZoneMap: nodeToZoneMap, StatefulSetName: s.statefulSetName, PodLister: s.podLister,
-		PodSpread: podSpread, NodeSpread: nodeSpread, ZoneSpread: zoneSpread}, nil
+		PodSpread: podSpread, NodeSpread: nodeSpread, ZoneSpread: zoneSpread}
+
+	s.logger.Infow("cluster state info", zap.Any("state", state), zap.Any("reserved", toJSONable(reserved)))
+
+	return state, nil
 }
 
 func (s *stateBuilder) updateFreeCapacity(free []int32, last int32, podName string, vreplicas int32) ([]int32, int32) {
@@ -412,4 +415,53 @@ func contains(taints []v1.Taint, taint *v1.Taint) bool {
 		}
 	}
 	return false
+}
+
+func (s *State) MarshalJSON() ([]byte, error) {
+
+	type S struct {
+		FreeCap         []int32                       `json:"freeCap"`
+		SchedulablePods []int32                       `json:"schedulablePods"`
+		LastOrdinal     int32                         `json:"lastOrdinal"`
+		Capacity        int32                         `json:"capacity"`
+		Replicas        int32                         `json:"replicas"`
+		NumZones        int32                         `json:"numZones"`
+		NumNodes        int32                         `json:"numNodes"`
+		NodeToZoneMap   map[string]string             `json:"nodeToZoneMap"`
+		StatefulSetName string                        `json:"statefulSetName"`
+		PodSpread       map[string]map[string]int32   `json:"podSpread"`
+		NodeSpread      map[string]map[string]int32   `json:"nodeSpread"`
+		ZoneSpread      map[string]map[string]int32   `json:"zoneSpread"`
+		SchedulerPolicy scheduler.SchedulerPolicyType `json:"schedulerPolicy"`
+		SchedPolicy     *scheduler.SchedulerPolicy    `json:"schedPolicy"`
+		DeschedPolicy   *scheduler.SchedulerPolicy    `json:"deschedPolicy"`
+	}
+
+	sj := S{
+		FreeCap:         s.FreeCap,
+		SchedulablePods: s.SchedulablePods,
+		LastOrdinal:     s.LastOrdinal,
+		Capacity:        s.Capacity,
+		Replicas:        s.Replicas,
+		NumZones:        s.NumZones,
+		NumNodes:        s.NumNodes,
+		NodeToZoneMap:   s.NodeToZoneMap,
+		StatefulSetName: s.StatefulSetName,
+		PodSpread:       toJSONable(s.PodSpread),
+		NodeSpread:      toJSONable(s.NodeSpread),
+		ZoneSpread:      toJSONable(s.ZoneSpread),
+		SchedulerPolicy: s.SchedulerPolicy,
+		SchedPolicy:     s.SchedPolicy,
+		DeschedPolicy:   s.DeschedPolicy,
+	}
+
+	return json.Marshal(sj)
+}
+
+func toJSONable(ps map[types.NamespacedName]map[string]int32) map[string]map[string]int32 {
+	r := make(map[string]map[string]int32, len(ps))
+	for k, v := range ps {
+		r[k.String()] = v
+	}
+	return r
 }
