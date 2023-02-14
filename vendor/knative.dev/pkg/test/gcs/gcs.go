cd57cc08c163e944300651b5441e34b86cafc2c0
knative-automation <automation@knative.team>
2023-02-14 02:03:51 -0500

    [release-1.7] Upgrade to latest dependencies (#6752)
    Cherry-pick https://github.com/knative-sandbox/control-protocol/pull/254
    to reduce informer cache usage -evankanderson
    
    /cc knative/eventing-writers
    /assign knative/eventing-writers
    
    Produced by: knative-sandbox/knobots/actions/update-deps
    
    Signed-off-by: Knative Automation <automation@knative.team>

--- vendor/knative.dev/pkg/test/gcs/gcs.go
+++ vendor/knative.dev/pkg/test/gcs/gcs.go
@@ -21,7 +21,6 @@ import (
 	"errors"
 	"fmt"
 	"io"
-	"io/ioutil"
 	"os"
 	"path"
 	"strings"
@@ -31,7 +30,7 @@ import (
 	"google.golang.org/api/option"
 )
 
-//nolint // there's also Client so they collide.
+// nolint // there's also Client so they collide.
 type GCSClient struct {
 	*storage.Client
 }
@@ -223,7 +222,7 @@ func (g *GCSClient) ReadObject(ctx context.Context, bucketName, objPath string) 
 		return contents, err
 	}
 	defer f.Close()
-	return ioutil.ReadAll(f)
+	return io.ReadAll(f)
 }
 
 // NewReader creates a new Reader of a gcs file.
