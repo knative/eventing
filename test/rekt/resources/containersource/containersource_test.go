bd530ebee9421ce60e265831b19bce7a870d47c6
Pierangelo Di Pilato <pierdipi@redhat.com>
2023-03-10 15:16:24 +0100

    Use released hearthbeats image for testing (#6808)
    We are duplicating functionality by having 2
    heathbeats images, one for testing and one
    for releases / tutorials.
    
    ---------
    
    Signed-off-by: Pierangelo Di Pilato <pierdipi@redhat.com>

--- test/rekt/resources/containersource/containersource_test.go
+++ test/rekt/resources/containersource/containersource_test.go
@@ -54,7 +54,7 @@ func Example_min() {
 	//     spec:
 	//       containers:
 	//       - name: heartbeats
-	//         image: ko://knative.dev/eventing/test/test_images/heartbeats
+	//         image: ko://knative.dev/eventing/cmd/heartbeats
 	//         imagePullPolicy: IfNotPresent
 	//         args:
 	//         - --period=1
@@ -116,7 +116,7 @@ func Example_full() {
 	//     spec:
 	//       containers:
 	//       - name: heartbeats
-	//         image: ko://knative.dev/eventing/test/test_images/heartbeats
+	//         image: ko://knative.dev/eventing/cmd/heartbeats
 	//         imagePullPolicy: IfNotPresent
 	//         args:
 	//         - --period=1
