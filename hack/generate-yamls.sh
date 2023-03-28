d7510074f511ee6fcfdae0ca43cd6196b73b2583
Abhishek Singh Chauhan <abhisheksinghchauhan357@gmail.com>
2023-03-28 13:00:59 +0530

    removed release label (#6807)
    Fixes #6383
    
    <!-- Please include the 'why' behind your changes if no issue exists -->
    
    ## Proposed Changes
    removed release labels.
    <!-- Please categorize your changes:
    - :gift: Add new feature
    - :bug: Fix bug
    - :broom: Update or clean up current behavior
    - :wastebasket: Remove feature or internal logic
    -->
    
    **Release Note**
    
    <!--
    :page_facing_up: If this change has user-visible impact, write a release
    note in the block
    below. Include the string "action required" if additional action is
    required of
    users switching to the new release, for example in case of a breaking
    change.
    
    Write as if you are speaking to users, not other Knative contributors.
    If this
    change has no user-visible impact, no release note is needed.
    -->
    
    ```release-note
    We have app.kubernetes.io/version label that contains release information, so droping release labels.
    ```
    
    ---------
    
    Signed-off-by: Abhishek357 <abhisheksinghchauhan357@gmail.com>

--- hack/generate-yamls.sh
+++ hack/generate-yamls.sh
@@ -71,7 +71,7 @@ fi
 readonly KO_YAML_FLAGS="${KO_YAML_FLAGS} ${KO_FLAGS}"
 
 if [[ -n "${TAG:-}" ]]; then
-  LABEL_YAML_CMD=(sed -e "s|eventing.knative.dev/release: devel|eventing.knative.dev/release: \"${TAG}\"|" -e "s|app.kubernetes.io/version: devel|app.kubernetes.io/version: \"${TAG:1}\"|")
+  LABEL_YAML_CMD=(sed -e "s|app.kubernetes.io/version: devel|app.kubernetes.io/version: \"${TAG:1}\"|")
 else
   LABEL_YAML_CMD=(cat)
 fi
