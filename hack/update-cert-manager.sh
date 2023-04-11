748bfbd59575d0eafedf6173e096b7dbaaf5a7a8
Vishal Choudhary <contactvishaltech@gmail.com>
2023-04-11 17:20:27 +0530

    Update hack/update-cert-manager.sh
    Co-authored-by: Pierangelo Di Pilato <pierangelodipilato@gmail.com>

--- hack/update-cert-manager.sh
+++ hack/update-cert-manager.sh
@@ -7,8 +7,8 @@ function update_cert_manager() {
   echo "Updating Cert Manager to version $version"
   rm -rf third_party/cert-manager
   mkdir -p third_party/cert-manager
-  curl -L https://github.com/cert-manager/cert-manager/releases/download/$version/cert-manager.yaml > third_party/cert-manager/01-cert-manager.crds.yaml
-  curl -L https://github.com/cert-manager/cert-manager/releases/download/$version/cert-manager.yaml > third_party/cert-manager/02-cert-manager.yaml
+  curl -L https://github.com/cert-manager/cert-manager/releases/download/$version/cert-manager.yaml >third_party/cert-manager/01-cert-manager.crds.yaml
+  curl -L https://github.com/cert-manager/cert-manager/releases/download/$version/cert-manager.yaml >third_party/cert-manager/02-cert-manager.yaml
 }
 
 update_cert_manager "v1.11.1"
