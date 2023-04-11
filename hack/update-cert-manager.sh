726f31983b080566aebdfcbbcdf718d7c95069be
Vishal Choudhary <sendtovishalchoudhary@gmail.com>
2023-04-11 15:23:57 +0530

    updated
    Signed-off-by: Vishal Choudhary <sendtovishalchoudhary@gmail.com>

--- hack/update-cert-manager.sh
+++ hack/update-cert-manager.sh
@@ -5,6 +5,7 @@ set -euo pipefail
 function update_cert_manager() {
   version="$1"
   echo "Updating Cert Manager to version $version"
+  rm -rf third_party/cert-manager
   mkdir -p third_party/cert-manager
   curl -L https://github.com/cert-manager/cert-manager/releases/download/$version/cert-manager.yaml > third_party/cert-manager/01-cert-manager.crds.yaml
   curl -L https://github.com/cert-manager/cert-manager/releases/download/$version/cert-manager.yaml > third_party/cert-manager/02-cert-manager.yaml
