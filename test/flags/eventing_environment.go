ad5cd423ff59a604001dd20868b618f018301e66
Martin Gencur <mgencur@redhat.com>
2023-05-23 14:25:34 +0200

    Do not parse flags in InitializeEventingFlags
    Provides better flexibility for reusing in downstream repositories.
    It is now possible to initialize flags from this repo and any additional
    flags from downstream repositories and then call flag.Parse explicitly
    to fill the flags. Then it's possible to run RunMainTest independently.
    
    This is especially useful when we want to combine REKT tests with
    upgrade test suite.

--- test/flags/eventing_environment.go
+++ test/flags/eventing_environment.go
@@ -18,7 +18,6 @@ package flags
 
 // EventingEnvironmentFlags holds the e2e flags needed only by the eventing repo.
 type EventingEnvironmentFlags struct {
-	BrokerClass string
 	Channels
 	Sources
 	Brokers
