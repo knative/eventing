d5ef675a8f2af89773c7af4c9d7bb4e0be9323f7
Christoph Stäbler <cstabler@redhat.com>
2023-05-24 13:10:12 +0200

    Fix linting issues

--- pkg/kncloudevents/http_client_new.go
+++ pkg/kncloudevents/http_client_new.go
@@ -5,7 +5,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+	http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -79,7 +79,8 @@ func createNewClient(addressable duckv1.Addressable) (*nethttp.Client, error) {
 		}
 
 		base.TLSClientConfig = &tls.Config{
-			RootCAs: certPool,
+			MinVersion: tls.VersionTLS12,
+			RootCAs:    certPool,
 		}
 	}
 
