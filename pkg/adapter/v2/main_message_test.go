0a80638399dcfd1cf4808bc348359919a885cae2
Christoph Stäbler <cstabler@redhat.com>
2023-05-30 08:47:24 +0200

    Use eventingtlstesting.CA instead of custom CA cert for testing

--- pkg/adapter/v2/main_message_test.go
+++ pkg/adapter/v2/main_message_test.go
@@ -22,6 +22,7 @@ import (
 	"testing"
 
 	"go.opencensus.io/stats/view"
+	"knative.dev/eventing/pkg/eventingtls/eventingtlstesting"
 	"knative.dev/eventing/pkg/metrics/source"
 	duckv1 "knative.dev/pkg/apis/duck/v1"
 	"knative.dev/pkg/leaderelection"
@@ -46,7 +47,7 @@ func TestMainMessageAdapterWithContext(t *testing.T) {
 	metricsJson, _ := metrics.OptionsToJSON(m)
 
 	os.Setenv("K_SINK", "http://sink")
-	os.Setenv("K_CA_CERTS", testCaCerts)
+	os.Setenv("K_CA_CERTS", string(eventingtlstesting.CA))
 	os.Setenv("NAMESPACE", "ns")
 	os.Setenv("K_METRICS_CONFIG", metricsJson)
 	os.Setenv("K_LOGGING_CONFIG", "logging")
@@ -76,7 +77,7 @@ func TestMainMessageAdapterWithContext(t *testing.T) {
 				t.Error("Expected sink.URL http://sink, got:", sink.URL.String())
 			}
 
-			if sink.CACerts == nil || *sink.CACerts != testCaCerts {
+			if sink.CACerts == nil || *sink.CACerts != string(eventingtlstesting.CA) {
 				t.Error("Unexpected sink.CACerts, got", sink.CACerts)
 			}
 
@@ -94,29 +95,3 @@ func TestMainMessageAdapterWithContext(t *testing.T) {
 func (m *myAdapterBindings) Start(_ context.Context) error {
 	return nil
 }
-
-const (
-	testCaCerts = `-----BEGIN CERTIFICATE-----
-MIIDmjCCAoKgAwIBAgIUYzA4bTMXevuk3pl2Mn8hpCYL2C0wDQYJKoZIhvcNAQEL
-BQAwLzELMAkGA1UEBhMCVVMxIDAeBgNVBAMMF0tuYXRpdmUtRXhhbXBsZS1Sb290
-LUNBMB4XDTIzMDQwNTEzMTUyNFoXDTI2MDEyMzEzMTUyNFowbTELMAkGA1UEBhMC
-VVMxEjAQBgNVBAgMCVlvdXJTdGF0ZTERMA8GA1UEBwwIWW91ckNpdHkxHTAbBgNV
-BAoMFEV4YW1wbGUtQ2VydGlmaWNhdGVzMRgwFgYDVQQDDA9sb2NhbGhvc3QubG9j
-YWwwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQC5teo+En6U5nhqn7Sc
-uanqswUmPlgs9j/8l21Rhb4T+ezlYKGQGhbJyFFMuiCE1Rjn8bpCwi7Nnv12Y2nz
-FhEv2Jx0yL3Tqx0Q593myqKDq7326EtbO7wmDT0XD03twH5i9XZ0L0ihPWn1mjUy
-WxhnHhoFpXrsnQECJorZY6aTrFbGVYelIaj5AriwiqyL0fET8pueI2GwLjgWHFSH
-X8XsGAlcLUhkQG0Z+VO9usy4M1Wpt+cL6cnTiQ+sRmZ6uvaj8fKOT1Slk/oUeAi4
-WqFkChGzGzLik0QrhKGTdw3uUvI1F2sdQj0GYzXaWqRz+tP9qnXdzk1GrszKKSlm
-WBTLAgMBAAGjcDBuMB8GA1UdIwQYMBaAFJJcCftus4vj98N0zQQautsjEu82MAkG
-A1UdEwQCMAAwCwYDVR0PBAQDAgTwMBQGA1UdEQQNMAuCCWxvY2FsaG9zdDAdBgNV
-HQ4EFgQUnu/3vqA3VEzm128x/hLyZzR9JlgwDQYJKoZIhvcNAQELBQADggEBAFc+
-1cKt/CNjHXUsirgEhry2Mm96R6Yxuq//mP2+SEjdab+FaXPZkjHx118u3PPX5uTh
-gTT7rMfka6J5xzzQNqJbRMgNpdEFH1bbc11aYuhi0khOAe0cpQDtktyuDJQMMv3/
-3wu6rLr6fmENo0gdcyUY9EiYrglWGtdXhlo4ySRY8UZkUScG2upvyOhHTxVCAjhP
-efbMkNjmDuZOMK+wqanqr5YV6zMPzkQK7DspfRgasMAQmugQu7r2MZpXg8Ilhro1
-s/wImGnMVk5RzpBVrq2VB9SkX/ThTVYEC/Sd9BQM364MCR+TA1l8/ptaLFLuwyw8
-O2dgzikq8iSy1BlRsVw=
------END CERTIFICATE-----
-`
-)
