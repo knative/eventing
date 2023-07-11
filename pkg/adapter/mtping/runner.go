a3aea4fa2b31abaa0f7078cdc80a685e2dea6224
Pierangelo Di Pilato <pierdipi@redhat.com>
2023-07-11 13:02:02 +0200

    More debug logs
    Signed-off-by: Pierangelo Di Pilato <pierdipi@redhat.com>

--- pkg/adapter/mtping/runner.go
+++ pkg/adapter/mtping/runner.go
@@ -201,6 +201,13 @@ func (a *cronJobsRunner) newPingSourceClient(source *sourcesv1.PingSource) (adap
 	env.Sink = source.Status.SinkURI.String()
 	env.CACerts = source.Status.SinkCACerts
 
+	a.Logger.Debugw("Creating client",
+		"namespace", source.Namespace,
+		"name", source.Name,
+		"env", env,
+		"source", source,
+	)
+
 	cfg := adapter.ClientConfig{
 		Env:                 &env,
 		CeOverrides:         source.Spec.CloudEventOverrides,
