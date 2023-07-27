3fdc52810548bd6b20ed74508c164b523bd236cb
Leo Li <leoli@redhat.com>
2023-07-27 16:39:47 -0400

    Save the work progress. Complete the first draft code for exactFilter

--- pkg/eventfilter/benchmarks/common_benchmark_test.go
+++ pkg/eventfilter/benchmarks/common_benchmark_test.go
@@ -18,6 +18,8 @@ package benchmarks
 
 import (
 	"context"
+	cetest "github.com/cloudevents/sdk-go/v2/test"
+	"knative.dev/eventing/pkg/eventfilter/subscriptionsapi"
 	"testing"
 
 	cloudevents "github.com/cloudevents/sdk-go/v2"
@@ -54,3 +56,65 @@ func RunFilterBenchmarks(b *testing.B, filterCtor func(interface{}) eventfilter.
 		})
 	}
 }
+
+// Test Exact Filter
+func BenchmarkExactFilter(b *testing.B) {
+	event := cetest.FullEvent()
+
+	RunFilterBenchmarks(b,
+		func(i interface{}) eventfilter.Filter {
+			filter, err := subscriptionsapi.NewExactFilter(i.(map[string]string))
+			if err != nil {
+				b.Fatalf("failed to create filter: %v", err)
+			}
+			return filter
+		},
+		FilterBenchmark{
+			name:  "Pass with exact match of id",
+			arg:   map[string]string{"id": event.ID()},
+			event: event,
+		},
+		FilterBenchmark{
+			// We don't test time because the exact match on the string apparently doesn't work well,
+			// which might cause the filter to fail.
+			name: "Pass with exact match of all context attributes (except time)",
+			arg: map[string]string{
+				"id":              event.ID(),
+				"source":          event.Source(),
+				"type":            event.Type(),
+				"dataschema":      event.DataSchema(),
+				"datacontenttype": event.DataContentType(),
+				"subject":         event.Subject(),
+			},
+			event: event,
+		},
+		FilterBenchmark{
+			name: "No pass with exact match of id and source",
+			arg: map[string]string{
+				"id":     "qwertyuiopasdfghjklzxcvbnm",
+				"source": "qwertyuiopasdfghjklzxcvbnm",
+			},
+			event: event,
+		},
+	)
+}
+
+// Test Not Filter
+//func BenchmarkNotFilter(b *testing.B) {
+//	event := cetest.FullEvent()
+//
+//	RunFilterBenchmarks(b,
+//		func(i interface{}) eventfilter.Filter {
+//			filter := subscriptionsapi.NewNotFilter(i.(eventfilter.Filter))
+//			return filter
+//		},
+//		FilterBenchmark{
+//			name: "No pass with exact match of id and source",
+//			arg: map[string]string{
+//				"id":     "qwertyuiopasdfghjklzxcvbnm",
+//				"source": "qwertyuiopasdfghjklzxcvbnm",
+//			},
+//			event: event,
+//		},
+//	)
+//}
