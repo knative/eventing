d86b68122228dd624521702d9476f284266c6a02
Pierangelo Di Pilato <pierdipi@redhat.com>
2023-08-01 17:19:07 +0200

    Global Resync

--- pkg/reconciler/broker/controller.go
+++ pkg/reconciler/broker/controller.go
@@ -69,11 +69,11 @@ func NewController(
 	configmapInformer := configmapinformer.Get(ctx)
 	secretInformer := secretinformer.Get(ctx)
 
-	var grCb func(obj interface{})
+	var globalResync func(obj interface{})
 
 	featureStore := feature.NewStore(logging.FromContext(ctx).Named("feature-config-store"), func(name string, value interface{}) {
-		if grCb != nil {
-			grCb(nil)
+		if globalResync != nil {
+			globalResync(nil)
 		}
 	})
 	featureStore.WatchConfigs(cmw)
@@ -118,7 +118,7 @@ func NewController(
 	// When the endpoints in our multi-tenant filter/ingress change, do a global resync.
 	// During installation, we might reconcile Brokers before our shared filter/ingress is
 	// ready, so when these endpoints change perform a global resync.
-	grCb = func(obj interface{}) {
+	globalResync = func(obj interface{}) {
 		// Since changes in the Filter/Ingress Service endpoints affect all the Broker objects,
 		// do a global resync.
 		logger.Info("Doing a global resync due to endpoint changes in shared broker component")
