eacaac5a92d85b24c1875293f39118eb3b1e30e0
Matthias Wessendorf <mwessend@redhat.com>
2023-08-28 16:43:01 +0200

    deprecate kntest (#7211)
    Co-authored-by: upodroid <upodroid@users.noreply.github.com>

--- test/e2e-tests.sh
+++ test/e2e-tests.sh
@@ -30,7 +30,7 @@ source "$(dirname "$0")/e2e-common.sh"
 
 # Script entry point.
 
-initialize $@ --skip-istio-addon
+initialize "$@"
 
 echo "Running E2E tests for: Multi Tenant Channel Based Broker, Channel (v1), InMemoryChannel (v1) , ApiServerSource (v1), ContainerSource (v1) and PingSource (v1beta2)"
 go_test_e2e -timeout=1h -parallel=20 ./test/e2e \
