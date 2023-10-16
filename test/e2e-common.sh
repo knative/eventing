27f1db6d97cee780c8231ee0ff8ebf75a6e78301
Martin Gencur <mgencur@redhat.com>
2023-10-16 10:45:13 +0200

    TMP: Induce error

--- test/e2e-common.sh
+++ test/e2e-common.sh
@@ -212,7 +212,7 @@ function install_head {
 function install_latest_release() {
   header ">> Installing Knative Eventing latest public release"
 
-  install_knative_eventing \
+  install_knative_eventing_nonexistent \
     "latest-release" || \
     fail_test "Knative latest release installation failed"
 }
