6c0cf5b65f8a8cb0ce0a06eb25275de3ca4701c6
Pierangelo Di Pilato <pierangelodipilato@gmail.com>
2023-10-20 16:08:51 +0200

    Remove OIDC and auto create FFs
    Co-authored-by: Calum Murray <cmurray@redhat.com>

--- pkg/apis/feature/features.go
+++ pkg/apis/feature/features.go
@@ -59,8 +59,6 @@ func newDefaults() Flags {
 		KReferenceMapping:   Disabled,
 		NewTriggerFilters:   Enabled,
 		TransportEncryption: Disabled,
-		OIDCAuthentication:  Disabled,
-		EvenTypeAutoCreate:  Disabled,
 	}
 }
 
