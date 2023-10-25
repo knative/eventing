a886240a4041c3ba81686f52a548ecfb0f4e99d0
SiBell <simon.bell@campbellsci.co.uk>
2023-10-25 16:11:25 +0100

    chore: e2e test for configuring pingsource with seconds field in schedule

--- test/rekt/pingsource_test.go
+++ test/rekt/pingsource_test.go
@@ -89,3 +89,17 @@ func TestPingSourceWithCloudEventData(t *testing.T) {
 
 	env.Test(ctx, t, pingsource.SendsEventsWithCloudEventData())
 }
+
+func TestPingSourceWithSecondsInSchedule(t *testing.T) {
+	t.Parallel()
+
+	ctx, env := global.Environment(
+		knative.WithKnativeNamespace(system.Namespace()),
+		knative.WithLoggingConfig,
+		knative.WithTracingConfig,
+		k8s.WithEventListener,
+		environment.Managed(t),
+	)
+
+	env.Test(ctx, t, pingsource.SendsEventsWithSecondsInSchedule())
+}
