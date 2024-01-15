66d9178b2160353ad8c901b90de0941d558b713a
sadath-12 <sadathsadu2002@gmail.com>
2024-01-15 22:11:48 +0530

    fix minors
    Signed-off-by: sadath-12 <sadathsadu2002@gmail.com>

--- vendor/github.com/cloudevents/sdk-go/v2/event/event_reader.go
+++ vendor/github.com/cloudevents/sdk-go/v2/event/event_reader.go
@@ -28,33 +28,30 @@ func (e Event) Type() string {
 	return ""
 }
 
-// Scheme implements EventReader.Scheme
-func (e Event) Scheme() string {
-	if e.Context != nil {
-		return e.Source()
-	}
-	return ""
-}
-
 func extractScheme(sourceURL string) (string, error) {
 	parsedURL, err := url.Parse(sourceURL)
 	if err != nil {
 		return "", err
 	}
-	if parsedURL.Scheme == "" {
-		return "http", nil
-	}
 	return parsedURL.Scheme, nil
 }
 
-// Source implements EventReader.Source
-func (e Event) Source() string {
+// Scheme implements EventReader.Scheme
+func (e Event) Scheme() string {
 	if e.Context != nil {
 		scheme , err := extractScheme(e.Context.GetSource())
 		if err != nil {
 			return ""
 		}
 		return scheme
+	}
+	return ""
+}
+
+// Source implements EventReader.Source
+func (e Event) Source() string {
+	if e.Context != nil {
+		return e.Context.GetSource()
 	}
 	return ""
 }
