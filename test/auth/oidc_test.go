47935dc91a7ba9a01929997479abfbcd2962d781
Leo Li <leoli@redhat.com>
2024-01-17 14:48:41 -0500

    Revert "comment out all the other tests to validate my assumption"
    This reverts commit ee4d57e26bedd00a799909df3185182c68689c3d.

--- test/auth/oidc_test.go
+++ test/auth/oidc_test.go
@@ -33,7 +33,14 @@ import (
 
 	"knative.dev/eventing/test/auth/features/oidc"
 	brokerfeatures "knative.dev/eventing/test/rekt/features/broker"
+	"knative.dev/eventing/test/rekt/features/channel"
+	parallelfeatures "knative.dev/eventing/test/rekt/features/parallel"
+	sequencefeatures "knative.dev/eventing/test/rekt/features/sequence"
 	"knative.dev/eventing/test/rekt/resources/broker"
+	"knative.dev/eventing/test/rekt/resources/channel_impl"
+	"knative.dev/eventing/test/rekt/resources/channel_template"
+	"knative.dev/eventing/test/rekt/resources/parallel"
+	"knative.dev/eventing/test/rekt/resources/sequence"
 )
 
 func TestBrokerSupportsOIDC(t *testing.T) {
@@ -70,130 +77,130 @@ func TestBrokerSendsEventsWithOIDCSupport(t *testing.T) {
 	env.TestSet(ctx, t, oidc.BrokerSendEventWithOIDC())
 }
 
-//func TestChannelImplSupportsOIDC(t *testing.T) {
-//	t.Parallel()
-//
-//	ctx, env := global.Environment(
-//		knative.WithKnativeNamespace(system.Namespace()),
-//		knative.WithLoggingConfig,
-//		knative.WithTracingConfig,
-//		k8s.WithEventListener,
-//		environment.Managed(t),
-//		environment.WithPollTimings(4*time.Second, 12*time.Minute),
-//	)
-//
-//	name := feature.MakeRandomK8sName("channelimpl")
-//	env.Prerequisite(ctx, t, channel.ImplGoesReady(name))
-//
-//	env.TestSet(ctx, t, oidc.AddressableOIDCConformance(channel_impl.GVR(), channel_impl.GVK().Kind, name, env.Namespace()))
-//}
-//
-//func TestParallelSupportsOIDC(t *testing.T) {
-//	t.Parallel()
-//
-//	ctx, env := global.Environment(
-//		knative.WithKnativeNamespace(system.Namespace()),
-//		knative.WithLoggingConfig,
-//		knative.WithTracingConfig,
-//		k8s.WithEventListener,
-//		environment.Managed(t),
-//	)
-//
-//	name := feature.MakeRandomK8sName("parallel")
-//	env.Prerequisite(ctx, t, parallelfeatures.GoesReady(name, parallel.WithChannelTemplate(channel_template.ChannelTemplate{
-//		TypeMeta: channel_impl.TypeMeta(),
-//		Spec:     map[string]interface{}{},
-//	})))
-//
-//	env.Test(ctx, t, oidc.ParallelHasAudienceOfInputChannel(name, env.Namespace(), channel_impl.GVR(), channel_impl.GVK().Kind))
-//}
-//
-//func TestChannelDispatcherAuthenticatesWithOIDC(t *testing.T) {
-//	t.Parallel()
-//
-//	ctx, env := global.Environment(
-//		knative.WithKnativeNamespace(system.Namespace()),
-//		knative.WithLoggingConfig,
-//		knative.WithTracingConfig,
-//		k8s.WithEventListener,
-//		environment.Managed(t),
-//	)
-//
-//	env.Test(ctx, t, oidc.ChannelDispatcherAuthenticatesRequestsWithOIDC())
-//}
-//
-//func TestSequenceSupportsOIDC(t *testing.T) {
-//	t.Parallel()
-//
-//	ctx, env := global.Environment(
-//		knative.WithKnativeNamespace(system.Namespace()),
-//		knative.WithLoggingConfig,
-//		knative.WithTracingConfig,
-//		k8s.WithEventListener,
-//		environment.Managed(t),
-//	)
-//
-//	name := feature.MakeRandomK8sName("sequence")
-//	env.Prerequisite(ctx, t, sequencefeatures.GoesReady(name, sequence.WithChannelTemplate(channel_template.ChannelTemplate{
-//		TypeMeta: channel_impl.TypeMeta(),
-//		Spec:     map[string]interface{}{},
-//	})))
-//
-//	env.Test(ctx, t, oidc.SequenceHasAudienceOfInputChannel(name, env.Namespace(), channel_impl.GVR(), channel_impl.GVK().Kind))
-//}
-//
-//func TestApiserversourceSendEventWithJWT(t *testing.T) {
-//	t.Parallel()
-//
-//	ctx, env := global.Environment(
-//		knative.WithKnativeNamespace(system.Namespace()),
-//		knative.WithLoggingConfig,
-//		knative.WithTracingConfig,
-//		k8s.WithEventListener,
-//		environment.Managed(t),
-//	)
-//
-//	env.Test(ctx, t, oidc.ApiserversourceSendEventWithJWT())
-//}
-//
-//func TestContainerSourceSendsEventsWithOIDCSupport(t *testing.T) {
-//	t.Parallel()
-//
-//	ctx, env := global.Environment(
-//		knative.WithKnativeNamespace(system.Namespace()),
-//		knative.WithLoggingConfig,
-//		knative.WithTracingConfig,
-//		k8s.WithEventListener,
-//		environment.Managed(t),
-//	)
-//
-//	env.Test(ctx, t, oidc.SendsEventsWithSinkRefOIDC())
-//}
-//
-//func TestSequenceSendsEventsWithOIDCSupport(t *testing.T) {
-//	t.Parallel()
-//
-//	ctx, env := global.Environment(
-//		knative.WithKnativeNamespace(system.Namespace()),
-//		knative.WithLoggingConfig,
-//		knative.WithTracingConfig,
-//		k8s.WithEventListener,
-//		environment.Managed(t),
-//	)
-//
-//	env.TestSet(ctx, t, oidc.SequenceSendsEventWithOIDC())
-//}
-//
-//func TestParallelTwoBranchesWithOIDCSupport(t *testing.T) {
-//	t.Parallel()
-//
-//	ctx, env := global.Environment(
-//		knative.WithKnativeNamespace(system.Namespace()),
-//		knative.WithLoggingConfig,
-//		knative.WithTracingConfig,
-//		k8s.WithEventListener,
-//		environment.Managed(t),
-//	)
-//
-//	env.Test(ctx, t, oidc.ParallelWithTwoBranchesOIDC(channel_template.ImmemoryChannelTemplate()))
-//}
+func TestChannelImplSupportsOIDC(t *testing.T) {
+	t.Parallel()
+
+	ctx, env := global.Environment(
+		knative.WithKnativeNamespace(system.Namespace()),
+		knative.WithLoggingConfig,
+		knative.WithTracingConfig,
+		k8s.WithEventListener,
+		environment.Managed(t),
+		environment.WithPollTimings(4*time.Second, 12*time.Minute),
+	)
+
+	name := feature.MakeRandomK8sName("channelimpl")
+	env.Prerequisite(ctx, t, channel.ImplGoesReady(name))
+
+	env.TestSet(ctx, t, oidc.AddressableOIDCConformance(channel_impl.GVR(), channel_impl.GVK().Kind, name, env.Namespace()))
+}
+
+func TestParallelSupportsOIDC(t *testing.T) {
+	t.Parallel()
+
+	ctx, env := global.Environment(
+		knative.WithKnativeNamespace(system.Namespace()),
+		knative.WithLoggingConfig,
+		knative.WithTracingConfig,
+		k8s.WithEventListener,
+		environment.Managed(t),
+	)
+
+	name := feature.MakeRandomK8sName("parallel")
+	env.Prerequisite(ctx, t, parallelfeatures.GoesReady(name, parallel.WithChannelTemplate(channel_template.ChannelTemplate{
+		TypeMeta: channel_impl.TypeMeta(),
+		Spec:     map[string]interface{}{},
+	})))
+
+	env.Test(ctx, t, oidc.ParallelHasAudienceOfInputChannel(name, env.Namespace(), channel_impl.GVR(), channel_impl.GVK().Kind))
+}
+
+func TestChannelDispatcherAuthenticatesWithOIDC(t *testing.T) {
+	t.Parallel()
+
+	ctx, env := global.Environment(
+		knative.WithKnativeNamespace(system.Namespace()),
+		knative.WithLoggingConfig,
+		knative.WithTracingConfig,
+		k8s.WithEventListener,
+		environment.Managed(t),
+	)
+
+	env.Test(ctx, t, oidc.ChannelDispatcherAuthenticatesRequestsWithOIDC())
+}
+
+func TestSequenceSupportsOIDC(t *testing.T) {
+	t.Parallel()
+
+	ctx, env := global.Environment(
+		knative.WithKnativeNamespace(system.Namespace()),
+		knative.WithLoggingConfig,
+		knative.WithTracingConfig,
+		k8s.WithEventListener,
+		environment.Managed(t),
+	)
+
+	name := feature.MakeRandomK8sName("sequence")
+	env.Prerequisite(ctx, t, sequencefeatures.GoesReady(name, sequence.WithChannelTemplate(channel_template.ChannelTemplate{
+		TypeMeta: channel_impl.TypeMeta(),
+		Spec:     map[string]interface{}{},
+	})))
+
+	env.Test(ctx, t, oidc.SequenceHasAudienceOfInputChannel(name, env.Namespace(), channel_impl.GVR(), channel_impl.GVK().Kind))
+}
+
+func TestApiserversourceSendEventWithJWT(t *testing.T) {
+	t.Parallel()
+
+	ctx, env := global.Environment(
+		knative.WithKnativeNamespace(system.Namespace()),
+		knative.WithLoggingConfig,
+		knative.WithTracingConfig,
+		k8s.WithEventListener,
+		environment.Managed(t),
+	)
+
+	env.Test(ctx, t, oidc.ApiserversourceSendEventWithJWT())
+}
+
+func TestContainerSourceSendsEventsWithOIDCSupport(t *testing.T) {
+	t.Parallel()
+
+	ctx, env := global.Environment(
+		knative.WithKnativeNamespace(system.Namespace()),
+		knative.WithLoggingConfig,
+		knative.WithTracingConfig,
+		k8s.WithEventListener,
+		environment.Managed(t),
+	)
+
+	env.Test(ctx, t, oidc.SendsEventsWithSinkRefOIDC())
+}
+
+func TestSequenceSendsEventsWithOIDCSupport(t *testing.T) {
+	t.Parallel()
+
+	ctx, env := global.Environment(
+		knative.WithKnativeNamespace(system.Namespace()),
+		knative.WithLoggingConfig,
+		knative.WithTracingConfig,
+		k8s.WithEventListener,
+		environment.Managed(t),
+	)
+
+	env.TestSet(ctx, t, oidc.SequenceSendsEventWithOIDC())
+}
+
+func TestParallelTwoBranchesWithOIDCSupport(t *testing.T) {
+	t.Parallel()
+
+	ctx, env := global.Environment(
+		knative.WithKnativeNamespace(system.Namespace()),
+		knative.WithLoggingConfig,
+		knative.WithTracingConfig,
+		k8s.WithEventListener,
+		environment.Managed(t),
+	)
+
+	env.Test(ctx, t, oidc.ParallelWithTwoBranchesOIDC(channel_template.ImmemoryChannelTemplate()))
+}
