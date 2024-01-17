ec2b5b733ba7a55c59f9ff85ae5f09412e1810b5
Leo Li <leoli@redhat.com>
2024-01-17 15:40:03 -0500

    enable the TLS for TestParallelTwoBranchesWithOIDCSupport
    Signed-off-by: Leo Li <leoli@redhat.com>

--- test/auth/oidc_test.go
+++ test/auth/oidc_test.go
@@ -200,6 +200,7 @@ func TestParallelTwoBranchesWithOIDCSupport(t *testing.T) {
 		knative.WithTracingConfig,
 		k8s.WithEventListener,
 		environment.Managed(t),
+		eventshub.WithTLS(t),
 	)
 
 	env.Test(ctx, t, oidc.ParallelWithTwoBranchesOIDC(channel_template.ImmemoryChannelTemplate()))
