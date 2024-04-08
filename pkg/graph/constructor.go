84115ea9bf55f7009a70e1f727b5eb4b73c5728f
Calum Murray <cmurray@redhat.com>
2024-04-08 11:44:46 -0400

    Map eventtypes into event lineage graph (#7783)
    * cleanup: add helper function for creating/getting vertices
    
    Signed-off-by: Calum Murray <cmurray@redhat.com>
    
    * feat: event lineage now supports eventtypes
    
    Signed-off-by: Calum Murray <cmurray@redhat.com>
    
    * fix: lineage and lineage tests work with new changes
    
    Signed-off-by: Calum Murray <cmurray@redhat.com>
    
    * test/fix: fixed event lineage constructor tests and added new test for eventtype lineage constructor
    
    Signed-off-by: Calum Murray <cmurray@redhat.com>
    
    * fix unit tests
    
    Signed-off-by: Calum Murray <cmurray@redhat.com>
    
    ---------
    
    Signed-off-by: Calum Murray <cmurray@redhat.com>

--- pkg/graph/constructor.go
+++ pkg/graph/constructor.go
@@ -20,6 +20,7 @@ import (
 	"fmt"
 
 	eventingv1 "knative.dev/eventing/pkg/apis/eventing/v1"
+	eventingv1beta3 "knative.dev/eventing/pkg/apis/eventing/v1beta3"
 	messagingv1 "knative.dev/eventing/pkg/apis/messaging/v1"
 	duckv1 "knative.dev/pkg/apis/duck/v1"
 )
@@ -34,13 +35,7 @@ func (g *Graph) AddBroker(broker eventingv1.Broker) {
 	dest := &duckv1.Destination{Ref: ref}
 
 	// check if this vertex already exists
-	v, ok := g.vertices[makeComparableDestination(dest)]
-	if !ok {
-		v = &Vertex{
-			self: dest,
-		}
-		g.vertices[makeComparableDestination(dest)] = v
-	}
+	v := g.getOrCreateVertex(dest)
 
 	if broker.Spec.Delivery == nil || broker.Spec.Delivery.DeadLetterSink == nil {
 		// no DLS, we are done
@@ -48,15 +43,9 @@ func (g *Graph) AddBroker(broker eventingv1.Broker) {
 	}
 
 	// broker has a DLS, we need to add an edge to that
-	to, ok := g.vertices[makeComparableDestination(broker.Spec.Delivery.DeadLetterSink)]
-	if !ok {
-		to = &Vertex{
-			self: broker.Spec.Delivery.DeadLetterSink,
-		}
-		g.vertices[makeComparableDestination(broker.Spec.Delivery.DeadLetterSink)] = to
-	}
+	to := g.getOrCreateVertex(broker.Spec.Delivery.DeadLetterSink)
 
-	v.AddEdge(to, dest, NoTransform)
+	v.AddEdge(to, dest, NoTransform{}, true)
 }
 
 func (g *Graph) AddChannel(channel messagingv1.Channel) {
@@ -72,13 +61,7 @@ func (g *Graph) AddChannel(channel messagingv1.Channel) {
 	}
 	dest := &duckv1.Destination{Ref: ref}
 
-	v, ok := g.vertices[makeComparableDestination(dest)]
-	if !ok {
-		v = &Vertex{
-			self: dest,
-		}
-		g.vertices[makeComparableDestination(dest)] = v
-	}
+	v := g.getOrCreateVertex(dest)
 
 	if channel.Spec.Delivery == nil || channel.Spec.Delivery.DeadLetterSink == nil {
 		// no DLS, we are done
@@ -86,15 +69,50 @@ func (g *Graph) AddChannel(channel messagingv1.Channel) {
 	}
 
 	// channel has a DLS, we need to add an edge to that
-	to, ok := g.vertices[makeComparableDestination(channel.Spec.Delivery.DeadLetterSink)]
-	if !ok {
-		to = &Vertex{
-			self: channel.Spec.Delivery.DeadLetterSink,
+	to := g.getOrCreateVertex(channel.Spec.Delivery.DeadLetterSink)
+
+	v.AddEdge(to, dest, NoTransform{}, true)
+}
+
+func (g *Graph) AddEventType(et *eventingv1beta3.EventType) error {
+	ref := &duckv1.KReference{
+		Name:       et.Name,
+		Namespace:  et.Namespace,
+		APIVersion: "eventing.knative.dev/v1beta3",
+		Kind:       "EventType",
+	}
+	dest := &duckv1.Destination{Ref: ref}
+
+	if et.Spec.Reference.Kind == "Subscription" || et.Spec.Reference.Kind == "Trigger" {
+		outEdge := g.GetPrimaryOutEdgeWithRef(et.Spec.Reference)
+		if outEdge == nil {
+			return fmt.Errorf("trigger/subscription must have a primary outward edge, but had none")
 		}
-		g.vertices[makeComparableDestination(channel.Spec.Delivery.DeadLetterSink)] = to
+
+		outEdge.To().AddEdge(outEdge.From(), dest, EventTypeTransform{EventType: et}, false)
+
+		return nil
 	}
 
-	v.AddEdge(to, dest, NoTransform)
+	from := g.getOrCreateVertex(dest)
+	to := g.getOrCreateVertex(&duckv1.Destination{Ref: et.Spec.Reference})
+
+	from.AddEdge(to, dest, EventTypeTransform{EventType: et}, false)
+
+	return nil
+}
+
+func (g *Graph) getOrCreateVertex(dest *duckv1.Destination) *Vertex {
+	v, ok := g.vertices[makeComparableDestination(dest)]
+	if !ok {
+		v = &Vertex{
+			self:   dest,
+			parent: g,
+		}
+		g.vertices[makeComparableDestination(dest)] = v
+	}
+
+	return v
 }
 
 func (g *Graph) AddTrigger(trigger eventingv1.Trigger) error {
@@ -118,30 +136,18 @@ func (g *Graph) AddTrigger(trigger eventingv1.Trigger) error {
 	}
 	triggerDest := &duckv1.Destination{Ref: triggerRef}
 
-	to, ok := g.vertices[makeComparableDestination(&trigger.Spec.Subscriber)]
-	if !ok {
-		to = &Vertex{
-			self: &trigger.Spec.Subscriber,
-		}
-		g.vertices[makeComparableDestination(&trigger.Spec.Subscriber)] = to
-	}
+	to := g.getOrCreateVertex(&trigger.Spec.Subscriber)
 
 	//TODO: the transform function should be set according to the trigger filter - there are multiple open issues to address this later
-	broker.AddEdge(to, triggerDest, NoTransform)
+	broker.AddEdge(to, triggerDest, NoTransform{}, false)
 
 	if trigger.Spec.Delivery == nil || trigger.Spec.Delivery.DeadLetterSink == nil {
 		return nil
 	}
 
-	dls, ok := g.vertices[makeComparableDestination(trigger.Spec.Delivery.DeadLetterSink)]
-	if !ok {
-		dls = &Vertex{
-			self: trigger.Spec.Delivery.DeadLetterSink,
-		}
-		g.vertices[makeComparableDestination(trigger.Spec.Delivery.DeadLetterSink)] = dls
-	}
+	dls := g.getOrCreateVertex(trigger.Spec.Delivery.DeadLetterSink)
 
-	broker.AddEdge(dls, triggerDest, NoTransform)
+	broker.AddEdge(dls, triggerDest, NoTransform{}, true)
 
 	return nil
 
