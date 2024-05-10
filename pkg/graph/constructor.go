5f4d76ed2db61856a061a235399b1ee587e0493c
Leo Li <leoli@redhat.com>
2024-05-10 11:22:28 -0400

    fix: the new edge should be from subscriber to the reply
    Signed-off-by: Leo Li <leoli@redhat.com>

--- pkg/graph/constructor.go
+++ pkg/graph/constructor.go
@@ -196,8 +196,7 @@ func (g *Graph) AddSubscription(subscription messagingv1.Subscription) error {
 	// If the subscription has a reply field set, there should be another Edge struct.
 	if subscription.Spec.Reply != nil {
 		reply := g.getOrCreateVertex(subscription.Spec.Reply)
-		subscriber := g.getOrCreateVertex(subscriptionDest)
-		subscriber.AddEdge(reply, subscriptionDest, NoTransform{}, false)
+		to.AddEdge(reply, subscriptionDest, NoTransform{}, false)
 	}
 
 	// If the subscription has the deadLetterSink property set on the delivery field, then another Edge should be constructed.
