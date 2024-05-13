43585745f326f60431fe86dc22f27b5bbcbbe0b9
Yijie Wang <147119743+yijie-04@users.noreply.github.com>
2024-05-13 14:03:48 -0400

    CrossNamespace: Testing Code Style (#7918)
    * split parameters
    
    * pointer change for roles
    
    * rolebinding pointer change
    
    * pointer change
    
    * unit test fix

--- pkg/reconciler/testing/v1/rolebinding.go
+++ pkg/reconciler/testing/v1/rolebinding.go
@@ -17,27 +17,88 @@ limitations under the License.
 package testing
 
 import (
-	corev1 "k8s.io/api/core/v1"
 	v1 "k8s.io/api/rbac/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-func CreateRoleBinding(namespace, roleName string, sa *corev1.ServiceAccount) *v1.RoleBinding {
-	return &v1.RoleBinding{
+// RoleBindingOption enables further configuration of a RoleBinding.
+type RoleBindingOption func(*v1.RoleBinding)
+type SubjectOption func(*v1.Subject)
+type RoleRefOption func(*v1.RoleRef)
+
+func CreateRoleBinding(roleName, namespace string, o ...RoleBindingOption) *v1.RoleBinding {
+	rb := &v1.RoleBinding{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      roleName + "-binding",
 			Namespace: namespace,
 		},
-		Subjects: []v1.Subject{
-			{
-				Kind: "ServiceAccount",
-				Name: sa.Name,
-			},
-		},
-		RoleRef: v1.RoleRef{
-			APIGroup: "rbac.authorization.k8s.io",
-			Kind:     "Role",
-			Name:     roleName,
-		},
+		Subjects: []v1.Subject{},
+		RoleRef:  v1.RoleRef{},
+	}
+	for _, opt := range o {
+		opt(rb)
+	}
+	return rb
+}
+
+func WithRoleBindingSubjects(subjects ...*v1.Subject) RoleBindingOption {
+	s := make([]v1.Subject, 0, len(subjects))
+	for _, subject := range subjects {
+		s = append(s, *subject)
+	}
+	return func(rb *v1.RoleBinding) {
+		rb.Subjects = s
+	}
+}
+
+func WithSubjects(o ...SubjectOption) *v1.Subject {
+	s := &v1.Subject{}
+	for _, opt := range o {
+		opt(s)
+	}
+	return s
+}
+
+func WithSubjectKind(kind string) SubjectOption {
+	return func(s *v1.Subject) {
+		s.Kind = kind
+	}
+}
+
+func WithSubjectName(name string) SubjectOption {
+	return func(s *v1.Subject) {
+		s.Name = name
+	}
+}
+
+func WithRoleBindingRoleRef(roleRef *v1.RoleRef) RoleBindingOption {
+	return func(rb *v1.RoleBinding) {
+		rb.RoleRef = *roleRef
+	}
+}
+
+func WithRoleRef(o ...RoleRefOption) *v1.RoleRef {
+	rr := &v1.RoleRef{}
+	for _, opt := range o {
+		opt(rr)
+	}
+	return rr
+}
+
+func WithRoleRefAPIGroup(apiGroup string) RoleRefOption {
+	return func(rr *v1.RoleRef) {
+		rr.APIGroup = apiGroup
+	}
+}
+
+func WithRoleRefKind(kind string) RoleRefOption {
+	return func(rr *v1.RoleRef) {
+		rr.Kind = kind
+	}
+}
+
+func WithRoleRefName(name string) RoleRefOption {
+	return func(rr *v1.RoleRef) {
+		rr.Name = name
 	}
 }
