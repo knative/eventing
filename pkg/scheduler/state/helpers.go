7c97e6ff8358d8fcd3a9158253d9710673ad7e66
Pierangelo Di Pilato <pierdipi@redhat.com>
2024-10-29 21:30:49 +0100

    Schduler: MAXFILLUP strategy will spread vreplicas across multiple pods (#8263)
    * Schduler: MAXFILLUP strategy will spread vreplicas across multiple pods
    
    the MAXFILLUP algorithm was using an affinity strategy, meaning that
    it would prioritize adding new vreplicas to pods with the same resources.
    
    However, the downside is that if one pod goes down or gets
    re-scheduled the entire resource would be down and not produce
    events. By spreading replicas across multiple real replicas we would
    guarantee better availability.
    
    Signed-off-by: Pierangelo Di Pilato <pierdipi@redhat.com>
    
    * Remove configurable HA scheduler, fix reserved replicas logic
    
    Signed-off-by: Pierangelo Di Pilato <pierdipi@redhat.com>
    
    * Log reserved
    
    Signed-off-by: Pierangelo Di Pilato <pierdipi@redhat.com>
    
    * Handle unschedulables pods and always start from reserved no matter what is placements
    
    Signed-off-by: Pierangelo Di Pilato <pierdipi@redhat.com>
    
    * Add reserved + overcommit
    
    Signed-off-by: Pierangelo Di Pilato <pierdipi@redhat.com>
    
    * Add benchmark + reduce OrdinalFromPodName calls
    
    Signed-off-by: Pierangelo Di Pilato <pierdipi@redhat.com>
    
    * Handle unschedulable pods
    
    Signed-off-by: Pierangelo Di Pilato <pierdipi@redhat.com>
    
    ---------
    
    Signed-off-by: Pierangelo Di Pilato <pierdipi@redhat.com>

--- pkg/scheduler/state/helpers.go
+++ pkg/scheduler/state/helpers.go
@@ -17,14 +17,10 @@ limitations under the License.
 package state
 
 import (
-	"context"
-	"math"
 	"strconv"
 	"strings"
-	"time"
 
 	"k8s.io/apimachinery/pkg/types"
-	"k8s.io/apimachinery/pkg/util/wait"
 
 	"knative.dev/eventing/pkg/scheduler"
 )
@@ -36,7 +32,7 @@ func PodNameFromOrdinal(name string, ordinal int32) string {
 func OrdinalFromPodName(podName string) int32 {
 	ordinal, err := strconv.ParseInt(podName[strings.LastIndex(podName, "-")+1:], 10, 32)
 	if err != nil {
-		return math.MaxInt32
+		panic(podName + " is not a valid pod name")
 	}
 	return int32(ordinal)
 }
@@ -49,32 +45,4 @@ func GetVPod(key types.NamespacedName, vpods []scheduler.VPod) scheduler.VPod {
 		}
 	}
 	return nil
-}
-
-func SatisfyZoneAvailability(feasiblePods []int32, states *State) bool {
-	zoneMap := make(map[string]struct{})
-	var zoneName string
-	var err error
-	for _, podID := range feasiblePods {
-		zoneName, _, err = states.GetPodInfo(PodNameFromOrdinal(states.StatefulSetName, podID))
-		if err != nil {
-			continue
-		}
-		zoneMap[zoneName] = struct{}{}
-	}
-	return len(zoneMap) == int(states.NumZones)
-}
-
-func SatisfyNodeAvailability(feasiblePods []int32, states *State) bool {
-	nodeMap := make(map[string]struct{})
-	var nodeName string
-	var err error
-	for _, podID := range feasiblePods {
-		wait.PollUntilContextTimeout(context.Background(), 50*time.Millisecond, 5*time.Second, true, func(ctx context.Context) (bool, error) {
-			_, nodeName, err = states.GetPodInfo(PodNameFromOrdinal(states.StatefulSetName, podID))
-			return err == nil, nil
-		})
-		nodeMap[nodeName] = struct{}{}
-	}
-	return len(nodeMap) == int(states.NumNodes)
 }
