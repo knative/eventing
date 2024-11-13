300a61df6f04a04034ef61a0302d1a73093c0630
Matthias Wessendorf <mwessend@redhat.com>
2024-11-13 17:02:08 +0100

    create new integration/source folder and factor common code into helper.go
    Signed-off-by: Matthias Wessendorf <mwessend@redhat.com>

--- pkg/reconciler/integration/source/integrationsource.go
+++ pkg/reconciler/integration/source/integrationsource.go
@@ -19,6 +19,7 @@ package source
 import (
 	"context"
 	"fmt"
+	"knative.dev/eventing/pkg/reconciler/integration/source/resources"
 
 	"knative.dev/eventing/pkg/reconciler/integration/source/resources"
 
