ca175200aff76d4dca0bb35871cfe2537c1cfd67
Matthias Wessendorf <mwessend@redhat.com>
2024-12-10 10:58:11 +0100

    Reconcile Certificate manifest and add RBAC
    Signed-off-by: Matthias Wessendorf <mwessend@redhat.com>

--- pkg/reconciler/integration/sink/resources/certificate.go
+++ pkg/reconciler/integration/sink/resources/certificate.go
@@ -18,8 +18,11 @@ package resources
 
 import (
 	"fmt"
+	"time"
+
 	cmv1 "github.com/cert-manager/cert-manager/pkg/apis/certmanager/v1"
 	cmmeta "github.com/cert-manager/cert-manager/pkg/apis/meta/v1"
+	"knative.dev/pkg/kmeta"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"knative.dev/eventing/pkg/apis/sinks/v1alpha1"
@@ -30,10 +33,36 @@ func MakeCertificate(sink *v1alpha1.IntegrationSink) *cmv1.Certificate {
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      DeploymentName(sink) + "-cert",
 			Namespace: sink.Namespace,
+			OwnerReferences: []metav1.OwnerReference{
+				*kmeta.NewControllerRef(sink),
+			},
 		},
 		Spec: cmv1.CertificateSpec{
 			SecretName: DeploymentName(sink) + "-cert",
+			SecretTemplate: &cmv1.CertificateSecretTemplate{
+				Labels: map[string]string{
+					"app.kubernetes.io/component": "knative-eventing",
+					"app.kubernetes.io/name":      "integration-sink",
+				},
+			},
+			Duration: &metav1.Duration{
+				Duration: 90 * 24 * time.Hour, // 90 days
+			},
+			RenewBefore: &metav1.Duration{
+				Duration: 15 * 24 * time.Hour, // 15 days
+			},
+
+			Subject: &cmv1.X509Subject{
+				Organizations: []string{"local"},
+			},
+			PrivateKey: &cmv1.CertificatePrivateKey{
+				Algorithm:      cmv1.RSAKeyAlgorithm,
+				Encoding:       cmv1.PKCS1,
+				Size:           2048,
+				RotationPolicy: cmv1.RotationPolicyAlways,
+			},
 			DNSNames: []string{
+				fmt.Sprintf("%s.%s.svc.cluster.local", DeploymentName(sink), sink.Namespace),
 				fmt.Sprintf("%s.%s.svc", DeploymentName(sink), sink.Namespace),
 			},
 			IssuerRef: cmmeta.ObjectReference{
