f7c966363558df453ffeb34bf137ba680cb1a992
Pierangelo Di Pilato <pierdipi@redhat.com>
2025-02-10 10:12:07 +0100

    Add EventTransform API
    Signed-off-by: Pierangelo Di Pilato <pierdipi@redhat.com>

---
+++ pkg/apis/eventing/v1alpha1/eventtransform_types.go
@@ -0,0 +1,137 @@
+/*
+Copyright 2025 The Knative Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	appsv1 "k8s.io/api/apps/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+
+	"knative.dev/pkg/apis"
+	duckv1 "knative.dev/pkg/apis/duck/v1"
+	"knative.dev/pkg/kmeta"
+)
+
+// +genclient
+// +genreconciler
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// EventTransform represents an even transformation.
+type EventTransform struct {
+	metav1.TypeMeta `json:",inline"`
+	// +optional
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	// Spec defines the desired state of the EventTransform.
+	Spec EventTransformSpec `json:"spec,omitempty"`
+
+	// Status represents the current state of the EventTransform.
+	// This data may be out of date.
+	// +optional
+	Status EventTransformStatus `json:"status,omitempty"`
+}
+
+var (
+	// Check that EventTransform can be validated, can be defaulted, and has immutable fields.
+	_ apis.Validatable = (*EventTransform)(nil)
+
+	// Check that EventTransform can return its spec untyped.
+	_ apis.HasSpec = (*EventTransform)(nil)
+
+	_ runtime.Object = (*EventTransform)(nil)
+
+	// Check that we can create OwnerReferences to an EventTransform.
+	_ kmeta.OwnerRefable = (*EventTransform)(nil)
+
+	// Check that the type conforms to the duck Knative Resource shape.
+	_ duckv1.KRShaped = (*EventTransform)(nil)
+)
+
+type EventTransformSpec struct {
+
+	// Sink is a reference to an object that will resolve to a uri to use as the sink.
+	//
+	// If not present, the transformation will send back the transformed event as response, this
+	// is useful to leverage the built-in Broker reply feature to re-publish a transformed event
+	// back to the broker.
+	//
+	// +optional
+	Sink *duckv1.Destination `json:"sink,omitempty"`
+
+	// Transformations contain all possible transformations, only one "type" can be used.
+	Transformations EventTransformations `json:",inline"`
+}
+
+type EventTransformations struct {
+	Jsonata *JsonataEventTransformationSpec `json:"jsonata,omitempty"`
+}
+
+type JsonataEventTransformationSpec struct {
+	// Expression is the JSONata expression.
+	Expression string `json:"expression,omitempty"`
+}
+
+// EventTransformStatus represents the current state of a EventTransform.
+type EventTransformStatus struct {
+	// SourceStatus inherits duck/v1 SourceStatus, which currently provides:
+	// * ObservedGeneration - the 'Generation' of the Service that was last
+	//   processed by the controller.
+	// * Conditions - the latest available observations of a resource's current
+	//   state.
+	// * SinkURI - the current active sink URI that has been configured for the
+	//   Source.
+	duckv1.SourceStatus `json:",inline"`
+
+	// AddressStatus is the part where the EventTransform fulfills the Addressable contract.
+	// It exposes the endpoint as an URI to get events delivered.
+	// +optional
+	duckv1.AddressStatus `json:",inline"`
+
+	// JsonataTransformationStatus is the status associated with JsonataEventTransformationSpec.
+	// +optional
+	JsonataTransformationStatus *JsonataEventTransformationStatus `json:"jsonata,omitempty"`
+}
+
+type JsonataEventTransformationStatus struct {
+	Deployment appsv1.DeploymentStatus `json:"deployment,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// TransformList is a collection of EventTransform.
+type TransformList struct {
+	metav1.TypeMeta `json:",inline"`
+	// +optional
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []EventTransform `json:"items"`
+}
+
+// GetGroupVersionKind returns GroupVersionKind for EventTransform
+func (ep *EventTransform) GetGroupVersionKind() schema.GroupVersionKind {
+	return SchemeGroupVersion.WithKind("EventTransform")
+}
+
+// GetUntypedSpec returns the spec of the EventTransform.
+func (ep *EventTransform) GetUntypedSpec() interface{} {
+	return ep.Spec
+}
+
+// GetStatus retrieves the status of the EventTransform. Implements the KRShaped interface.
+func (ep *EventTransform) GetStatus() *duckv1.Status {
+	return &ep.Status.Status
+}
---
+++ pkg/apis/eventing/v1alpha1/eventtransform_validation.go
@@ -0,0 +1,79 @@
+/*
+Copyright 2025 The Knative Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	"context"
+
+	"knative.dev/pkg/apis"
+)
+
+func (t *EventTransform) Validate(ctx context.Context) *apis.FieldError {
+	ctx = apis.WithinParent(ctx, t.ObjectMeta)
+	return t.Spec.Validate(ctx).ViaField("spec")
+}
+
+var possibleTransformations = []string{"jsonata"}
+
+func (ts *EventTransformSpec) Validate(ctx context.Context) *apis.FieldError {
+	var errs *apis.FieldError
+
+	// Only one type of transformation is allowed.
+	// These are transformations field paths.
+	transformations := make([]string, 0, 2)
+
+	if ts.Transformations.Jsonata != nil {
+		transformations = append(transformations, "jsonata")
+	}
+
+	if len(transformations) == 0 {
+		errs = errs.Also(apis.ErrMissingOneOf(possibleTransformations...))
+	} else if len(transformations) > 1 {
+		errs = errs.Also(apis.ErrMultipleOneOf(transformations...))
+	}
+
+	errs = errs.Also(ts.Transformations.Jsonata.Validate(ctx).ViaField("jsonata"))
+	errs = errs.Also(ts.Sink.Validate(ctx).ViaField("sink"))
+
+	if apis.IsInUpdate(ctx) {
+		original := apis.GetBaseline(ctx).(*EventTransform)
+		errs = errs.Also(ts.CheckImmutableFields(ctx, original))
+	}
+
+	return errs
+}
+
+func (js *JsonataEventTransformationSpec) Validate(context.Context) *apis.FieldError {
+	// Jsonata parsers for Go are not maintained, therefore, we will not parse the expression here.
+	// The downside is that the errors will only be present in the status of the EventTransform resource.
+	// We can reconsider this in the future and improve.
+	return nil
+}
+
+func (in *EventTransformSpec) CheckImmutableFields(_ context.Context, original *EventTransform) *apis.FieldError {
+	if original == nil {
+		return nil
+	}
+
+	var errs *apis.FieldError
+
+	if original.Spec.Transformations.Jsonata != nil && in.Transformations.Jsonata == nil {
+		errs = errs.Also(apis.ErrGeneric("transformations types are immutable, jsonata transformation cannot be changed to a different transformation type"))
+	}
+
+	return errs
+}
