15a82afda18d45d57b650e106d012184c2146f6d
Matthias Wessendorf <mwessend@redhat.com>
2025-03-06 14:20:25 +0100

    Update to use their client set
    Signed-off-by: Matthias Wessendorf <mwessend@redhat.com>

--- pkg/reconciler/integration/sink/integrationsink.go
+++ pkg/reconciler/integration/sink/integrationsink.go
@@ -45,10 +45,9 @@ import (
 	"knative.dev/eventing/pkg/auth"
 	eventingv1alpha1listers "knative.dev/eventing/pkg/client/listers/eventing/v1alpha1"
 
-	certmanagerclientset "knative.dev/eventing/pkg/client/certmanager/clientset/versioned"
+	certmanagerclientset "github.com/cert-manager/cert-manager/pkg/client/clientset/versioned"
 
-	certmanagerlisters "knative.dev/eventing/pkg/client/certmanager/listers/certmanager/v1"
-
+	certmanagerlisters "github.com/cert-manager/cert-manager/pkg/client/listers/certmanager/v1"
 	"knative.dev/eventing/pkg/eventingtls"
 	duckv1 "knative.dev/pkg/apis/duck/v1"
 	"knative.dev/pkg/controller"
