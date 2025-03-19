4080fcf142b8ca08acf5226957c6708db562d9e8
Matthias Wessendorf <mwessend@redhat.com>
2025-03-19 08:33:14 +0100

    Try to bump to latest of CE Conformance (#8532)
    Try to bump to latest of this
    
    Signed-off-by: Matthias Wessendorf <mwessend@redhat.com>

--- vendor/github.com/cloudevents/conformance/pkg/event/event.go
+++ vendor/github.com/cloudevents/conformance/pkg/event/event.go
@@ -1,3 +1,8 @@
+/*
+ Copyright 2022 The CloudEvents Authors
+ SPDX-License-Identifier: Apache-2.0
+*/
+
 package event
 
 type MutationFn func(Event) (Event, error)
@@ -11,10 +16,38 @@ const (
 
 type Event struct {
 	Mode                string            `yaml:"Mode,omitempty"`
-	Attributes          ContextAttributes `yaml:"ContextAttributes"`
+	Attributes          ContextAttributes `yaml:"ContextAttributes,omitempty"`
 	TransportExtensions Extensions        `yaml:"TransportExtensions,omitempty"`
-	Data                string            `yaml:"Data"`
+	Data                string            `yaml:"Data,omitempty"`
 	// TODO: add support for data_base64
+}
+
+// Get returns the value of the attribute or extension named `key`.
+func (e *Event) Get(key string) string {
+	switch key {
+	case "specversion":
+		return e.Attributes.SpecVersion
+	case "type":
+		return e.Attributes.Type
+	case "time":
+		return e.Attributes.Time
+	case "id":
+		return e.Attributes.ID
+	case "source":
+		return e.Attributes.Source
+	case "subject":
+		return e.Attributes.Subject
+	case "schemaurl":
+		return e.Attributes.SchemaURL
+	case "dataschema":
+		return e.Attributes.DataSchema
+	case "dataecontentncoding":
+		return e.Attributes.DataContentEncoding
+	case "datacontenttype":
+		return e.Attributes.DataContentType
+	default:
+		return e.Attributes.Extensions[key]
+	}
 }
 
 type ContextAttributes struct {
--- vendor/github.com/cloudevents/conformance/pkg/event/write.go
+++ vendor/github.com/cloudevents/conformance/pkg/event/write.go
@@ -1,3 +1,8 @@
+/*
+ Copyright 2022 The CloudEvents Authors
+ SPDX-License-Identifier: Apache-2.0
+*/
+
 package event
 
 import (
