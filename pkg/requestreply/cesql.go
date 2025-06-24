7f047f979e61b5bfb0941359843f3f64567f6994
Calum Murray <cmurray@redhat.com>
2025-06-24 11:45:04 -0400

    feat: add request reply cesql function (#8608)
    * feat: add cesql function for verifying requestreply correlation ids
    
    Signed-off-by: Calum Murray <cmurray@redhat.com>
    
    * refactor: VerifyCorrelationId accepts the correlation id value instead of name
    
    Signed-off-by: Calum Murray <cmurray@redhat.com>
    
    * test: added unit tests for CESQL function KN_VERIFY_CORRELATION_ID
    
    Signed-off-by: Calum Murray <cmurray@redhat.com>
    
    * docs: add comment explaining the cesql function signature
    
    Signed-off-by: Calum Murray <cmurray@redhat.com>
    
    * fix formatting
    
    Signed-off-by: Calum Murray <cmurray@redhat.com>
    
    * chore: add license header comments
    
    Signed-off-by: Calum Murray <cmurray@redhat.com>
    
    ---------
    
    Signed-off-by: Calum Murray <cmurray@redhat.com>

---
+++ pkg/requestreply/cesql.go
@@ -0,0 +1,83 @@
+/*
+Copyright 2025 The Knative Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package requestreply
+
+import (
+	"context"
+	"sync"
+
+	cesql "github.com/cloudevents/sdk-go/sql/v2"
+	cefn "github.com/cloudevents/sdk-go/sql/v2/function"
+	ceruntime "github.com/cloudevents/sdk-go/sql/v2/runtime"
+	cloudevents "github.com/cloudevents/sdk-go/v2"
+
+	secretinformer "knative.dev/pkg/injection/clients/namespacedkube/informers/core/v1/secret"
+	"knative.dev/pkg/logging"
+)
+
+var once = sync.Once{}
+
+// RegisterCESQLVerifyCorrelationIdFilter registers a new function with the CESQL runtime to verify correlation ids
+// The function signature is KN_VERIFY_CORRELATION_ID(correlation_id, namespace, secret_names...)
+func RegisterCESQLVerifyCorrelationIdFilter(ctx context.Context) error {
+	secretInformer := secretinformer.Get(ctx)
+	logger := logging.FromContext(ctx)
+
+	var verifyCorrelationId = cefn.NewFunction(
+		"KN_VERIFY_CORRELATION_ID",
+		[]cesql.Type{cesql.StringType, cesql.StringType},
+		cesql.TypePtr(cesql.StringType),
+		cesql.BooleanType,
+		func(e cloudevents.Event, args []interface{}) (interface{}, error) {
+			replyId, namespace := args[0].(string), args[1].(string)
+			secretNames := args[2:]
+
+			for _, secretName := range secretNames {
+				secret, err := secretInformer.Lister().Secrets(namespace).Get(secretName.(string))
+				if err != nil {
+					logger.Warnf("failed to get secret %s in namespace %s: %s", secretName.(string), namespace, err.Error())
+					continue
+				}
+
+				aesKey, ok := secret.Data["key"]
+				if !ok {
+					logger.Warnf("no key in secret %s in namespace %s", secretName.(string), namespace)
+					continue
+				}
+
+				validId, err := VerifyReplyId(replyId, aesKey)
+				if err != nil {
+					logger.Warnf("failed to verify replyid for event: %s", err)
+				}
+
+				if validId {
+					return true, nil
+				}
+			}
+
+			return false, nil
+		},
+	)
+
+	var err error = nil
+
+	once.Do(func() {
+		err = ceruntime.AddFunction(verifyCorrelationId)
+	})
+
+	return err
+}
