6e5cb3a209fdb1c0953e168847a88684c07b2388
Calum Murray <cmurray@redhat.com>
2025-07-11 14:10:06 -0400

    feat: add new otel config (#8624)
    * feat: add new otel config
    
    Signed-off-by: Calum Murray <cmurray@redhat.com>
    
    * chore: run ./hack/update-deps.sh
    
    Signed-off-by: Calum Murray <cmurray@redhat.com>
    
    * chore: fix boilerplate comments to use tabs instead of spaces
    
    Signed-off-by: Calum Murray <cmurray@redhat.com>
    
    * fix(test): otel config tests work
    
    Signed-off-by: Calum Murray <cmurray@redhat.com>
    
    * fix: configmap comments reflect eventing resources, not serving
    
    Signed-off-by: Calum Murray <cmurray@redhat.com>
    
    * change example metrics url to more sensible value
    
    Signed-off-by: Calum Murray <cmurray@redhat.com>
    
    * chore: update codegen to correct configmap checksum
    
    Signed-off-by: Calum Murray <cmurray@redhat.com>
    
    * cleanup: drop request metrics as they are unused in eventing
    
    Signed-off-by: Calum Murray <cmurray@redhat.com>
    
    * chore: fix configmap checksum
    
    Signed-off-by: Calum Murray <cmurray@redhat.com>
    
    ---------
    
    Signed-off-by: Calum Murray <cmurray@redhat.com>

---
+++ pkg/observability/config.go
@@ -0,0 +1,84 @@
+/*
+Copyright 2025 The Knative Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package observability
+
+import (
+	"context"
+
+	configmap "knative.dev/pkg/configmap/parser"
+	pkgo11y "knative.dev/pkg/observability"
+)
+
+const (
+	// EnableSinkEventErrorReportingKey is the CM key to enable emitting a k8s event when delivery to a sink fails
+	EnableSinkEventErrorReportingKey = "sink-event-error-reporting.enable"
+
+	// DefaultEnableSinkEventErrorReporting is used to set the default sink event error reporting value
+	DefaultEnableSinkEventErrorReporting = false
+)
+
+type (
+	BaseConfig    = pkgo11y.Config
+	MetricsConfig = pkgo11y.MetricsConfig
+	RuntimeConfig = pkgo11y.RuntimeConfig
+	TracingConfig = pkgo11y.TracingConfig
+)
+
+// +k8s:deepcopy-gen=true
+type Config struct {
+	BaseConfig
+
+	// EnableSinkEventErrorReporting specifies whether we should emit a k8s
+	// event when delivery to a sink fails
+	EnableSinkEventErrorReporting bool `json:"enableSinkEventErrorReporting"`
+}
+
+func DefaultConfig() *Config {
+	return &Config{
+		BaseConfig:                    *pkgo11y.DefaultConfig(),
+		EnableSinkEventErrorReporting: DefaultEnableSinkEventErrorReporting,
+	}
+}
+
+func NewFromMap(m map[string]string) (*Config, error) {
+	c := DefaultConfig()
+
+	if cfg, err := pkgo11y.NewFromMap(m); err != nil {
+		return nil, err
+	} else {
+		c.BaseConfig = *cfg
+	}
+
+	err := configmap.Parse(m, configmap.As(EnableSinkEventErrorReportingKey, &c.EnableSinkEventErrorReporting))
+	if err != nil {
+		return c, err
+	}
+
+	return c, nil
+}
+
+type cfgKey struct{}
+
+func WithConfig(ctx context.Context, cfg *Config) context.Context {
+	return context.WithValue(ctx, cfgKey{}, cfg)
+}
+
+func GetConfig(ctx context.Context) *Config {
+	// we can ignore the "ok" value here, because the config will just be nil in that case
+	cfg, _ := ctx.Value(cfgKey{}).(*Config)
+	return cfg
+}
---
+++ pkg/observability/otel/otel.go
@@ -0,0 +1,136 @@
+/*
+Copyright 2025 The Knative Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package otel
+
+import (
+	"context"
+
+	"go.opentelemetry.io/contrib/instrumentation/runtime"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/sdk/metric"
+	"go.opentelemetry.io/otel/sdk/trace"
+
+	"go.uber.org/zap"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/util/workqueue"
+
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	k8stoolmetrics "k8s.io/client-go/tools/metrics"
+	"knative.dev/eventing/pkg/observability"
+	"knative.dev/eventing/pkg/observability/configmap"
+	kubeclient "knative.dev/pkg/client/injection/kube/client"
+	"knative.dev/pkg/controller"
+	o11yconfigmap "knative.dev/pkg/observability/configmap"
+	"knative.dev/pkg/observability/metrics"
+	k8smetrics "knative.dev/pkg/observability/metrics/k8s"
+	"knative.dev/pkg/observability/resource"
+	k8sruntime "knative.dev/pkg/observability/runtime/k8s"
+	"knative.dev/pkg/observability/tracing"
+	"knative.dev/pkg/system"
+)
+
+func SetupObservabilityOrDie(
+	ctx context.Context,
+	component string,
+	logger *zap.SugaredLogger,
+	pprof *k8sruntime.ProfilingServer,
+) (*metrics.MeterProvider, *tracing.TracerProvider) {
+	cfg, err := GetObservabilityConfig(ctx)
+	if err != nil {
+		logger.Fatalw("error loading observability configuration", zap.Error(err))
+	}
+
+	pprof.UpdateFromConfig(cfg.Runtime)
+
+	otelResource := resource.Default(component)
+
+	meterProvider, err := metrics.NewMeterProvider(
+		ctx,
+		cfg.Metrics,
+		metric.WithResource(otelResource),
+	)
+	if err != nil {
+		logger.Fatalw("failed to set up meter provider", zap.Error(err))
+	}
+
+	otel.SetMeterProvider(meterProvider)
+
+	workQueueMetrics, err := k8smetrics.NewWorkqueueMetricsProvider(
+		k8smetrics.WithMeterProvider(meterProvider),
+	)
+	if err != nil {
+		logger.Fatalw("failed to setup k8s workqueue metrics", zap.Error(err))
+	}
+
+	workqueue.SetProvider(workQueueMetrics)
+	controller.SetMetricsProvider(workQueueMetrics)
+
+	clientMetrics, err := k8smetrics.NewClientMetricProvider(
+		k8smetrics.WithMeterProvider(meterProvider),
+	)
+	if err != nil {
+		logger.Fatalw("failed to setup k8s client-go metrics", zap.Error(err))
+	}
+
+	k8stoolmetrics.Register(k8stoolmetrics.RegisterOpts{
+		RequestLatency: clientMetrics.RequestLatencyMetric(),
+		RequestResult:  clientMetrics.RequestResultMetric(),
+	})
+
+	err = runtime.Start(
+		runtime.WithMinimumReadMemStatsInterval(cfg.Runtime.ExportInterval),
+	)
+	if err != nil {
+		logger.Fatalw("failed to start runtime metrics", zap.Error(err))
+	}
+
+	tracerProvider, err := tracing.NewTracerProvider(
+		ctx,
+		cfg.Tracing,
+		trace.WithResource(otelResource),
+	)
+	if err != nil {
+		logger.Fatalw("failed to setup trace provider", zap.Error(err))
+	}
+
+	otel.SetTextMapPropagator(tracing.DefaultTextMapPropagator())
+	otel.SetTracerProvider(tracerProvider)
+
+	return meterProvider, tracerProvider
+}
+
+// GetObservabilityConfig gets the observability config from the (in order):
+// 1. the provided context,
+// 2. from the API server,
+// 3. default values (if not found and there were no other errors in the api server request).
+func GetObservabilityConfig(ctx context.Context) (*observability.Config, error) {
+	if cfg := observability.GetConfig(ctx); cfg != nil {
+		return cfg, nil
+	}
+
+	cm, err := kubeclient.Get(ctx).CoreV1().ConfigMaps(system.Namespace()).
+		Get(ctx, o11yconfigmap.Name(), metav1.GetOptions{})
+
+	if apierrors.IsNotFound(err) {
+		return observability.DefaultConfig(), nil
+	} else if err != nil {
+		return nil, err
+	}
+
+	return configmap.Parse(cm)
+}
